9c5342897f9d63fa4e890f84b48c33d3a8116e1b
Aaron Daub <aarondaub@Aarons-MacBook-Pro.local>
2015-05-21 01:22:27 +0200

    Fix linters property by removing incorrect early return and by setting inContext correctly

--- Source/SwiftLintFramework/Linter.swift
+++ Source/SwiftLintFramework/Linter.swift
@@ -39,16 +39,19 @@ public struct Linter {
     
     var regions: [LinterRegion] = flatten(self.context.file.contents.lines().map { (line: Line) -> (LinterRegion)? in
       if line.content == linterContextBegin {
-        if inContext { // starting a new context, so we need the region for the code BEFORE this
-          return (contextEndingLineNumber, currentLineNumber - 1)
-        }
-      
+        let startingNewContext = !inContext
+        
         inContext = true
         contextDepth += 1
         contextStartingLineNumber = currentLineNumber + 1
+        
+        if startingNewContext { // starting a new context, so we need the region for the code BEFORE this
+          return (contextEndingLineNumber, currentLineNumber - 1)
+        }
+        
       } else if line.content == linterContextEnd && inContext {
         contextDepth -= 1
-        inContext = contextDepth == 0
+        inContext = contextDepth != 0
         
         contextEndingLineNumber = currentLineNumber - 1
         if !inContext {
@@ -64,6 +67,7 @@ public struct Linter {
     // let's add that region to regions
     let lastRegion: LinterRegion = (contextEndingLineNumber, max(self.context.file.contents.lines().count - 1, 0))
     regions.append(lastRegion)
+    
     
     let linters = regions.map { (region: LinterRegion) -> Linter in
       let file = File(contents: (self.context.file.contents as NSString).substringWithRange(NSMakeRange(region.start, region.end - region.start)))

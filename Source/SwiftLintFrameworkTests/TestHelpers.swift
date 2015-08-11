fdd814c64f0663318d9b15896702b2953331ca00
Keith Smiley <keithbsmiley@gmail.com>
2015-08-11 15:34:28 -0700

    Only validate examples produce their own violations
    When adding new general rules, examples from other rules could violate
    your new rule. Since we're already testing rules practically through the
    integration tests we should only validate that examples produce the
    expected violation types.

--- Source/SwiftLintFrameworkTests/TestHelpers.swift
+++ Source/SwiftLintFrameworkTests/TestHelpers.swift
@@ -14,16 +14,20 @@ func violations(string: String) -> [StyleViolation] {
     return Linter(file: File(contents: string)).styleViolations
 }
 
+private func violations(string: String, type: StyleViolationType) -> [StyleViolation] {
+    return violations(string).filter { $0.type == type }
+}
+
 extension XCTestCase {
     func verifyRule(rule: RuleExample,
         type: StyleViolationType,
         commentDoesntViolate: Bool = true) {
-        XCTAssertEqual(rule.nonTriggeringExamples.flatMap({violations($0)}), [])
-        XCTAssertEqual(rule.triggeringExamples.flatMap({violations($0).map({$0.type})}),
+        XCTAssertEqual(rule.nonTriggeringExamples.flatMap({violations($0, type)}), [])
+        XCTAssertEqual(rule.triggeringExamples.flatMap({violations($0, type).map({$0.type})}),
             Array(count: rule.triggeringExamples.count, repeatedValue: type))
 
         if commentDoesntViolate {
-            XCTAssertEqual(rule.triggeringExamples.flatMap({violations("// " + $0)}), [])
+            XCTAssertEqual(rule.triggeringExamples.flatMap({violations("// " + $0, type)}), [])
         }
     }
 }

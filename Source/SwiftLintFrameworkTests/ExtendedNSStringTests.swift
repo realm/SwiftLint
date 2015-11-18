2611a75ffb68938c29a8ad39aca2bc76e82ea40d
crimsonwoods <crimsonwoods.dev@gmail.com>
2015-11-18 11:31:12 +0900

    Add test code to verify change: 247f02eff49e31a761f57a3b2c1c45e47e6ca041
    That change is made to fix issue #213.

---
+++ Source/SwiftLintFrameworkTests/ExtendedNSStringTests.swift
@@ -0,0 +1,30 @@
+//
+//  ExtendedNSStringTests.swift
+//  SwiftLint
+//
+//  Created by crimsonwoods on 2015/11/18.
+//  Copyright © 2015 Realm. All rights reserved.
+//
+
+import XCTest
+
+class ExtendedNSStringTests: XCTestCase {
+    func testLineAndCharacterForByteOffset_forContentsContainingMultibyteCharacters() {
+        let contents = "" +
+        "import Foundation\n" +                               // 18 characters
+        "class Test {\n" +                                    // 13 characters
+            "func test() {\n" +                               // 14 characters
+                "// 日本語コメント : comment in Japanese\n" + // 33 characters
+                "// do something\n"                           // 16 characters
+            "}\n" +
+        "}"
+        let string = NSString(string: contents)
+        // A character placed on 80 offset indicates a white-space before 'do' at 5th line.
+        if let lineAndCharacter = string.lineAndCharacterForCharacterOffset(80) {
+            XCTAssertEqual(lineAndCharacter.line, 5)
+            XCTAssertEqual(lineAndCharacter.character, 3)
+        } else {
+            XCTFail("NSString.lineAndCharacterForByteOffset should return non-nil tuple.")
+        }
+    }
+}

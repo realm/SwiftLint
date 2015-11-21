427d58b14e14e4add46978901c166185b8449c26
JP Simard <jp@jpsim.com>
2015-11-20 23:37:48 -0800

    fix formatting issues in ExtendedNSStringTests

--- Source/SwiftLintFrameworkTests/ExtendedNSStringTests.swift
+++ Source/SwiftLintFrameworkTests/ExtendedNSStringTests.swift
@@ -14,8 +14,8 @@ class ExtendedNSStringTests: XCTestCase {
         "import Foundation\n" +                               // 18 characters
         "class Test {\n" +                                    // 13 characters
             "func test() {\n" +                               // 14 characters
-                "// 日本語コメント : comment in Japanese\n" + // 33 characters
-                "// do something\n"                           // 16 characters
+                "// 日本語コメント : comment in Japanese\n" +   // 33 characters
+                "// do something\n" +                         // 16 characters
             "}\n" +
         "}"
         let string = NSString(string: contents)

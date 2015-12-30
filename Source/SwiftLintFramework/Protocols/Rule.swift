fcf9396a08046b81ecbd24da769e1a04f62fb11d
Scott Hoyt <scottrhoyt@gmail.com>
2015-12-29 20:20:00 -0600

    Added isEqual for Rule and ParameterizedRule. Made Configuration and RuleParameter conform to Equatable. Wrote tests to verify. All done in support of testing recursive configuration file search.

--- Source/SwiftLintFramework/Protocols/Rule.swift
+++ Source/SwiftLintFramework/Protocols/Rule.swift
@@ -13,12 +13,35 @@ public protocol Rule {
     func validateFile(file: File) -> [StyleViolation]
 }
 
+extension Rule {
+    func isEqualTo(rule: Rule) -> Bool {
+        return self.dynamicType.description == rule.dynamicType.description
+    }
+}
+
 public protocol ParameterizedRule: Rule {
-    typealias ParameterType
+    typealias ParameterType: Equatable
     init(parameters: [RuleParameter<ParameterType>])
     var parameters: [RuleParameter<ParameterType>] { get }
+}
+
+extension ParameterizedRule {
+    func isEqualTo(rule: Self) -> Bool {
+        return (self.dynamicType.description == rule.dynamicType.description) &&
+               (self.parameters == rule.parameters)
+    }
 }
 
 public protocol CorrectableRule: Rule {
     func correctFile(file: File) -> [Correction]
 }
+
+//// MARK: - == implementation
+//
+//public func == (lhs: Rule, rhs: Rule) -> Bool {
+//    return lhs.isEqualTo(rhs)
+//}
+//
+//public func == <T: ParameterizedRule>(lhs: T, rhs: T) -> Bool {
+//    return lhs.isEqualTo(rhs)
+//}

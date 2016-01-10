7fa100a033b2df1d32471701541ecaae69a7c4b4
Norio Nomura <norio.nomura@gmail.com>
2016-01-10 20:39:31 +0900

    Reduce calls to `String.characters.count` in `LineLengthRule.validateFile(_:)`
    By applying this, the duration of linting Carthage is reduced from:
    ```
    swiftlint lint  20.07s user 1.10s system 84% cpu 25.023 total
    ```
    to:
    ```
    swiftlint lint  18.27s user 1.05s system 84% cpu 22.769 total
    ```

--- Source/SwiftLintFramework/Rules/LineLengthRule.swift
+++ Source/SwiftLintFramework/Rules/LineLengthRule.swift
@@ -30,12 +30,13 @@ public struct LineLengthRule: ParameterizedRule {
 
     public func validateFile(file: File) -> [StyleViolation] {
         return file.lines.flatMap { line in
-            for param in parameters.reverse() where line.content.characters.count > param.value {
+            let length = line.content.characters.count
+            for param in parameters.reverse() where length > param.value {
                 return StyleViolation(ruleDescription: self.dynamicType.description,
                     severity: param.severity,
                     location: Location(file: file.path, line: line.index),
                     reason: "Line should be \(parameters.first!.value) characters or less: " +
-                    "currently \(line.content.characters.count) characters")
+                    "currently \(length) characters")
             }
             return nil
         }

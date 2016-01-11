a0a76d898f61221fbdfe3f448e916047b4202b3d
JP Simard <jp@jpsim.com>
2016-01-11 08:55:10 -0800

    fix escaped double quote character

--- Source/SwiftLintFramework/Reporters/CSVReporter.swift
+++ Source/SwiftLintFramework/Reporters/CSVReporter.swift
@@ -10,7 +10,7 @@ import Foundation
 
 extension String {
     private func escapedForCSV() -> String {
-        let escapedString = stringByReplacingOccurrencesOfString("\"", withString: "\"\"")
+        let escapedString = stringByReplacingOccurrencesOfString("\"", withString: "\\\"")
         if escapedString.containsString(",") || escapedString.containsString("\n") {
             return "\"\(escapedString)\""
         }

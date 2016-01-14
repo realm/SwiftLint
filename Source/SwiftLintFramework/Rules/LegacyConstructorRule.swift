f56875bb8794e4907894c2d9f8c6d5fcda9366f4
JP Simard <jp@jpsim.com>
2016-01-13 17:56:53 -0800

    fix spacing issues in LegacyConstructorRule

--- Source/SwiftLintFramework/Rules/LegacyConstructorRule.swift
+++ Source/SwiftLintFramework/Rules/LegacyConstructorRule.swift
@@ -55,12 +55,12 @@ public struct LegacyConstructorRule: CorrectableRule {
         let number = "([\\-0-9\\.]+)"
         let twoNumbers = "\(number)\\s*,\\s*\(number)"
         let patterns = [
-          "CGPointMake\\(\\s*\(twoNumbers)\\s*\\)": "CGPoint(x: $1, y: $2)",
-          "CGSizeMake\\(\\s*\(twoNumbers)\\s*\\)": "CGSize(width: $1, height: $2)",
-          "CGRectMake\\(\\s*\(twoNumbers)\\s*,\\s*\(twoNumbers)\\s*\\)":
-              "CGRect(x: $1, y: $2, width: $3, height: $4)",
-          "CGVectorMake\\(\\s*\(twoNumbers)\\s*\\)": "CGVector(dx: $1, dy: $2)",
-          "NSMakeRange\\(\\s*\(twoNumbers)\\s*\\)": "NSRange(location: $1, length: $2)",
+            "CGPointMake\\(\\s*\(twoNumbers)\\s*\\)": "CGPoint(x: $1, y: $2)",
+            "CGSizeMake\\(\\s*\(twoNumbers)\\s*\\)": "CGSize(width: $1, height: $2)",
+            "CGRectMake\\(\\s*\(twoNumbers)\\s*,\\s*\(twoNumbers)\\s*\\)":
+            "CGRect(x: $1, y: $2, width: $3, height: $4)",
+            "CGVectorMake\\(\\s*\(twoNumbers)\\s*\\)": "CGVector(dx: $1, dy: $2)",
+            "NSMakeRange\\(\\s*\(twoNumbers)\\s*\\)": "NSRange(location: $1, length: $2)",
         ]
 
         let description = self.dynamicType.description
@@ -72,10 +72,10 @@ public struct LegacyConstructorRule: CorrectableRule {
 
             let regularExpression = regex(pattern)
             for range in matches.reverse() {
-              contents = regularExpression.stringByReplacingMatchesInString(contents,
-                options: [], range: range, withTemplate: template)
-              let location = Location(file: file, characterOffset: range.location)
-              corrections.append(Correction(ruleDescription: description, location: location))
+                contents = regularExpression.stringByReplacingMatchesInString(contents,
+                    options: [], range: range, withTemplate: template)
+                let location = Location(file: file, characterOffset: range.location)
+                corrections.append(Correction(ruleDescription: description, location: location))
             }
         }
 

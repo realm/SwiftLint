f463b9ba40c66197b6adddcc89d2498d6548f2fb
Scott Hoyt <scott@wildonion.io>
2016-01-21 10:42:07 -0800

    Filled out implementation details for RegexConfig.setConfig().

--- Source/SwiftLintFramework/Rules/RuleConfigs/RegexConfig.swift
+++ Source/SwiftLintFramework/Rules/RuleConfigs/RegexConfig.swift
@@ -21,7 +21,22 @@ public struct RegexConfig: RuleConfig, Equatable {
     }
 
     public mutating func setConfig(config: AnyObject) throws {
-        //
+        guard let configDict = config as? [String: AnyObject] else {
+            throw ConfigurationError.UnknownConfiguration
+        }
+
+        if let message = configDict["message"] as? String {
+            self.message = message
+        }
+        if let regexString = configDict["regex"] as? String {
+            self.regex = try NSRegularExpression(pattern: regexString, options: [])
+        }
+        try [String].arrayOf(configDict["match_tokens"])?.forEach {
+            self.matchTokens.append(try SyntaxKind(nickname: $0))
+        }
+        if let severityString = configDict["severity"] as? String {
+            try severity.setConfig(severityString)
+        }
     }
 }
 
@@ -31,4 +46,53 @@ public func == (lhs: RegexConfig, rhs: RegexConfig) -> Bool {
         lhs.regex == rhs.regex &&
         lhs.matchTokens == rhs.matchTokens &&
         lhs.severity == rhs.severity
+}
+
+
+public extension SyntaxKind {
+    // swiftlint:disable:next function_body_length
+    init(nickname: Swift.String) throws {
+        switch nickname.lowercaseString {
+        case "argument":
+            self = .Argument
+        case "attribute_builtin":
+            self = .AttributeBuiltin
+        case "attribute_id":
+            self = .AttributeID
+        case "buildconfig_id":
+            self = .BuildconfigID
+        case "buildconfig_keyword":
+            self = .BuildconfigKeyword
+        case "comment":
+            self = .Comment
+        case "comment_mark":
+            self = .CommentMark
+        case "comment_url":
+            self = .CommentURL
+        case "doccomment":
+            self = .DocComment
+        case "doccomment_field":
+            self = .DocCommentField
+        case "identifier":
+            self = .Identifier
+        case "keyword":
+            self = .Keyword
+        case "number":
+            self = .Number
+        case "objectliteral":
+            self = .ObjectLiteral
+        case "parameter":
+            self = .Parameter
+        case "placeholder":
+            self = .Placeholder
+        case "string":
+            self = .String
+        case "string_interpolation_anchor":
+            self = .StringInterpolationAnchor
+        case "typeidentifier":
+            self = .Typeidentifier
+        default:
+            throw ConfigurationError.UnknownConfiguration
+        }
+    }
 }

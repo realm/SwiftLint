8cc5ba4f322eb91379654e6c3d9f88154c2ae235
Scott Hoyt <scott@wildonion.io>
2016-01-21 09:24:55 -0800

    Stub out CustomRules.

---
+++ Source/SwiftLintFramework/Rules/CustomRules.swift
@@ -0,0 +1,29 @@
+//
+//  CustomRules.swift
+//  SwiftLint
+//
+//  Created by Scott Hoyt on 1/21/16.
+//  Copyright © 2016 Realm. All rights reserved.
+//
+
+import Foundation
+import SourceKittenFramework
+import SwiftXPC
+
+public struct CustomRule: ASTRule {
+
+    public static let description = RuleDescription(
+        identifier: "custom_rules",
+        name: "Custom Rules",
+        description: "Create custom rules by providing a regex string. " +
+          "Optionally specify what syntax kinds to match against, the severity " +
+          "level, and what message to display")
+
+    public init() {}
+
+    public func validateFile(file: File,
+                             kind: SwiftDeclarationKind,
+                             dictionary: XPCDictionary) -> [StyleViolation] {
+        return []
+    }
+}

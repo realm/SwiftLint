2f60e0b8f185abeb4ab7e3c9100a50cce26a09d7
Scott Hoyt <scott@wildonion.io>
2016-01-25 10:02:55 -0800

    Removed unneeded rule disable.

--- Source/SwiftLintFramework/Extensions/SyntaxKind+SwiftLint.swift
+++ Source/SwiftLintFramework/Extensions/SyntaxKind+SwiftLint.swift
@@ -9,7 +9,6 @@
 import SourceKittenFramework
 
 extension SyntaxKind {
-    // swiftlint:disable:next function_body_length
     init(shortName: Swift.String) throws {
         let prefix = "source.lang.swift.syntaxtype."
         guard let kind = SyntaxKind(rawValue: prefix + shortName.lowercaseString) else {

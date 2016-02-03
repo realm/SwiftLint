267417e9a5278c5e95c45d2e5e238595c686cef8
Norio Nomura <norio.nomura@gmail.com>
2016-02-03 13:03:36 +0900

    [SPM] Add `Source/SwiftLintFrameworkTests/main.swift`
    That also added to `project.pbxproj` for browsing in Xcode's Project Navigator.
    That used only on building by SPM

---
+++ Source/SwiftLintFrameworkTests/main.swift
@@ -0,0 +1,23 @@
+//
+//  main.swift
+//  SwiftLint
+//
+//  Created by 野村 憲男 on 2/3/16.
+//  Copyright © 2016 Realm. All rights reserved.
+//
+
+import XCTest
+
+XCTMain([
+    ConfigurationTests(),
+    CustomRulesTests(),
+    ExtendedNSStringTests(),
+    FunctionBodyLengthRuleTests(),
+    IntegrationTests(),
+    ReporterTests(),
+    RuleConfigurationsTests(),
+    RulesTests(),
+    RuleTests(),
+    YamlSwiftLintTests(),
+    YamlParserTests(),
+    ])

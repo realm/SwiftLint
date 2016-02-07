9b55b8923a4982220e715aea8229489f2db2c4a0
JP Simard <jp@jpsim.com>
2016-02-06 16:38:32 -0800

    move assertCorrection into Configuration extension
    to not clog up verifyRule, making it harder to read.

--- Source/SwiftLintFrameworkTests/TestHelpers.swift
+++ Source/SwiftLintFrameworkTests/TestHelpers.swift
@@ -23,6 +23,34 @@ func violations(string: String, config: Configuration = Configuration()) -> [Sty
     return Linter(file: file, configuration: config).styleViolations
 }
 
+extension Configuration {
+    private func assertCorrection(before: String, expected: String) {
+        guard let path = NSURL(fileURLWithPath: NSTemporaryDirectory(), isDirectory: true)
+            .URLByAppendingPathComponent(NSUUID().UUIDString + ".swift").path else {
+                XCTFail("couldn't generate temporary path for assertCorrection()")
+                return
+        }
+        if before.dataUsingEncoding(NSUTF8StringEncoding)?
+            .writeToFile(path, atomically: true) != true {
+                XCTFail("couldn't write to file for assertCorrection()")
+                return
+        }
+        guard let file = File(path: path) else {
+            XCTFail("couldn't read file at path '\(path)' for assertCorrection()")
+            return
+        }
+        let corrections = Linter(file: file, configuration: self).correct()
+        XCTAssertEqual(corrections.count, Int(before != expected))
+        XCTAssertEqual(file.contents, expected)
+        do {
+            let corrected = try NSString(contentsOfFile: path, encoding: NSUTF8StringEncoding)
+            XCTAssertEqual(corrected as String, expected)
+        } catch {
+            XCTFail("couldn't read file at path '\(path)': \(error)")
+        }
+    }
+}
+
 extension String {
     private func toStringLiteral() -> String {
         return "\"" + stringByReplacingOccurrencesOfString("\n", withString: "\\n") + "\""
@@ -33,33 +61,6 @@ extension XCTestCase {
     func verifyRule(ruleDescription: RuleDescription, commentDoesntViolate: Bool = true,
                     stringDoesntViolate: Bool = true) {
         let config = Configuration(whitelistRules: [ruleDescription.identifier])!
-
-        func assertCorrection(before: String, expected: String) {
-            guard let path = NSURL(fileURLWithPath: NSTemporaryDirectory(), isDirectory: true)
-                .URLByAppendingPathComponent(NSUUID().UUIDString + ".swift").path else {
-                    XCTFail("couldn't generate temporary path for assertCorrection()")
-                    return
-            }
-            if before.dataUsingEncoding(NSUTF8StringEncoding)?
-                .writeToFile(path, atomically: true) != true {
-                XCTFail("couldn't write to file for assertCorrection()")
-                return
-            }
-            guard let file = File(path: path) else {
-                XCTFail("couldn't read file at path '\(path)' for assertCorrection()")
-                return
-            }
-            let corrections = Linter(file: file, configuration: config).correct()
-            XCTAssertEqual(corrections.count, Int(before != expected))
-            XCTAssertEqual(file.contents, expected)
-            do {
-                let corrected = try NSString(contentsOfFile: path, encoding: NSUTF8StringEncoding)
-                XCTAssertEqual(corrected as String, expected)
-            } catch {
-                XCTFail("couldn't read file at path '\(path)': \(error)")
-            }
-        }
-
         let triggers = ruleDescription.triggeringExamples
         let nonTriggers = ruleDescription.nonTriggeringExamples
 
@@ -98,9 +99,9 @@ extension XCTestCase {
         XCTAssert(triggers.flatMap({ violations(command + $0, config: config) }).isEmpty)
 
         // corrections
-        ruleDescription.corrections.forEach(assertCorrection)
+        ruleDescription.corrections.forEach(config.assertCorrection)
         // make sure strings that don't trigger aren't corrected
-        zip(nonTriggers, nonTriggers).forEach(assertCorrection)
+        zip(nonTriggers, nonTriggers).forEach(config.assertCorrection)
     }
 
     func checkError<T: protocol<ErrorType, Equatable>>(error: T, closure: () throws -> () ) {

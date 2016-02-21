fa78d39ff8df997d9531a282d862bd739c1f3678
Norio Nomura <norio.nomura@gmail.com>
2016-02-21 22:54:33 +0900

    [SPM] Update SourceKitten to 0.11.0

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
       ]),
   ],
   dependencies: [
-    .Package(url: "https://github.com/jpsim/SourceKitten.git", majorVersion: 0, minor: 10),
+    .Package(url: "https://github.com/jpsim/SourceKitten.git", majorVersion: 0, minor: 11),
     .Package(url: "https://github.com/behrang/YamlSwift.git", majorVersion: 1),
     .Package(url: "https://github.com/norio-nomura/swift-corelibs-xctest.git", majorVersion: 0),
     .Package(url: "https://github.com/scottrhoyt/SwiftyTextTable.git", majorVersion: 0, minor: 2),

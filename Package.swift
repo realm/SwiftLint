070bbb87ddad449ca9c790e49b03b500564411a3
Norio Nomura <norio.nomura@gmail.com>
2016-03-18 00:10:05 +0900

    [SPM] Update `Package.swift`

--- Package.swift
+++ Package.swift
@@ -8,15 +8,10 @@ let package = Package(
       dependencies: [
         .Target(name: "SwiftLintFramework")
       ]),
-    Target(name: "SwiftLintFrameworkTests",
-      dependencies: [
-        .Target(name: "SwiftLintFramework")
-      ]),
   ],
   dependencies: [
     .Package(url: "https://github.com/jpsim/SourceKitten.git", majorVersion: 0, minor: 11),
     .Package(url: "https://github.com/behrang/YamlSwift.git", majorVersion: 1),
-    .Package(url: "https://github.com/norio-nomura/swift-corelibs-xctest.git", majorVersion: 0),
     .Package(url: "https://github.com/scottrhoyt/SwiftyTextTable.git", majorVersion: 0, minor: 2),
   ]
 )

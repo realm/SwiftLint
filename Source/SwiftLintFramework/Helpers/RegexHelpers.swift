20ef756c4077693499b5075d6d134f69fa99e121
Blaise Sarr <sarrblaise@gmail.com>
2016-04-13 11:06:50 +0200

    Add RegexHelpers file
     - This will be used to centralize common used regex and avoid duplicate them

---
+++ Source/SwiftLintFramework/Helpers/RegexHelpers.swift
@@ -0,0 +1,27 @@
+//
+//  RegexHelpers.swift
+//  SwiftLint
+//
+//  Created by Blaise Sarr on 13/04/16.
+//  Copyright © 2016 Realm. All rights reserved.
+//
+
+struct RegexHelpers {
+    // A single variable
+    static let varName = "[a-zA-Z_][a-zA-Z0-9_]+"
+
+    // A single variable in a group (capturable)
+    static let varNameGroup = "\\s*(\(varName))\\s*"
+
+    // Two variables (capturables)
+    static let twoVars = "\(varNameGroup),\(varNameGroup)"
+
+    // A number
+    static let number = "[\\-0-9\\.]+"
+
+    // A variable or a number (capturable)
+    static let variableOrNumber = "\\s*(\(varName)|\(number))\\s*"
+
+    // Two 'variable or number'
+    static let twoVariableOrNumber = "\(variableOrNumber),\(variableOrNumber)"
+}

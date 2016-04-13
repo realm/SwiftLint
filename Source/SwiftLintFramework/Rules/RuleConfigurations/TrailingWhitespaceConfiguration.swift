4df2a627abf6fe05f591be5d4d64107888d9f6f5
Reimar Twelker <Twelker@adesso-mobile.de>
2016-04-13 09:32:32 +0200

    Added a `RuleConfiguration` subclass specifically for the trailing whitespace rule.
    Its `ignoresEmptyLines` flag controls whether the rule should ignore whitespace-indented empty lines when detecting violations and correcting files. By default, the flag is false.

---
+++ Source/SwiftLintFramework/Rules/RuleConfigurations/TrailingWhitespaceConfiguration.swift
@@ -0,0 +1,46 @@
+//
+//  TrailingWhitespaceConfiguration.swift
+//  SwiftLint
+//
+//  Created by Reimar Twelker on 12.04.16.
+//  Copyright © 2016 Realm. All rights reserved.
+//
+
+import Foundation
+
+public struct TrailingWhitespaceConfiguration: RuleConfiguration, Equatable {
+    var severityConfiguration = SeverityConfiguration(.Warning)
+    var ignoresEmptyLines = false
+    
+    public var consoleDescription: String {
+        return "ignores empty lines: \(ignoresEmptyLines ? "true" : "false")"
+    }
+
+    public init() {
+        self.init(ignoresEmptyLines: false)
+    }
+
+    public init(ignoresEmptyLines: Bool) {
+        self.ignoresEmptyLines = ignoresEmptyLines
+    }
+
+    public mutating func applyConfiguration(configuration: AnyObject) throws {
+        guard let configuration = configuration as? [String: AnyObject] else {
+            throw ConfigurationError.UnknownConfiguration
+        }
+
+        if let ignoresEmptyLinesString = configuration["ignores_empty_lines"] as? String {
+            ignoresEmptyLines = (ignoresEmptyLinesString == "true")
+        } else {
+            ignoresEmptyLines = false
+        }
+        
+        if let severityString = configuration["severity"] as? String {
+            try severityConfiguration.applyConfiguration(severityString)
+        }
+    }
+}
+
+public func == (lhs: TrailingWhitespaceConfiguration, rhs: TrailingWhitespaceConfiguration) -> Bool {
+    return lhs.ignoresEmptyLines == rhs.ignoresEmptyLines
+}

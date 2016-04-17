cf91026c88d785ca39f38d93cadf2560963e24a3
Norio Nomura <norio.nomura@gmail.com>
2016-04-17 11:04:15 +0900

    Fix `LegacyConstructorRule`
    #623

--- Source/SwiftLintFramework/Rules/LegacyConstructorRule.swift
+++ Source/SwiftLintFramework/Rules/LegacyConstructorRule.swift
@@ -72,18 +72,21 @@ public struct LegacyConstructorRule: CorrectableRule, ConfigurationProviderRule 
         var corrections = [Correction]()
         var contents = file.contents
 
-        for (pattern, template) in patterns {
-            let matches = file.matchPattern(pattern)
-                .filter({ $0.1.first == .Identifier })
-                .map({ $0.0 })
-
-            let regularExpression = regex(pattern)
-            for range in matches.reverse() {
-                contents = regularExpression.stringByReplacingMatchesInString(contents,
-                    options: [], range: range, withTemplate: template)
-                let location = Location(file: file, characterOffset: range.location)
-                corrections.append(Correction(ruleDescription: description, location: location))
+        let matches = patterns.map {
+                (pattern, template) -> [(NSRange, String, String)] in
+                let matches = file.matchPattern(pattern)
+                    .filter { $0.1.first == .Identifier }
+                    .map { ($0.0, pattern, template) }
+                return matches
             }
+            .flatten()
+            .sort { $0.0.location > $1.0.location } // reversed
+
+        for (range, pattern, template) in matches {
+            contents = regex(pattern).stringByReplacingMatchesInString(contents,
+                options: [], range: range, withTemplate: template)
+            let location = Location(file: file, characterOffset: range.location)
+            corrections.append(Correction(ruleDescription: description, location: location))
         }
 
         file.write(contents)

27aa8e338381179e61986a98aa1509908a63d7eb
Cristian Filipov <filipov@gmail.com>
2016-08-18 16:44:21 -0700

    Use path arg as root for include/exclude paths

--- Source/SwiftLintFramework/Extensions/NSFileManager+SwiftLint.swift
+++ Source/SwiftLintFramework/Extensions/NSFileManager+SwiftLint.swift
@@ -9,8 +9,8 @@
 import Foundation
 
 extension NSFileManager {
-    internal func filesToLintAtPath(path: String) -> [String] {
-        let absolutePath = path.absolutePathStandardized()
+    internal func filesToLintAtPath(path: String, rootDirectory: String? = nil) -> [String] {
+        let absolutePath = (path.absolutePathRepresentation(rootDirectory ?? NSFileManager.defaultManager().currentDirectoryPath) as NSString).stringByStandardizingPath
         var isDirectory: ObjCBool = false
         guard fileExistsAtPath(absolutePath, isDirectory: &isDirectory) else {
             return []

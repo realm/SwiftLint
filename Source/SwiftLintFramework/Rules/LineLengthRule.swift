c6e2a621dd8201e729140facff92be96fda7b4a3
Jonas Wippermann <jonas@vfuc.co>
2016-10-13 21:30:16 +0200

    Extract literal stripping into function, add image literal stripping

--- Source/SwiftLintFramework/Rules/LineLengthRule.swift
+++ Source/SwiftLintFramework/Rules/LineLengthRule.swift
@@ -19,11 +19,13 @@ public struct LineLengthRule: ConfigurationProviderRule, SourceKitFreeRule {
         description: "Lines should not span too many characters.",
         nonTriggeringExamples: [
             Repeat(count: 100, repeatedValue: "/").joinWithSeparator("") + "\n",
-            Repeat(count: 100, repeatedValue: "#colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1)").joinWithSeparator("") + "\n"
+            Repeat(count: 100, repeatedValue: "#colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1)").joinWithSeparator("") + "\n",
+            Repeat(count: 100, repeatedValue: "#imageLiteral(resourceName: \"image.jpg\")").joinWithSeparator("") + "\n"
         ],
         triggeringExamples: [
             Repeat(count: 101, repeatedValue: "/").joinWithSeparator("") + "\n",
-            Repeat(count: 101, repeatedValue: "#colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1)").joinWithSeparator("") + "\n"
+            Repeat(count: 101, repeatedValue: "#colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1)").joinWithSeparator("") + "\n",
+            Repeat(count: 101, repeatedValue: "#imageLiteral(resourceName: \"image.jpg\")").joinWithSeparator("") + "\n"
         ]
     )
 
@@ -37,22 +39,13 @@ public struct LineLengthRule: ConfigurationProviderRule, SourceKitFreeRule {
                 return nil
             }
 
-            var string = line.content //Mutable copy of content string
-            // While copy of content contains color literal, replace with a single character
-            while string.containsString("#colorLiteral(") {
-                if let rangeStart = string.rangeOfString("#colorLiteral("),
-                    let rangeEnd = string.rangeOfString(")",
-                        options: .LiteralSearch,
-                        range: rangeStart.startIndex..<string.endIndex,
-                        locale: nil) {
-                    string.replaceRange(rangeStart.startIndex..<rangeEnd.endIndex, with: "#")
+            var strippedString = line.content
+            strippedString = stripLiterals(fromSourceString: strippedString,
+                withDelimiter: "#colorLiteral")
+            strippedString = stripLiterals(fromSourceString: strippedString,
+                withDelimiter: "#imageLiteral")
 
-                } else { // Should never be the case, but break to avoid accidental infinity loop
-                    break
-                }
-            }
-            
-            let length = string.characters.count
+            let length = strippedString.characters.count
 
             for param in configuration.params where length > param.value {
                 return StyleViolation(ruleDescription: self.dynamicType.description,
@@ -64,4 +57,34 @@ public struct LineLengthRule: ConfigurationProviderRule, SourceKitFreeRule {
             return nil
         }
     }
+
+    /// Takes a string and replaces any literals specified by the `delimiter` parameter with `#`
+    ///
+    /// - parameter sourceString: Original string, possibly containing literals
+    /// - parameter delimiter:    Delimiter of the literal
+    ///     (characters before the parentheses, e.g. `#colorLiteral`)
+    ///
+    /// - returns: sourceString with the given literals replaced by `#`
+    private func stripLiterals(fromSourceString sourceString: String,
+                                                withDelimiter delimiter: String) -> String {
+        var modifiedString = sourceString
+
+        // While copy of content contains literal, replace with a single character
+        while modifiedString.containsString("\(delimiter)(") {
+            if let rangeStart = modifiedString.rangeOfString("\(delimiter)("),
+                let rangeEnd = modifiedString.rangeOfString(")",
+                                                            options: .LiteralSearch,
+                                                            range:
+                    rangeStart.startIndex..<modifiedString.endIndex,
+                                                            locale: nil) {
+                modifiedString.replaceRange(rangeStart.startIndex..<rangeEnd.endIndex, with: "#")
+
+            } else { // Should never be the case, but break to avoid accidental infinity loop
+                break
+            }
+        }
+        
+        return modifiedString
+    }
+    
 }

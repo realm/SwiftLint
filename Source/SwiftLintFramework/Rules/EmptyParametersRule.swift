3262e70e1a6045b9914c2d5137de96f4112db3c2
Marcelo Fabri <marcelofabrimf@gmail.com>
2016-12-12 20:56:12 -0200

    Excluding curried functions

--- Source/SwiftLintFramework/Rules/EmptyParametersRule.swift
+++ Source/SwiftLintFramework/Rules/EmptyParametersRule.swift
@@ -21,18 +21,26 @@ public struct EmptyParametersRule: Rule, ConfigurationProviderRule {
         nonTriggeringExamples: [
             "let abc: () -> Void = {}\n",
             "func foo(completion: () -> Void)\n",
-            "func foo(completion: () thows -> Void)\n"
+            "func foo(completion: () thows -> Void)\n",
+            "let foo: (ConfigurationTests) -> Void throws -> Void)\n",
+            "let foo: (ConfigurationTests) ->   Void throws -> Void)\n",
+            "let foo: (ConfigurationTests) ->Void throws -> Void)\n"
         ],
         triggeringExamples: [
             "let abc: ↓Void -> Void = {}\n",
             "func foo(completion: ↓Void -> Void)\n",
-            "func foo(completion: ↓Void throws -> Void)\n"
+            "func foo(completion: ↓Void throws -> Void)\n",
+            "let foo: ↓Void -> () throws -> Void)\n"
         ]
     )
 
     public func validateFile(_ file: File) -> [StyleViolation] {
         let kinds = SyntaxKind.commentAndStringKinds()
-        return file.matchPattern("Void\\s*(throws\\s+)?->", excludingSyntaxKinds: kinds).map {
+        let pattern = "Void\\s*(throws\\s+)?->"
+        let excludingPattern = "->\\s*" + pattern // excludes curried functions
+        return file.matchPattern(pattern,
+                                 excludingSyntaxKinds: kinds,
+                                 excludingPattern: excludingPattern).map {
             StyleViolation(ruleDescription: type(of: self).description,
                            severity: configuration.severity,
                            location: Location(file: file, characterOffset: $0.location))

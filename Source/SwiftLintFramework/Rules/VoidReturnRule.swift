7af7eb8fbef10526d4ff1f9d9fd792b78785867e
Marcelo Fabri <marcelofabrimf@gmail.com>
2016-12-13 11:57:43 -0200

    Handling curried declarations

--- Source/SwiftLintFramework/Rules/VoidReturnRule.swift
+++ Source/SwiftLintFramework/Rules/VoidReturnRule.swift
@@ -21,25 +21,26 @@ public struct VoidReturnRule: Rule, ConfigurationProviderRule {
         nonTriggeringExamples: [
             "let abc: () -> Void = {}\n",
             "func foo(completion: () -> Void)\n",
-            "func foo: (ConfigurationTests) -> () throws -> Void)\n",
-            "func foo: (ConfigurationTests) ->   () throws -> Void)\n",
-            "func foo: (ConfigurationTests) ->() throws -> Void)\n",
-            "func foo: (ConfigurationTests) -> () -> Void)\n"
+            "let foo: (ConfigurationTests) -> () throws -> Void)\n",
+            "let foo: (ConfigurationTests) ->   () throws -> Void)\n",
+            "let foo: (ConfigurationTests) ->() throws -> Void)\n",
+            "let foo: (ConfigurationTests) -> () -> Void)\n"
         ],
         triggeringExamples: [
             "let abc: () -> ↓() = {}\n",
             "func foo(completion: () -> ↓())\n",
-            "func foo(completion: () -> ↓(   ))\n"
+            "func foo(completion: () -> ↓(   ))\n",
+            "let foo: (ConfigurationTests) -> () throws -> ↓())\n"
         ]
     )
 
     public func validateFile(_ file: File) -> [StyleViolation] {
         let kinds = SyntaxKind.commentAndStringKinds()
         let pattern = "->\\s*\\(\\s*\\)\\s*(?!->)"
-        let excludingPattern = pattern + "\\s*(throws\\s+)?->"
+        let excludingPattern = "(\(pattern))\\s*(throws\\s+)?->"
 
         return file.matchPattern(pattern, excludingSyntaxKinds: kinds,
-                                 excludingPattern: excludingPattern).flatMap {
+                                 excludingPattern: excludingPattern) { $0.rangeAt(1) }.flatMap {
 
             let range = file.contents.bridge().substring(with: $0).bridge().range(of: "(")
             guard range.location != NSNotFound else {

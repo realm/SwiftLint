9d42a73d26beeb820f795f97a20dc4dd1954d6f3
Marcelo Fabri <marcelofabrimf@gmail.com>
2016-12-27 17:22:56 -0200

    Add more tests

--- Tests/SwiftLintFrameworkTests/LinterCacheTests.swift
+++ Tests/SwiftLintFrameworkTests/LinterCacheTests.swift
@@ -31,4 +31,44 @@ class LinterCacheTests: XCTestCase {
         let linterCache = try? LinterCache(cache: cache, currentVersion: Version(value: "0.2.0"))
         XCTAssertNotNil(linterCache)
     }
+
+    func testParsesViolations() {
+        var cache = LinterCache(currentVersion: Version(value: "0.2.0"))
+        let file = "foo.swift"
+        let ruleDescription = RuleDescription(identifier: "rule", name: "Some rule",
+                                              description: "Validates stuff")
+        let violations = [
+            StyleViolation(ruleDescription: ruleDescription,
+                           severity: .warning,
+                           location: Location(file: file, line: 10, character: 2),
+                           reason: "Something is not right."),
+            StyleViolation(ruleDescription: ruleDescription,
+                           severity: .error,
+                           location: Location(file: file, line: 5, character: nil),
+                           reason: "Something is wrong.")
+        ]
+
+        cache.cacheFile(file, violations: violations, hash: 1)
+        let cachedViolations = cache.violations(for: file, hash: 1)
+
+        XCTAssertNotNil(cachedViolations)
+        XCTAssertEqual(cachedViolations!, violations)
+    }
+
+    func testParsesViolationsWithModifiedHash() {
+        var cache = LinterCache(currentVersion: Version(value: "0.2.0"))
+        let file = "foo.swift"
+        cache.cacheFile(file, violations: [], hash: 1)
+        let cachedViolations = cache.violations(for: file, hash: 2)
+
+        XCTAssertNil(cachedViolations)
+    }
+
+    func testParsesViolationsWithEmptyViolations() {
+        let cache = LinterCache(currentVersion: Version(value: "0.2.0"))
+        let file = "foo.swift"
+        let cachedViolations = cache.violations(for: file, hash: 2)
+
+        XCTAssertNil(cachedViolations)
+    }
 }

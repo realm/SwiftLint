5282134feca20d4f2a3d07bf6773f93da960d32b
Marcelo Fabri <marcelofabrimf@gmail.com>
2017-01-02 03:25:04 -0200

    Remove unnecessary import

--- Source/SwiftLintFramework/Rules/RuleConfigurations/NumberSeparatorConfiguration.swift
+++ Source/SwiftLintFramework/Rules/RuleConfigurations/NumberSeparatorConfiguration.swift
@@ -6,8 +6,6 @@
 //  Copyright © 2017 Realm. All rights reserved.
 //
 
-import Foundation
-
 public struct NumberSeparatorConfiguration: RuleConfiguration, Equatable {
     private(set) var severityConfiguration = SeverityConfiguration(.warning)
     private(set) var minimumLength: Int

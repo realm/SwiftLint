3294e5cbab8ee95a435588aaa8185879fe14dd58
JP Simard <jp@jpsim.com>
2017-01-20 09:58:38 -0800

    allow forcing Swift version via SWIFTLINT_SWIFT_VERSION

--- Source/SwiftLintFramework/Models/SwiftVersion.swift
+++ Source/SwiftLintFramework/Models/SwiftVersion.swift
@@ -13,6 +13,14 @@ enum SwiftVersion {
     case three
 
     static let current: SwiftVersion = {
+        // Allow forcing the Swift version, useful in cases where SourceKit isn't available
+        if let envVersion = ProcessInfo.processInfo.environment["SWIFTLINT_SWIFT_VERSION"] {
+            if envVersion == "2" {
+                return .two
+            } else {
+                return .three
+            }
+        }
         let file = File(contents: "#sourceLocation()")
         let kinds = file.syntaxMap.tokens.flatMap { SyntaxKind(rawValue: $0.type) }
         if kinds == [.identifier] {

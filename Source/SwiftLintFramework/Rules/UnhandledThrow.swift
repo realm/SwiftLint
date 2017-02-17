e23186e9645f6569b2ddccdd004e1609f18fdc9c
Arthur Ariel Sabintsev <arthur@sabintsev.com>
2017-02-17 00:53:29 -0500

    Tests now pass!

--- Source/SwiftLintFramework/Rules/UnhandledThrow.swift
+++ Source/SwiftLintFramework/Rules/UnhandledThrow.swift
@@ -17,19 +17,36 @@ public struct UnhandledThrowRule: OptInRule, ConfigurationProviderRule {
     public static let description = RuleDescription(
         identifier: "unhandled_throw",
         name: "Unhandled Throw",
-        description: "When a throwing function does not handle a throw, the `throws` keyword can be removed.",
+        description: "When a throwing function does not explicitly handle a throw, the `throws` keyword can be removed.",
         nonTriggeringExamples: [
             "func f() throws {\n throw anError \n}\n",
-            "func f() throws -> Any {\n throw anError \n}\n"
+            "func f() throws { throw anError }\n",
+            "func f() throws -> Any {\n throw anError \n}\n",
+            "func f() throws -> Any { throw anError }\n"
         ],
         triggeringExamples: [
+            "func f() throws {}\n",
+            "func f() throws { }\n",
+            "func f() throws {\n}\n",
             "func f() throws {\n \n}\n",
-            "func f() throws -> Any {\n \n}\n"
+            "func f() throws -> Any {}\n",
+            "func f() throws -> Any { }\n",
+            "func f() throws -> Any {\n}\n",
+            "func f() throws -> Any {\n \n}\n",
+            "func f() throws { var x = 0 }\n",
+            "func f() throws {\n var x = 0 \n}\n",
+            "func f() throws -> Any { var x = 0 }\n",
+            "func f() throws -> Any {\n var x = 0 \n}\n"
         ]
     )
 
     public func validate(file: File) -> [StyleViolation] {
-        let matches = file.match(pattern: "(func).+(throws).+(throw)\\s", excludingSyntaxKinds: [])
+        let pattern = "(func).+(throws).+"
+        let excludingPattern = "(throw)\\s\\S"
+
+        let matches = file.match(pattern: pattern,
+                                 excludingSyntaxKinds: SyntaxKind.commentAndStringKinds(),
+                                 excludingPattern: excludingPattern)
 
         return matches.map {
             StyleViolation(ruleDescription: type(of: self).description,

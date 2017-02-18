97e72bc66581dc3d4a47c10f31440f1b0c689543
Arthur Ariel Sabintsev <arthur@sabintsev.com>
2017-02-18 00:35:18 -0500

    Handled protocol and do catch scenarios

--- Source/SwiftLintFramework/Rules/UnhandledThrow.swift
+++ Source/SwiftLintFramework/Rules/UnhandledThrow.swift
@@ -20,42 +20,43 @@ public struct UnhandledThrowRule: Rule, ConfigurationProviderRule {
         name: "Unhandled Throw",
         description: "When a throwing function does not explicitly handle a throw, the `throws` keyword can be removed.",
         nonTriggeringExamples: [
-            "func f() throws {\n throw anError \n}\n",
-            "func f() throws { throw anError }\n",
-            "func f() throws -> Any {\n throw anError \n}\n",
-            "func f() throws -> Any { throw anError }\n",
-            "func f() throws {\n try anExpression \n}\n",
-            "func f() throws { try anExpression }\n",
-            "func f() throws -> Any {\n try anExpression \n}\n",
-            "func f() throws -> Any { try anExpression }\n",
-            "func f() throws {\n try? anExpression \n}\n",
-            "func f() throws { try? anExpression }\n",
-            "func f() throws -> Any {\n try? anExpression \n}\n",
-            "func f() throws -> Any { try? anExpression }\n",
-            "func f() throws {\n try! anExpression \n}\n",
-            "func f() throws { try! anExpression }\n",
-            "func f() throws -> Any {\n try! anExpression \n}\n",
-            "func f() throws -> Any { try! anExpression }\n"
+            "func f() throws",
+            "func f() throws -> Any",
+            "func f() throws {\n throw anException \n}",
+            "func f() throws -> Any {\n throw anException \n}",
+            "func f() throws {\n return try anException \n}",
+            "func f() throws -> Any {\n return try anException \n}",
+            "func f() throws {\n return try? anException \n}",
+            "func f() throws -> Any {\n return try? anException \n}",
+            "func f() throws {\n return try! anException \n}",
+            "func f() throws { return try! anException }",
+            "func f() throws -> Any {\n return try! anException \n}",
+            "func f() throws {\n do {\n try b() \n} catch {\n throw b \n} \n}",
+            "func f() throws -> Any {\n do {\n try b() \n} catch {\n throw anException \n} \n}",
+            "func f() throws {\n do {\n return try b() \n} catch {\n throw b \n} \n}",
+            "func f() throws -> Any {\n do {\n return try b() \n} catch {\n throw anException \n} \n}"
         ],
         triggeringExamples: [
-            "func f() throws {}\n",
-            "func f() throws { }\n",
-            "func f() throws {\n}\n",
-            "func f() throws {\n \n}\n",
-            "func f() throws -> Any {}\n",
-            "func f() throws -> Any { }\n",
-            "func f() throws -> Any {\n}\n",
-            "func f() throws -> Any {\n \n}\n",
-            "func f() throws { var x = 0 }\n",
-            "func f() throws {\n var x = 0 \n}\n",
-            "func f() throws -> Any { var x = 0 }\n",
-            "func f() throws -> Any {\n var x = 0 \n}\n"
+            "func f() throws {}",
+            "func f() throws { }",
+            "func f() throws {\n}",
+            "func f() throws {\n \n}",
+            "func f() throws -> Any {}",
+            "func f() throws -> Any { }",
+            "func f() throws -> Any {\n}",
+            "func f() throws -> Any {\n \n}",
+            "func f() throws { var x = 0 }",
+            "func f() throws {\n var x = 0 \n}",
+            "func f() throws -> Any { var x = 0 }",
+            "func f() throws -> Any {\n var x = 0 \n}",
+            "func f() throws {\n do {\n try b() \n} catch {\n \n} \n}",
+            "func f() throws -> Any{\n do {\n try b() \n} catch {\n \n} \n}"
         ]
     )
 
     public func validate(file: File) -> [StyleViolation] {
-        let pattern = "(func).+(throws).+"
-        let excludingPattern = "(throw|try|try\\!|try\\?)\\s\\S.+\\}"
+        let pattern = "(func).+(throws)"
+        let excludingPattern = "((func).+(throws)\\b$|(func).+(throws)\\b.+[\\-\\>].+?\\b$|(func).+(throws)\\b.+(throw)\\s\\S.+?$|(func).+(throws)\\b.+(try).+[\\{].+(throw)\\s\\S.+?$|(func).+(throws)\\b.+(return)\\s(try).+$)"
 
         let matches = file.match(pattern: pattern,
                                  excludingSyntaxKinds: SyntaxKind.commentAndStringKinds(),

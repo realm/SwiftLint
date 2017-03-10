0136c9101620e0ec02600d65fadfe04ec65dff13
jeffreybergier <jeffburg@jeffburg.com>
2017-03-09 22:14:32 -0800

    add opt-in IBOutlet Setter Rule
    This rule checks if the file contains IBOutlets, if it does it looks for any
    lines of code that attempt to call the setter on that outlet.
    e.g. self.myLabel = UILabel() would trigger this rule.
    
    While the rule is not perfect, it will help avoid an Interface Builder
    anti- pattern. In general, if your code is resetting IBOutlets, there is
    probably a better way to accomplish what you want.
    
    There are a lot of triggering and non-triggering test cases. Along with a 3
    TODOs where the rule can be triggered when it shouldn't. Because this is opt-in
    and the cases where the rule is triggered are fairly rare and can often be fixed
    by renaming a local variable, I left them as TODOs for now.

---
+++ Source/SwiftLintFramework/Rules/OutletSetterRule.swift
@@ -0,0 +1,116 @@
+//
+//  OutletSetterRule.swift
+//  SwiftLint
+//
+//  Created by Jeffrey Bergier on 3/9/17.
+//  Copyright © 2017 Realm. All rights reserved.
+//
+
+import Foundation
+import SourceKittenFramework
+
+public struct OutletSetterRule: ASTRule, OptInRule, ConfigurationProviderRule {
+
+    public var configuration = SeverityConfiguration(.warning)
+
+    public init() {}
+
+    public static let description = RuleDescription(
+        identifier: "outlet_setter",
+        name: "Outlet Setter",
+        description: "@IBOutlet properties should only be set by Interface Builder, not in code.",
+        nonTriggeringExamples: [
+            "class Foo {\n @IBOutlet var bar: UIView?\n func fubar() {\n \t let \t bar \t  = UILabel()\n }\n }",
+            "class Foo {\n @IBOutlet var bar: UIView?\n func fubar() {\n \t let \t bar=UILabel()\n }\n }",
+            "class Foo {\n @IBOutlet var bar: UIView?\n func fubar() {\n \t var \t bar \t  = UILabel()\n }\n }",
+            "class Foo {\n @IBOutlet var bar: UIView?\n func fubar() {\n \t var \t bar=UILabel()\n }\n }",
+            "class Foo {\n @IBOutlet var bar: UIView?\n func fubar() {\n \t if case \t .bar \t = bar {} \n }\n }",
+            "class Foo {\n @IBOutlet var bar: UIView?\n func fubar() {\n \t if case \t .bar=bar {} \n }\n }",
+            "class Foo {\n @IBOutlet var bar: UIView?\n func fubar() {\n \t 123bar \t  = UILabel()\n }\n }",
+            "class Foo {\n @IBOutlet var bar: UIView?\n func fubar() {\n \t 123bar=UILabel()\n }\n }",
+            "class Foo {\n @IBOutlet var bar: UIView?\n func fubar() {\n \t XYZbar \t  = UILabel()\n }\n }",
+            "class Foo {\n @IBOutlet var bar: UIView?\n func fubar() {\n \t XYZbar=UILabel()\n }\n }",
+            "class Foo {\n @IBOutlet var bar: UIView?\n func fubar() {\n \t XYZ123bar \t  = UILabel()\n }\n }",
+            "class Foo {\n @IBOutlet var bar: UIView?\n func fubar() {\n \t XYZ123bar=UILabel()\n }\n }",
+            "class Foo {\n @IBOutlet var bar: UIView?\n func fubar() {\n \t self.bar?.backgroundColor = .blue\n }\n }",
+            "class Foo {\n @IBOutlet var bar: UIView?\n func fubar() {\n \t self.bar?.backgroundColor=.blue\n }\n }",
+            "class Foo {\n @IBOutlet var bar: UIView?\n func fubar() {\n \t self!.bar?.backgroundColor = .blue\n }\n }",
+            "class Foo {\n @IBOutlet var bar: UIView?\n func fubar() {\n \t self!.bar?.backgroundColor=.blue\n }\n }",
+            "class Foo {\n @IBOutlet var bar: UIView?\n func fubar() {\n \t self?.bar?.backgroundColor = .blue\n }\n }",
+            "class Foo {\n @IBOutlet var bar: UIView?\n func fubar() {\n \t self?.bar?.backgroundColor=.blue\n }\n }",
+            "class Foo {\n var bar: UIView?\n func fubar() {\n \t self.bar \t = UILabel()\n }\n }",
+            "class Foo {\n var bar: UIView?\n func fubar() {\n \t self.bar=UILabel()\n }\n }",
+            "class Foo {\n var bar: UIView?\n func fubar() {\n \t self!.bar \t = UILabel()\n }\n }",
+            "class Foo {\n var bar: UIView?\n func fubar() {\n \t self!.bar=UILabel()\n }\n }",
+            "class Foo {\n var bar: UIView?\n func fubar() {\n \t self?.bar \t = UILabel()\n }\n }",
+            "class Foo {\n var bar: UIView?\n func fubar() {\n \t self?.bar=UILabel()\n }\n }",
+            "class Foo {\n var bar: UIView?\n func fubar() {\n \t bar \t = UILabel()\n }\n }",
+            "class Foo {\n var bar: UIView?\n func fubar() {\n \t bar=UILabel()\n }\n }"
+            // swiftlint:disable line_length
+            // MARK: ToDo: This string literal causes warning
+            // "class Foo {\n @IBOutlet var bar: UIView?\n func fubar() {\n print(\"my bar = \\(bar?.description)\")\n }\n }",
+            // MARK: ToDo: This valid multiline local variable will cause warning
+            // "class Foo {\n @IBOutlet var bar: UIView?\n func fubar() {\n \t var \t bar: UIView?\n bar \t = UILabel()\n }\n }",
+            // swiftlint:enable line_length
+        ],
+        triggeringExamples: [
+            "class Foo {\n @IBOutlet var bar: UIView?\n func fubar() {\n \t self.bar \t  = UILabel()\n }\n }",
+            "class Foo {\n @IBOutlet var bar: UIView?\n func fubar() {\n \t self.bar=UILabel()\n }\n }",
+            "class Foo {\n @IBOutlet var bar: UIView?\n func fubar() {\n \t self!.bar \t = UILabel()\n }\n }",
+            "class Foo {\n @IBOutlet var bar: UIView?\n func fubar() {\n \t self!.bar=UILabel()\n }\n }",
+            "class Foo {\n @IBOutlet var bar: UIView?\n func fubar() {\n \t self?.bar \t = UILabel()\n }\n }",
+            "class Foo {\n @IBOutlet var bar: UIView?\n func fubar() {\n \t self?.bar=UILabel()\n }\n }",
+            "class Foo {\n @IBOutlet var bar: UIView?\n func fubar() {\n \t bar \t = UILabel()\n }\n }",
+            "class Foo {\n @IBOutlet var bar: UIView?\n func fubar() {\n \t bar=UILabel()\n }\n }"
+            // MARK: ToDo: Fix Bug. This is very uncommon, but it should be warned against. Currently is not matched.
+            // "class Foo {\n @IBOutlet var invalidLabel = UILabel()\n }",
+        ]
+    )
+
+    public func validate(file: File, kind: SwiftDeclarationKind,
+                         dictionary: [String: SourceKitRepresentable]) -> [StyleViolation] {
+
+        // Find IBOutlets in the File
+        let substructure = dictionary.substructure
+        let outlets = substructure.filter({ $0.enclosedSwiftAttributes.contains("source.decl.attribute.iboutlet") })
+        let outletNames = outlets.flatMap({ $0.name })
+
+        // If there are no IBOutlets, we can bail
+        guard outletNames.isEmpty == false else { return [] }
+
+        // Find any line that contains a string that matches the discovered outlet names
+        // This will have many false positives
+        let possibleViolations: [(String, Line)] = outletNames.flatMap { outletName -> [(String, Line)] in
+            return file.lines.filter({ $0.content.contains(outletName) }).map({ (outletName, $0) })
+        }
+
+        // Go through possible violations, use regex to see if they violate rules
+        let confirmedViolations = possibleViolations.filter { outletName, line -> Bool in
+
+            // build the regex pattern
+            let noSelfPattern = "^\\s*\(outletName)\\s*="
+            let selfPattern = "self[\\?\\!]?\\.\(outletName)\\s*="
+            let noDotPattern = "[\\W*][\\D*][^\\.]\\s+\(outletName)\\s*="
+            let pattern = "(\(noSelfPattern)|\(selfPattern)|\(noDotPattern))"
+
+            // get the text that is being scanned
+            let content = line.content
+            let range = content.bridge().range(of: content)
+
+            // init new regex object and start operation
+            let regex = try? NSRegularExpression(pattern: pattern, options: [])
+            let match = regex?.firstMatch(in: content, options: [], range: range)
+
+            // if we have a match, then there is a violation
+            return match != nil
+        }
+
+        // Convert the Lines into errors
+        return confirmedViolations.map { _, line -> StyleViolation in
+            let location = Location(file: file, byteOffset: line.range.location)
+            return StyleViolation(ruleDescription: type(of: self).description,
+                                  severity: configuration.severity,
+                                  location: location)
+        }
+    }
+}

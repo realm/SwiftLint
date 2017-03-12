d475ed02413c4450cba3c83d6ab4a91682396141
Kim de Vos <kv@idealdev.dk>
2017-03-12 16:25:42 +0100

    Changed rule name from TypeInterfaceRule to ExplicitTypeInterfaceRule

--- Tests/SwiftLintFrameworkTests/RulesTests.swift
+++ Tests/SwiftLintFrameworkTests/RulesTests.swift
@@ -346,8 +346,8 @@ class RulesTests: XCTestCase {
         verifyRule(WeakDelegateRule.description)
     }
 
-    func testTypeInterface() {
-        verifyRule(TypeInterfaceRule.description)
+    func testExplicitTypeInterfaceRule() {
+        verifyRule(ExplicitTypeInterfaceRule.description)
     }
 
 }
@@ -424,7 +424,7 @@ extension RulesTests {
             ("testVoidReturn", testVoidReturn),
             ("testSuperCall", testSuperCall),
             ("testWeakDelegate", testWeakDelegate),
-            ("testTypeInterface", testTypeInterface)
+            ("testExplicitTypeInterfaceRule", testExplicitTypeInterfaceRule)
         ]
     }
 }

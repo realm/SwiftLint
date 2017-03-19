35e6474f0c6416421676a4f8d003e3b00631731e
Marcelo Fabri <marcelofabrimf@gmail.com>
2017-03-19 17:50:27 +0100

    Refactoring and make rule enabled by default

--- Source/SwiftLintFramework/Rules/FatalErrorMessageRule.swift
+++ Source/SwiftLintFramework/Rules/FatalErrorMessageRule.swift
@@ -9,7 +9,7 @@
 import Foundation
 import SourceKittenFramework
 
-public struct FatalErrorMessageRule: ASTRule, OptInRule, ConfigurationProviderRule {
+public struct FatalErrorMessageRule: ASTRule, ConfigurationProviderRule {
     public var configuration = SeverityConfiguration(.warning)
 
     public init() {}
@@ -17,9 +17,10 @@ public struct FatalErrorMessageRule: ASTRule, OptInRule, ConfigurationProviderRu
     public static let description = RuleDescription(
         identifier: "fatal_error_message",
         name: "Fatal Errror Message",
-        description: "The fatalError should have a message",
+        description: "A fatalError call should have a message.",
         nonTriggeringExamples: [
-            "func foo() {\n  fatalError(\"Foo\")\n}\n"
+            "func foo() {\n  fatalError(\"Foo\")\n}\n",
+            "func foo() {\n  fatalError(x)\n}\n"
         ],
         triggeringExamples: [
             "func foo() {\n  ↓fatalError(\"\")\n}\n",
@@ -27,30 +28,33 @@ public struct FatalErrorMessageRule: ASTRule, OptInRule, ConfigurationProviderRu
         ]
     )
 
-    public func validate(file: File, kind: SwiftDeclarationKind,
+    public func validate(file: File, kind: SwiftExpressionKind,
                          dictionary: [String: SourceKitRepresentable]) -> [StyleViolation] {
-        guard SwiftDeclarationKind.functionKinds().contains(kind)
-        else {
-            return []
+        guard kind == .call,
+            let offset = dictionary.offset,
+            dictionary.name == "fatalError",
+            hasEmptyBody(dictionary: dictionary, file: file) else {
+                return []
         }
 
-        return dictionary.substructure.flatMap { subDict -> [StyleViolation] in
-            var violations = validate(file: file, dictionary: subDict)
+        return [
+            StyleViolation(ruleDescription: type(of: self).description,
+                           severity: configuration.severity,
+                           location: Location(file: file, byteOffset: offset))
+        ]
+    }
 
-            if let offset = subDict.offset,
-                let kindString = subDict.kind,
-                let kind = SwiftExpressionKind(rawValue: kindString),
-                kind == SwiftExpressionKind.call,
-                subDict.name == "fatalError",
-                subDict.bodyLength == 2 || subDict.bodyLength == 0 {
-                let violation = StyleViolation(ruleDescription: type(of: self).description,
-                                               severity: configuration.severity,
-                                               location: Location(file: file, byteOffset: offset))
-
-                violations.append(violation)
-            }
-
-            return violations
+    private func hasEmptyBody(dictionary: [String: SourceKitRepresentable], file: File) -> Bool {
+        guard let bodyOffset = dictionary.bodyOffset,
+            let bodyLength = dictionary.bodyLength else {
+                return false
         }
+
+        if bodyLength == 0 {
+            return true
+        }
+
+        let body = file.contents.bridge().substringWithByteRange(start: bodyOffset, length: bodyLength)
+        return body == "\"\""
     }
 }

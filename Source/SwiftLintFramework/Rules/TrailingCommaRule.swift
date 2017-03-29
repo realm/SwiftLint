d884008199dcc24e315acf73828f5b6837645dfc
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2017-03-28 18:06:20 -0600

    Removed indents from empty lines.

--- Source/SwiftLintFramework/Rules/TrailingCommaRule.swift
+++ Source/SwiftLintFramework/Rules/TrailingCommaRule.swift
@@ -175,13 +175,13 @@ public struct TrailingCommaRule: ASTRule, CorrectableRule, ConfigurationProvider
         matches.reversed().forEach { range in
             let utf16Start = correctedContents.utf16.index(correctedContents.utf16.startIndex, offsetBy: range.location)
             let utf16End = correctedContents.utf16.index(utf16Start, offsetBy: range.length)
-            
+
             guard let scalarsStart = utf16Start.samePosition(in: correctedContents.unicodeScalars),
                 let scalarsEnd = utf16End.samePosition(in: correctedContents.unicodeScalars) else {
                 fatalError("A UTF‐16 index is pointing at a trailing surrogate.\nThere is a bug in SwiftLint.")
             }
             let scalarRange = scalarsStart ..< scalarsEnd
-            
+
             if configuration.mandatoryComma {
                 correctedContents.unicodeScalars.insert(",", at: scalarRange.lowerBound)
             } else {

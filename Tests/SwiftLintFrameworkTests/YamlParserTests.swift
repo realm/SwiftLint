3369c36b0266590739e3183d2addfe1c6e525ded
Norio Nomura <norio.nomura@gmail.com>
2017-04-03 10:45:04 +0900

    Skip a test failing on Swift 3.0.2 for Linux

--- Tests/SwiftLintFrameworkTests/YamlParserTests.swift
+++ Tests/SwiftLintFrameworkTests/YamlParserTests.swift
@@ -30,10 +30,19 @@ class YamlParserTests: XCTestCase {
 
 extension YamlParserTests {
     static var allTests: [(String, (YamlParserTests) -> () throws -> Void)] {
-        return [
-            ("testParseEmptyString", testParseEmptyString),
-            ("testParseValidString", testParseValidString),
-            ("testParseInvalidStringThrows", testParseInvalidStringThrows)
-        ]
+        #if swift(>=3.1)
+            return [
+                ("testParseEmptyString", testParseEmptyString),
+                ("testParseValidString", testParseValidString),
+                ("testParseInvalidStringThrows", testParseInvalidStringThrows)
+            ]
+        #else
+            // YamlParserError returns incorrect description because of the bug on Swift 3.0.2 for Linux
+            // - See: https://bugs.swift.org/browse/SR-3366
+            return [
+                ("testParseEmptyString", testParseEmptyString),
+                ("testParseValidString", testParseValidString)
+            ]
+        #endif
     }
 }

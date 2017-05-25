6c2bb0cf52f6fc4186c6b7afe8cfa851fd0a9b41
JP Simard <jp@jpsim.com>
2017-05-25 07:39:12 -0700

    remove SR-3366 workaround
    as we're no longer testing on Swift 3.0.2 on Linux

--- Tests/SwiftLintFrameworkTests/YamlParserTests.swift
+++ Tests/SwiftLintFrameworkTests/YamlParserTests.swift
@@ -30,19 +30,10 @@ class YamlParserTests: XCTestCase {
 
 extension YamlParserTests {
     static var allTests: [(String, (YamlParserTests) -> () throws -> Void)] {
-        #if swift(>=3.1)
-            return [
-                ("testParseEmptyString", testParseEmptyString),
-                ("testParseValidString", testParseValidString),
-                ("testParseInvalidStringThrows", testParseInvalidStringThrows)
-            ]
-        #else
-            // YamlParserError returns incorrect description because of the bug on Swift 3.0.2 for Linux
-            // - See: https://bugs.swift.org/browse/SR-3366
-            return [
-                ("testParseEmptyString", testParseEmptyString),
-                ("testParseValidString", testParseValidString)
-            ]
-        #endif
+        return [
+            ("testParseEmptyString", testParseEmptyString),
+            ("testParseValidString", testParseValidString),
+            ("testParseInvalidStringThrows", testParseInvalidStringThrows)
+        ]
     }
 }

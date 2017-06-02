876a7d2733a341daaa16d10a006ad7aff77f8ad5
Felipe Plets <felipe@menvia.com>
2017-06-02 09:16:42 -0300

    Problem occur also when using lowercased()

--- Source/SwiftLintFramework/Rules/EmptyEnumArgumentsRule.swift
+++ Source/SwiftLintFramework/Rules/EmptyEnumArgumentsRule.swift
@@ -24,6 +24,7 @@ public struct EmptyEnumArgumentsRule: ASTRule, ConfigurationProviderRule, Correc
             "switch foo {\n case let .bar(x): break\n}",
             "switch (foo, bar) {\n case (_, _): break\n}",
             "switch foo {\n case \"bar\".uppercased(): break\n}"
+            "switch foo {\n case \"bar\".lowercased(): break\n}"
         ],
         triggeringExamples: [
             "switch foo {\n case .bar↓(_): break\n}",

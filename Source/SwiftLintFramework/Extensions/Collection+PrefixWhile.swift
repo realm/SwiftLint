57d4dc7c52d4389f2bcf2e412ece5ad22cb012f9
JP Simard <jp@jpsim.com>
2017-07-10 15:56:02 -0700

    change an empty `#if` body for `#if !`

--- Source/SwiftLintFramework/Extensions/Collection+PrefixWhile.swift
+++ Source/SwiftLintFramework/Extensions/Collection+PrefixWhile.swift
@@ -8,16 +8,15 @@
 
 import Foundation
 
-#if swift(>=3.1)
-#else
-    extension Collection {
-        // from https://github.com/apple/swift/blob/4b0597a8/stdlib/public/core/Collection.swift#L1558
-        func prefix(while predicate: (Iterator.Element) throws -> Bool) rethrows -> SubSequence {
-            var end = startIndex
-            while try end != endIndex && predicate(self[end]) {
-                formIndex(after: &end)
-            }
-            return self[startIndex..<end]
+#if !swift(>=3.1)
+extension Collection {
+    // from https://github.com/apple/swift/blob/4b0597a8/stdlib/public/core/Collection.swift#L1558
+    func prefix(while predicate: (Iterator.Element) throws -> Bool) rethrows -> SubSequence {
+        var end = startIndex
+        while try end != endIndex && predicate(self[end]) {
+            formIndex(after: &end)
         }
+        return self[startIndex..<end]
     }
+}
 #endif

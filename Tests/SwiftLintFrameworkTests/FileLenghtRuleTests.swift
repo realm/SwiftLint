b6c5731c9faf6f562fc7fafd7f4ae7450389cb6f
Samuel Susla <samuel.susla@gmail.com>
2017-07-11 22:47:32 +0100

    Add tests

--- Tests/SwiftLintFrameworkTests/FileLenghtRuleTests.swift
+++ Tests/SwiftLintFrameworkTests/FileLenghtRuleTests.swift
@@ -1,9 +1,33 @@
 //
-//  FileLenghtRuleTests.swift
+//  FileLengthRuleTests.swift
 //  SwiftLint
 //
-//  Created by samuel susla on 7/11/17.
-//  Copyright © 2017 Realm. All rights reserved.
+//  Created by Samuel Susla on 11/07/17.
+//  Copyright © 2016 Realm. All rights reserved.
 //
 
-import Foundation
+import SwiftLintFramework
+import XCTest
+
+class FileLenghtRuleTests: XCTestCase {
+    private static let triggeringExamples = [
+        repeatElement("print(\"swiftlint\")\n", count: 401).joined()
+    ]
+
+    private static let nonTriggeringExamples = [
+        (repeatElement("print(\"swiftlint\")\n", count: 400) + ["//\n"]).joined(),
+        repeatElement("print(\"swiftlint\")\n", count: 400).joined()
+    ]
+
+    private let fileLenghRuleDescription = FileLengthRule.description
+        .with(nonTriggeringExamples: FileLenghtRuleTests.nonTriggeringExamples)
+        .with(triggeringExamples: FileLenghtRuleTests.triggeringExamples)
+
+    func testFileLengthRuleIgnoringLinesWithOnlyComments() {
+        // Verify TrailingCommaRule with test values for when mandatory_comma is true.
+        let ruleDescription = fileLenghRuleDescription
+        let ruleConfiguration = ["ignore_comment_only_lines": true]
+
+        verifyRule(ruleDescription, ruleConfiguration: ruleConfiguration)
+    }
+}

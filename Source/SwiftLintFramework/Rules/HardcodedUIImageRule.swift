a332107854c13bbb62416f5c9d1d70297dd7ddc6
Rishabh Tayal <rtayal11@gmail.com>
2017-08-14 13:12:40 -0500

    added file for hardcoded uiimage rule

---
+++ Source/SwiftLintFramework/Rules/HardcodedUIImageRule.swift
@@ -0,0 +1,14 @@
+//
+//  HardcodedUIImageRule.swift
+//  SwiftLint
+//
+//  Created by Tayal, Rishabh on 5/3/17.
+//  Copyright © 2017 Realm. All rights reserved.
+//
+
+import Foundation
+import SourceKittenFramework
+
+public struct HardcodedUIImageRule: ConfigurationProviderRule {
+
+}

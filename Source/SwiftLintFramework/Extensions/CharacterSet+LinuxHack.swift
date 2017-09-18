06781c7ddde3551a2ab96c7087f6d8cc088f339a
JP Simard <jp@jpsim.com>
2017-09-17 17:28:32 -0700

    Simplify CharacterSet+LinuxHack.swift

--- Source/SwiftLintFramework/Extensions/CharacterSet+LinuxHack.swift
+++ Source/SwiftLintFramework/Extensions/CharacterSet+LinuxHack.swift
@@ -10,26 +10,13 @@ import Foundation
 
 extension CharacterSet {
     func isSuperset(ofCharactersIn string: String) -> Bool {
-        #if swift(>=4.0) || os(macOS)
-            let otherSet = CharacterSet(charactersIn: string)
-            return isSuperset(of: otherSet)
-        #else
-            // workaround for https://bugs.swift.org/browse/SR-3485
-            let chars = Set(string.characters)
-            for char in chars where !contains(char.unicodeScalar) {
-                return false
-            }
-
-            return true
-        #endif
-    }
-}
-
-private extension Character {
-    var unicodeScalar: UnicodeScalar {
-        let characterString = String(self)
-        let scalars = characterString.unicodeScalars
-
-        return scalars[scalars.startIndex]
+#if swift(>=4.0) || os(macOS)
+        return isSuperset(of: CharacterSet(charactersIn: string))
+#else
+        // workaround for https://bugs.swift.org/browse/SR-3485
+        return !Set(string.characters).contains { character in
+            !contains(String(character).unicodeScalars.first!)
+        }
+#endif
     }
 }

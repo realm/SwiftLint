bd5f26f20461f2915159cf91f4037bd94661d7f7
Christopher Gretzki <gretzki@adesso-mobile.de>
2018-02-28 11:13:29 +0100

    add Reflection as dependency

--- Package.swift
+++ Package.swift
@@ -12,6 +12,7 @@ let package = Package(
         .package(url: "https://github.com/jpsim/SourceKitten.git", from: "0.19.1"),
         .package(url: "https://github.com/jpsim/Yams.git", from: "0.5.0"),
         .package(url: "https://github.com/scottrhoyt/SwiftyTextTable.git", from: "0.8.0"),
+        .package(url: "https://github.com/Zewo/Reflection.git", from: "0.15.0"),
     ],
     targets: [
         .target(
@@ -27,6 +28,7 @@ let package = Package(
             dependencies: [
                 "SourceKittenFramework",
                 "Yams",
+                "Reflection",
             ]
         ),
         .testTarget(

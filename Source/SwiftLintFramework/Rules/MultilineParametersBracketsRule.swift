b90c80038f242a5f78ff6e6ecc04c54d3d9cf903
Cihat Gündüz <commits@cihatguenduez.de>
2018-07-24 12:37:53 +0200

    Use guard where appropriate instead of if

--- Source/SwiftLintFramework/Rules/MultilineParametersBracketsRule.swift
+++ Source/SwiftLintFramework/Rules/MultilineParametersBracketsRule.swift
@@ -130,52 +130,54 @@ public struct MultilineParametersBracketsRule: OptInRule, ConfigurationProviderR
 
     private func openingBracketViolation(parameters: [[String: SourceKitRepresentable]],
                                          file: File) -> StyleViolation? {
-        if
+        guard
             let firstParamByteOffset = parameters.first?.offset,
             let firstParamByteLength = parameters.first?.length,
             let firstParamRange = file.contents.bridge().byteRangeToNSRange(
                 start: firstParamByteOffset,
                 length: firstParamByteLength
             )
-        {
-            let prefix = file.contents.bridge().substring(to: firstParamRange.lowerBound)
-            let invalidPrefixRegex = regex("\\([ \\t]*\\z")
-
-            if let invalidMatch = invalidPrefixRegex.firstMatch(in: prefix, options: [], range: prefix.fullNSRange) {
-                return StyleViolation(
-                    ruleDescription: type(of: self).description,
-                    severity: configuration.severity,
-                    location: Location(file: file, characterOffset: invalidMatch.range.location + 1)
-                )
-            }
+        else {
+                return nil
         }
 
-        return nil
+        let prefix = file.contents.bridge().substring(to: firstParamRange.lowerBound)
+        let invalidRegex = regex("\\([ \\t]*\\z")
+
+        guard let invalidMatch = invalidRegex.firstMatch(in: prefix, options: [], range: prefix.fullNSRange)
+            else { return nil }
+
+        return StyleViolation(
+            ruleDescription: type(of: self).description,
+            severity: configuration.severity,
+            location: Location(file: file, characterOffset: invalidMatch.range.location + 1)
+        )
     }
 
     private func closingBracketViolation(parameters: [[String: SourceKitRepresentable]],
                                          file: File) -> StyleViolation? {
-        if
+        guard
             let lastParamByteOffset = parameters.last?.offset,
             let lastParamByteLength = parameters.last?.length,
             let lastParamRange = file.contents.bridge().byteRangeToNSRange(
                 start: lastParamByteOffset,
                 length: lastParamByteLength
             )
-        {
-            let suffix = file.contents.bridge().substring(from: lastParamRange.upperBound)
-            let invalidSuffixRegex = regex("\\A[ \\t]*\\)")
-
-            if let invalidMatch = invalidSuffixRegex.firstMatch(in: suffix, options: [], range: suffix.fullNSRange) {
-                let characterOffset = lastParamRange.upperBound + invalidMatch.range.upperBound - 1
-                return StyleViolation(
-                    ruleDescription: type(of: self).description,
-                    severity: configuration.severity,
-                    location: Location(file: file, characterOffset: characterOffset)
-                )
-            }
+        else {
+            return nil
         }
 
-        return nil
+        let suffix = file.contents.bridge().substring(from: lastParamRange.upperBound)
+        let invalidRegex = regex("\\A[ \\t]*\\)")
+
+        guard let invalidMatch = invalidRegex.firstMatch(in: suffix, options: [], range: suffix.fullNSRange)
+            else { return nil }
+
+        let characterOffset = lastParamRange.upperBound + invalidMatch.range.upperBound - 1
+        return StyleViolation(
+            ruleDescription: type(of: self).description,
+            severity: configuration.severity,
+            location: Location(file: file, characterOffset: characterOffset)
+        )
     }
 }

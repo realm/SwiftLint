044d9168d6978871ae27c43db905ef818e26fdbc
Mike Ciesielka <maciesielka@comcast.net>
2018-07-28 15:49:36 -0400

    Disable "line_length" to make exception for console output of lengthy configuration name "only_enforce_after_first_closure_on_first_line"

--- Source/SwiftLintFramework/Rules/MultilineArgumentsConfiguration.swift
+++ Source/SwiftLintFramework/Rules/MultilineArgumentsConfiguration.swift
@@ -28,9 +28,11 @@ public struct MultilineArgumentsConfiguration: RuleConfiguration, Equatable {
     private(set) var onlyEnforceAfterFirstClosureOnFirstLine = false
 
     public var consoleDescription: String {
+        // swiftlint:disable line_length
         return severityConfiguration.consoleDescription +
             ", \(ConfigurationKey.firstArgumentLocation.rawValue): \(firstArgumentLocation.rawValue)" +
             ", \(ConfigurationKey.onlyEnforceAfterFirstClosureOnFirstLine.rawValue): \(onlyEnforceAfterFirstClosureOnFirstLine)"
+        // swiftlint:enable line_length
     }
 
     public mutating func apply(configuration: Any) throws {

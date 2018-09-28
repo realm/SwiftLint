b0ddef938a3e4d7f936429b88e0f4aae0c3c2e4e
Kim de Vos <kimdevos12@hotmail.com>
2018-09-28 11:28:56 +0200

    Added possibility to make lint fail if using wrong version

--- Source/SwiftLintFramework/Extensions/Configuration+Parsing.swift
+++ Source/SwiftLintFramework/Extensions/Configuration+Parsing.swift
@@ -8,6 +8,7 @@ extension Configuration {
         case optInRules = "opt_in_rules"
         case reporter = "reporter"
         case swiftlintVersion = "swiftlint_version"
+        case swiftlintVersionFailable = "swiftlint_version_failable"
         case useNestedConfigs = "use_nested_configs" // deprecated
         case warningThreshold = "warning_threshold"
         case whitelistRules = "whitelist_rules"
@@ -25,6 +26,7 @@ extension Configuration {
             .optInRules,
             .reporter,
             .swiftlintVersion,
+            .swiftlintVersionFailable,
             .useNestedConfigs,
             .warningThreshold,
             .whitelistRules,
@@ -97,6 +99,7 @@ extension Configuration {
                   ruleList: ruleList,
                   configuredRules: configuredRules,
                   swiftlintVersion: dict[Key.swiftlintVersion.rawValue] as? String,
+                  swiftlintVersionFailable: dict[Key.swiftlintVersionFailable.rawValue] as? Bool,
                   cachePath: cachePath ?? dict[Key.cachePath.rawValue] as? String,
                   indentation: indentation)
     }
@@ -113,6 +116,7 @@ extension Configuration {
                   ruleList: RuleList = masterRuleList,
                   configuredRules: [Rule]?,
                   swiftlintVersion: String?,
+                  swiftlintVersionFailable: Bool?,
                   cachePath: String?,
                   indentation: IndentationStyle) {
         let rulesMode: RulesMode
@@ -138,6 +142,7 @@ extension Configuration {
                   ruleList: ruleList,
                   configuredRules: configuredRules,
                   swiftlintVersion: swiftlintVersion,
+                  swiftlintVersionFailable: swiftlintVersionFailable,
                   cachePath: cachePath,
                   indentation: indentation)
     }

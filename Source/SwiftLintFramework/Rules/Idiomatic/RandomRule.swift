3d6030dfc077ef0dbc23634e5d8a4a5aef9b7cfb
Joshua Kaplan <yhkaplan@gmail.com>
2018-09-30 14:54:20 +0900

    Change rule implementation to use regex

--- Source/SwiftLintFramework/Rules/Idiomatic/RandomRule.swift
+++ Source/SwiftLintFramework/Rules/Idiomatic/RandomRule.swift
@@ -1,22 +1,16 @@
 import Foundation
 import SourceKittenFramework
 
-public struct RandomRule: ASTRule, OptInRule, ConfigurationProviderRule, AutomaticTestableRule {
+public struct RandomRule: OptInRule, ConfigurationProviderRule, AutomaticTestableRule {
 
     public var configuration = SeverityConfiguration(.warning)
-
-    private let randomFunctions: Set<String> = [
-        "arc4random",
-        "arc4random_uniform",
-        "drand48"
-    ]
 
     public init() {}
 
     public static var description = RuleDescription(
         identifier: "random",
         name: "Random",
-        description: "Prefer using `type.random` over C-based functions.",
+        description: "Prefer using `type.random(in:)` over C-based functions.",
         kind: .lint,
         minSwiftVersion: .fourDotTwo,
         nonTriggeringExamples: [
@@ -31,49 +25,14 @@ public struct RandomRule: ASTRule, OptInRule, ConfigurationProviderRule, Automat
         ]
     )
 
-    // MARK: - ASTRule
+    public func validate(file: File) -> [StyleViolation] {
+        let pattern = "(arc4random|drand48)"
+        let excludingKinds = SyntaxKind.commentAndStringKinds
 
-    public func validate(
-        file: File,
-        kind: SwiftDeclarationKind,
-        dictionary: [String: SourceKitRepresentable]
-    ) -> [StyleViolation] {
-
-        return violationRanges(in: file, kind: kind, dictionary: dictionary).map { violation in
-
-            let location = Location(file: file, characterOffset: violation.location)
-            let ruleDescription = type(of: self).description
-
-            return StyleViolation(
-                ruleDescription: ruleDescription,
-                severity: configuration.severity,
-                location: location
-            )
+        return file.match(pattern: pattern, excludingSyntaxKinds: excludingKinds).map {
+            StyleViolation(ruleDescription: type(of: self).description,
+                           severity: configuration.severity,
+                           location: Location(file: file, characterOffset: $0.location))
         }
     }
-
-    // MARK: - Private
-
-    private func violationRanges(
-        in file: File,
-        kind: SwiftDeclarationKind,
-        dictionary: [String: SourceKitRepresentable]
-    ) -> [NSRange] {
-
-        guard kind == .functionFree else { return [] }
-
-        return dictionary.elements.compactMap { subDict -> NSRange? in
-            guard
-                let offset = subDict.offset,
-                let length = subDict.length,
-                let content = file.contents.bridge().substringWithByteRange(start: offset, length: length),
-                randomFunctions.contains(content)
-            else {
-                return nil
-            }
-
-            return file.contents.bridge().byteRangeToNSRange(start: offset, length: length)
-        }
-    }
-
 }

f6bbc91fab8cbb38e23e09844c545b18d46f5e57
JP Simard <jp@jpsim.com>
2018-11-27 21:42:19 -0800

    Minor modifications to VerticalWhitespaceBetweenCasesRule

--- Source/SwiftLintFramework/Rules/Style/VerticalWhitespaceBetweenCasesRule.swift
+++ Source/SwiftLintFramework/Rules/Style/VerticalWhitespaceBetweenCasesRule.swift
@@ -62,72 +62,68 @@ public struct VerticalWhitespaceBetweenCasesRule: ConfigurationProviderRule {
     ]
 
     private static let violatingToValidExamples: [String: String] = [
-    """
-        switch x {
-        case 0..<5:
-            print("x is valid")
-    ↓    default:
-            print("x is invalid")
-        }
-    """: """
-        switch x {
-        case 0..<5:
-            print("x is valid")
+        """
+            switch x {
+            case 0..<5:
+                print("x is valid")
+        ↓    default:
+                print("x is invalid")
+            }
+        """: """
+            switch x {
+            case 0..<5:
+                print("x is valid")
 
-        default:
-            print("x is invalid")
-        }
-    """,
-    """
-        switch x {
-        case .valid:
-            print("x is valid")
-    ↓    case .invalid:
-            print("x is invalid")
-        }
-    """: """
-        switch x {
-        case .valid:
-            print("x is valid")
+            default:
+                print("x is invalid")
+            }
+        """,
+        """
+            switch x {
+            case .valid:
+                print("x is valid")
+        ↓    case .invalid:
+                print("x is invalid")
+            }
+        """: """
+            switch x {
+            case .valid:
+                print("x is valid")
 
-        case .invalid:
-            print("x is invalid")
-        }
-    """,
-    """
-        switch x {
-        case .valid:
-            print("multiple ...")
-            print("... lines")
-    ↓    case .invalid:
-            print("multiple ...")
-            print("... lines")
-        }
-    """: """
-        switch x {
-        case .valid:
-            print("multiple ...")
-            print("... lines")
+            case .invalid:
+                print("x is invalid")
+            }
+        """,
+        """
+            switch x {
+            case .valid:
+                print("multiple ...")
+                print("... lines")
+        ↓    case .invalid:
+                print("multiple ...")
+                print("... lines")
+            }
+        """: """
+            switch x {
+            case .valid:
+                print("multiple ...")
+                print("... lines")
 
-        case .invalid:
-            print("multiple ...")
-            print("... lines")
-        }
-    """
+            case .invalid:
+                print("multiple ...")
+                print("... lines")
+            }
+        """
     ]
 
     private let pattern = "([^\\n{][ \\t]*\\n)([ \\t]*(?:case[^\\n]+|default):[ \\t]*\\n)"
 }
 
 extension VerticalWhitespaceBetweenCasesRule: OptInRule, AutomaticTestableRule {
-    public var configurationDescription: String { return "N/A" }
-
-    public init(configuration: Any) throws {}
-
     public static let description = RuleDescription(
         identifier: "vertical_whitespace_between_cases",
         name: "Vertical Whitespace Between Cases",
-        description: "Include a vertical whitespace (empty line) between cases in switch statements.",
+        description: "Include a single empty line between switch cases.",
         kind: .style,
         nonTriggeringExamples: (violatingToValidExamples.values + nonTriggeringExamples).sorted(),
         triggeringExamples: Array(violatingToValidExamples.keys).sorted(),
@@ -135,12 +131,14 @@ extension VerticalWhitespaceBetweenCasesRule: OptInRule, AutomaticTestableRule {
     )
 
     public func validate(file: File) -> [StyleViolation] {
-        let patternRegex: NSRegularExpression = regex(pattern)
-
-        return file.violatingRanges(for: pattern).map { violationRange in
+        let patternRegex = regex(pattern)
+        return file.violatingRanges(for: pattern).compactMap { violationRange in
             let substring = file.contents.substring(from: violationRange.location, length: violationRange.length)
             let substringRange = NSRange(location: 0, length: substring.count)
-            let matchResult = patternRegex.firstMatch(in: substring, options: [], range: substringRange)!
+            guard let matchResult = patternRegex.firstMatch(in: substring, options: [], range: substringRange) else {
+                return nil
+            }
+
             let violatingSubrange = matchResult.range(at: 2)
             let characterOffset = violationRange.location + violatingSubrange.location
 
@@ -158,7 +156,7 @@ extension VerticalWhitespaceBetweenCasesRule: CorrectableRule {
         let violatingRanges = file.ruleEnabled(violatingRanges: file.violatingRanges(for: pattern), for: self)
         guard !violatingRanges.isEmpty else { return [] }
 
-        let patternRegex: NSRegularExpression = regex(pattern)
+        let patternRegex = regex(pattern)
         let replacementTemplate = "$1\n$2"
         let description = type(of: self).description
 

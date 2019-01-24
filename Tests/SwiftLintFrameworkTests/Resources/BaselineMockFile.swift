f4af00af0b4817d1503c52aa09fd1317b650d2db
Przemysław Olszacki <przemyslaw.olszacki@tooploox.com>
2019-01-24 13:56:44 +0100

    Remove unused BaselineMockFile

--- Tests/SwiftLintFrameworkTests/Resources/BaselineMockFile.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-class BaselineMockedClass {
-
-    func veryLongFunctionWithManyArguments(argumentWithVeryVeryVeryLongName: String, anotherArgumentWithVeryVeryVeryLongName: String, arugmentWithNotSoLongName: String) -> String {
-        return argumentWithVeryVeryVeryLongName + anotherArgumentWithVeryVeryVeryLongName + arugmentWithNotSoLongName
-    }
-}
-

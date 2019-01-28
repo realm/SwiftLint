f08c3e4239b27272e5aa2b3a5c120a8586e2bffb
JP Simard <jp@jpsim.com>
2019-01-27 20:19:16 -0800

    Use firstIndex(where:) instead of index(where:) (#2600)
    to silence a Swift 5 warning. This is compatible with Swift 4.2.

--- Source/SwiftLintFramework/Extensions/SyntaxMap+SwiftLint.swift
+++ Source/SwiftLintFramework/Extensions/SyntaxMap+SwiftLint.swift
@@ -11,7 +11,7 @@ extension SyntaxMap {
                 .intersects(byteRange)
         }
 
-        guard let startIndex = tokens.index(where: intersect) else {
+        guard let startIndex = tokens.firstIndex(where: intersect) else {
             return []
         }
         let tokensBeginningIntersect = tokens.lazy.suffix(from: startIndex)

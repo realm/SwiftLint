b49ecf7b9afea566e1bf4d19b32e4d2779424f8b
Marcelo Fabri <me@marcelofabri.com>
2019-01-27 18:43:45 -0800

    Migrate JoinedDefaultParameterRule

--- Source/SwiftLintFramework/Rules/Idiomatic/JoinedDefaultParameterRule.swift
+++ Source/SwiftLintFramework/Rules/Idiomatic/JoinedDefaultParameterRule.swift
@@ -1,7 +1,7 @@
 import Foundation
 import SourceKittenFramework
 
-public struct JoinedDefaultParameterRule: ASTRule, ConfigurationProviderRule, OptInRule, CorrectableRule,
+public struct JoinedDefaultParameterRule: SubstitutionCorrectableASTRule, ConfigurationProviderRule, OptInRule,
                                           AutomaticTestableRule {
     public var configuration = SeverityConfiguration(.warning)
 
@@ -51,50 +51,15 @@ public struct JoinedDefaultParameterRule: ASTRule, ConfigurationProviderRule, Op
         }
     }
 
-    // MARK: - CorrectableRule
+    // MARK: - SubstitutionCorrectableASTRule
 
-    public func correct(file: File) -> [Correction] {
-        let matches = file.ruleEnabled(violatingRanges: violationRanges(in: file), for: self)
-        var correctedContents = file.contents
-        var adjustedLocations: [Int] = []
-
-        for violatingRange in matches.reversed() {
-            if let range = file.contents.nsrangeToIndexRange(violatingRange) {
-                correctedContents = correctedContents.replacingCharacters(in: range, with: "")
-                adjustedLocations.insert(violatingRange.location, at: 0)
-            }
-        }
-
-        file.write(correctedContents)
-
-        return adjustedLocations.map {
-            Correction(ruleDescription: type(of: self).description, location: Location(file: file, characterOffset: $0))
-        }
+    public func substitution(for violationRange: NSRange, in file: File) -> (NSRange, String) {
+        return (violationRange, "")
     }
 
-    // MARK: - Private
-
-    private func violationRanges(in file: File) -> [NSRange] {
-        return violationRanges(in: file, dictionary: file.structure.dictionary).sorted { $0.location < $1.location }
-    }
-
-    private func violationRanges(in file: File,
-                                 dictionary: [String: SourceKitRepresentable]) -> [NSRange] {
-        let ranges = dictionary.substructure.flatMap { subDict -> [NSRange] in
-            var ranges = violationRanges(in: file, dictionary: subDict)
-            if let kind = subDict.kind.flatMap(SwiftExpressionKind.init(rawValue:)) {
-                ranges += violationRanges(in: file, kind: kind, dictionary: subDict)
-            }
-
-            return ranges
-        }
-
-        return ranges.unique
-    }
-
-    private func violationRanges(in file: File,
-                                 kind: SwiftExpressionKind,
-                                 dictionary: [String: SourceKitRepresentable]) -> [NSRange] {
+    public func violationRanges(in file: File,
+                                kind: SwiftExpressionKind,
+                                dictionary: [String: SourceKitRepresentable]) -> [NSRange] {
         guard
             // is it calling a method '.joined' and passing a single argument?
             kind == .call,

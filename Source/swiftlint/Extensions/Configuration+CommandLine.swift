cb9ba877d53f5bb02b709f44c04746931a815c15
Dylan Bruschi <dylan.bruschi@gotruemotion.com>
2019-02-14 14:00:23 -0500

    Revert CompactMap

--- Source/swiftlint/Extensions/Configuration+CommandLine.swift
+++ Source/swiftlint/Extensions/Configuration+CommandLine.swift
@@ -68,7 +68,7 @@ extension Configuration {
             // Files whose configuration specifies they should be excluded will be skipped
             let fileConfiguration = configuration(for: file)
             let excludedPaths = fileConfiguration.excluded
-                .compactMap { fileConfiguration.rootPath?.bridge().appendingPathComponent($0) }
+                .map { (fileConfiguration.rootPath ?? "").bridge().appendingPathComponent($0) }
 
             var shouldSkip = false
             for excludedPath in excludedPaths {

realm/SwiftLint

commit 2bcea4b04d964d312a2e4e2a1a3c11b61a2ee30e

Add ability for SwiftLint to lint files with full type-checked AST awareness (#2379)
* Add LintableFilesVisitor

* Move LintCommand logic into LintOrAnalyzeCommand

to prepare for the upcoming analyze command

* Add AnalyzeCommand (not fully implemented yet in SwiftLintFramework)

* Add analyzerRules configuration member

* Add AnalyzerRule protocol

* Pass compiler arguments to validate/correct

* Add requiresFileOnDisk member to RuleDescription

This will be used by AnalyzerRules because they need a file on disk
to pass in the compiler arguments to SourceKit.

* Exclusively run AnalyzerRules when the Linter has compiler arguments

* Enable testing AnalyzerRules in TestHelpers

* Add ExplicitSelfRule

* Update documentation

* Fix `analyze --autocorrect`

* Improve performance of CompilerArgumentsExtractor

* Fix lint command actually running analyze

* Move File operations in TestHelpers into a private extension

* Add analyzer column to rules command and markdown documentation

* Use a Set literal

* Make AnalyzerRule inherit from OptInRule

* Mention analyzer_rules in readme

* Mention that analyzer rules are slow

Source/swiftlint/Commands/RulesCommand.swift
@@ -112,6 +112,7 @@ extension TextTable {
             TextTableColumn(header: "correctable"),
             TextTableColumn(header: "enabled in your config"),
             TextTableColumn(header: "kind"),
+            TextTableColumn(header: "analyzer"),
             TextTableColumn(header: "configuration")
         ]
         self.init(columns: columns)
@@ -141,6 +142,7 @@ extension TextTable {
                 (rule is CorrectableRule) ? "yes" : "no",
                 configuredRule != nil ? "yes" : "no",
                 ruleType.description.kind.rawValue,
+                (rule is AnalyzerRule) ? "yes" : "no",
                 truncate((configuredRule ?? rule).configurationDescription)
             ])
         }

Source/SwiftLintFramework/Extensions/Configuration+Parsing.swift
@@ -14,6 +14,7 @@ extension Configuration {
         case warningThreshold = "warning_threshold"
         case whitelistRules = "whitelist_rules"
         case indentation = "indentation"
+        case analyzerRules = "analyzer_rules"
     }
 
     private static func validKeys(ruleList: RuleList) -> [String] {
@@ -29,7 +30,8 @@ extension Configuration {
             .useNestedConfigs,
             .warningThreshold,
             .whitelistRules,
-            .indentation
+            .indentation,
+            .analyzerRules
         ].map({ $0.rawValue }) + ruleList.allValidIdentifiers()
     }
 
@@ -65,6 +67,7 @@ extension Configuration {
 
         let disabledRules = defaultStringArray(dict[Key.disabledRules.rawValue])
         let whitelistRules = defaultStringArray(dict[Key.whitelistRules.rawValue])
+        let analyzerRules = defaultStringArray(dict[Key.analyzerRules.rawValue])
         let included = defaultStringArray(dict[Key.included.rawValue])
         let excluded = defaultStringArray(dict[Key.excluded.rawValue])
         let indentation = Configuration.getIndentationLogIfInvalid(from: dict)
@@ -88,6 +91,7 @@ extension Configuration {
                   optInRules: optInRules,
                   enableAllRules: enableAllRules,
                   whitelistRules: whitelistRules,
+                  analyzerRules: analyzerRules,
                   included: included,
                   excluded: excluded,
                   warningThreshold: dict[Key.warningThreshold.rawValue] as? Int,
@@ -103,6 +107,7 @@ extension Configuration {
                   optInRules: [String],
                   enableAllRules: Bool,
                   whitelistRules: [String],
+                  analyzerRules: [String],
                   included: [String],
                   excluded: [String],
                   warningThreshold: Int?,
@@ -112,7 +117,6 @@ extension Configuration {
                   swiftlintVersion: String?,
                   cachePath: String?,
                   indentation: IndentationStyle) {
-
         let rulesMode: RulesMode
         if enableAllRules {
             rulesMode = .allEnabled
@@ -123,9 +127,9 @@ extension Configuration {
                     "with '\(Key.whitelistRules.rawValue)'")
                 return nil
             }
-            rulesMode = .whitelisted(whitelistRules)
+            rulesMode = .whitelisted(whitelistRules + analyzerRules)
         } else {
-            rulesMode = .default(disabled: disabledRules, optIn: optInRules)
+            rulesMode = .default(disabled: disabledRules, optIn: optInRules + analyzerRules)
         }
 
         self.init(rulesMode: rulesMode,

Source/SwiftLintFramework/Models/Linter.swift
@@ -46,7 +46,8 @@ private extension Rule {
     }
 
     func lint(file: File, regions: [Region], benchmark: Bool,
-              superfluousDisableCommandRule: SuperfluousDisableCommandRule?) -> LintResult? {
+              superfluousDisableCommandRule: SuperfluousDisableCommandRule?,
+              compilerArguments: [String]) -> LintResult? {
         if !(self is SourceKitFreeRule) && file.sourcekitdFailed {
             return nil
         }
@@ -57,10 +58,10 @@ private extension Rule {
         let ruleTime: (String, Double)?
         if benchmark {
             let start = Date()
-            violations = validate(file: file)
+            violations = validate(file: file, compilerArguments: compilerArguments)
             ruleTime = (ruleID, -start.timeIntervalSinceNow)
         } else {
-            violations = validate(file: file)
+            violations = validate(file: file, compilerArguments: compilerArguments)
             ruleTime = nil
         }
 
@@ -104,6 +105,7 @@ public struct Linter {
     private let rules: [Rule]
     private let cache: LinterCache?
     private let configuration: Configuration
+    private let compilerArguments: [String]
 
     public var styleViolations: [StyleViolation] {
         return getStyleViolations().0
@@ -114,7 +116,6 @@ public struct Linter {
     }
 
     private func getStyleViolations(benchmark: Bool = false) -> ([StyleViolation], [(id: String, time: Double)]) {
-
         if let cached = cachedStyleViolations(benchmark: benchmark) {
             return cached
         }
@@ -128,7 +129,8 @@ public struct Linter {
         }) as? SuperfluousDisableCommandRule
         let validationResults = rules.parallelFlatMap {
             $0.lint(file: self.file, regions: regions, benchmark: benchmark,
-                    superfluousDisableCommandRule: superfluousDisableCommandRule)
+                    superfluousDisableCommandRule: superfluousDisableCommandRule,
+                    compilerArguments: self.compilerArguments)
         }
         let violations = validationResults.flatMap { $0.violations }
         let ruleTimes = validationResults.compactMap { $0.ruleTime }
@@ -170,11 +172,19 @@ public struct Linter {
         return (cachedViolations, ruleTimes)
     }
 
-    public init(file: File, configuration: Configuration = Configuration()!, cache: LinterCache? = nil) {
+    public init(file: File, configuration: Configuration = Configuration()!, cache: LinterCache? = nil,
+                compilerArguments: [String] = []) {
         self.file = file
-        self.cache = cache
         self.configuration = configuration
-        rules = configuration.rules
+        self.cache = cache
+        self.compilerArguments = compilerArguments
+        self.rules = configuration.rules.filter { rule in
+            if compilerArguments.isEmpty {
+                return !(rule is AnalyzerRule)
+            } else {
+                return rule is AnalyzerRule
+            }
+        }
     }
 
     public func correct() -> [Correction] {
@@ -184,7 +194,7 @@ public struct Linter {
 
         var corrections = [Correction]()
         for rule in rules.compactMap({ $0 as? CorrectableRule }) {
-            let newCorrections = rule.correct(file: file)
+            let newCorrections = rule.correct(file: file, compilerArguments: compilerArguments)
             corrections += newCorrections
             if !newCorrections.isEmpty {
                 file.invalidateCache()

Source/SwiftLintFramework/Models/MasterRuleList.swift
@@ -36,6 +36,7 @@ public let masterRuleList = RuleList(rules: [
     ExplicitACLRule.self,
     ExplicitEnumRawValueRule.self,
     ExplicitInitRule.self,
+    ExplicitSelfRule.self,
     ExplicitTopLevelACLRule.self,
     ExplicitTypeInterfaceRule.self,
     ExtensionAccessModifierRule.self,

Source/SwiftLintFramework/Models/RuleDescription.swift
@@ -8,6 +8,7 @@ public struct RuleDescription: Equatable {
     public let corrections: [String: String]
     public let deprecatedAliases: Set<String>
     public let minSwiftVersion: SwiftVersion
+    public let requiresFileOnDisk: Bool
 
     public var consoleDescription: String { return "\(name) (\(identifier)): \(description)" }
 
@@ -19,7 +20,8 @@ public struct RuleDescription: Equatable {
                 minSwiftVersion: SwiftVersion = .three,
                 nonTriggeringExamples: [String] = [], triggeringExamples: [String] = [],
                 corrections: [String: String] = [:],
-                deprecatedAliases: Set<String> = []) {
+                deprecatedAliases: Set<String> = [],
+                requiresFileOnDisk: Bool = false) {
         self.identifier = identifier
         self.name = name
         self.description = description
@@ -29,6 +31,7 @@ public struct RuleDescription: Equatable {
         self.corrections = corrections
         self.deprecatedAliases = deprecatedAliases
         self.minSwiftVersion = minSwiftVersion
+        self.requiresFileOnDisk = requiresFileOnDisk
     }
 }
 

Source/SwiftLintFramework/Models/RuleList+Documentation.swift
@@ -60,15 +60,17 @@ extension RuleList {
 
     private func detailsSummary(_ rule: Rule) -> String {
         let columns = ["Identifier", "Enabled by default", "Supports autocorrection",
-                       "Kind", "Minimum Swift Compiler Version"]
+                       "Kind", "Analyzer", "Minimum Swift Compiler Version"]
         var content = columns.joined(separator: " | ") + "\n"
         content += columns.map { _ in "---" }.joined(separator: " | ") + "\n"
         let identifier = type(of: rule).description.identifier
         let defaultStatus = rule is OptInRule ? "Disabled" : "Enabled"
         let correctable = rule is CorrectableRule ? "Yes" : "No"
         let kind = type(of: rule).description.kind
+        let analyzer = rule is AnalyzerRule ? "Yes" : "No"
         let minSwiftVersion = type(of: rule).description.minSwiftVersion.rawValue
-        content += "`\(identifier)` | \(defaultStatus) | \(correctable) | \(kind) | \(minSwiftVersion) \n\n"
+        let rowMembers = ["`\(identifier)`", defaultStatus, correctable, "\(kind)", analyzer, minSwiftVersion]
+        content += rowMembers.joined(separator: " | ") + " \n\n"
 
         return content
     }

Source/SwiftLintFramework/Protocols/Rule.swift
@@ -7,11 +7,16 @@ public protocol Rule {
     init() // Rules need to be able to be initialized with default values
     init(configuration: Any) throws
 
+    func validate(file: File, compilerArguments: [String]) -> [StyleViolation]
     func validate(file: File) -> [StyleViolation]
     func isEqualTo(_ rule: Rule) -> Bool
 }
 
 extension Rule {
+    public func validate(file: File, compilerArguments: [String]) -> [StyleViolation] {
+        return validate(file: file)
+    }
+
     public func isEqualTo(_ rule: Rule) -> Bool {
         return type(of: self).description == type(of: rule).description
     }
@@ -32,10 +37,31 @@ public protocol ConfigurationProviderRule: Rule {
 }
 
 public protocol CorrectableRule: Rule {
+    func correct(file: File, compilerArguments: [String]) -> [Correction]
     func correct(file: File) -> [Correction]
 }
 
+public extension CorrectableRule {
+    func correct(file: File, compilerArguments: [String]) -> [Correction] {
+        return correct(file: file)
+    }
+}
+
 public protocol SourceKitFreeRule: Rule {}
+
+public protocol AnalyzerRule: OptInRule {}
+
+public extension AnalyzerRule {
+    func validate(file: File) -> [StyleViolation] {
+        queuedFatalError("Must call `validate(file:compilerArguments:)` for AnalyzerRule")
+    }
+}
+
+public extension AnalyzerRule where Self: CorrectableRule {
+    func correct(file: File) -> [Correction] {
+        queuedFatalError("Must call `correct(file:compilerArguments:)` for AnalyzerRule")
+    }
+}
 
 // MARK: - ConfigurationProviderRule conformance to Configurable
 

Source/SwiftLintFramework/Rules/Style/ExplicitSelfRule.swift
@@ -1,0 +1,202 @@
+import Foundation
+import SourceKittenFramework
+
+public struct ExplicitSelfRule: CorrectableRule, ConfigurationProviderRule, AnalyzerRule, AutomaticTestableRule {
+    public var configuration = SeverityConfiguration(.warning)
+
+    public init() {}
+
+    public static let description = RuleDescription(
+        identifier: "explicit_self",
+        name: "Explicit Self",
+        description: "Instance variables and functions should be explicitly accessed with 'self.'.",
+        kind: .style,
+        nonTriggeringExamples: [
+            """
+            struct A {
+                func f1() {}
+                func f2() {
+                    self.f1()
+                }
+            }
+            """,
+            """
+            struct A {
+                let p1: Int
+                func f1() {
+                    _ = self.p1
+                }
+            }
+            """
+        ],
+        triggeringExamples: [
+            """
+            struct A {
+                func f1() {}
+                func f2() {
+                    ↓f1()
+                }
+            }
+            """,
+            """
+            struct A {
+                let p1: Int
+                func f1() {
+                    _ = ↓p1
+                }
+            }
+            """
+        ],
+        corrections: [
+            """
+            struct A {
+                func f1() {}
+                func f2() {
+                    ↓f1()
+                }
+            }
+            """:
+            """
+            struct A {
+                func f1() {}
+                func f2() {
+                    self.f1()
+                }
+            }
+            """,
+            """
+            struct A {
+                let p1: Int
+                func f1() {
+                    _ = ↓p1
+                }
+            }
+            """:
+            """
+            struct A {
+                let p1: Int
+                func f1() {
+                    _ = self.p1
+                }
+            }
+            """
+        ],
+        requiresFileOnDisk: true
+    )
+
+    public func validate(file: File, compilerArguments: [String]) -> [StyleViolation] {
+        return violationRanges(in: file, compilerArguments: compilerArguments).map {
+            StyleViolation(ruleDescription: type(of: self).description,
+                           severity: configuration.severity,
+                           location: Location(file: file, characterOffset: $0.location))
+        }
+    }
+
+    public func correct(file: File, compilerArguments: [String]) -> [Correction] {
+        let violations = violationRanges(in: file, compilerArguments: compilerArguments)
+        let matches = file.ruleEnabled(violatingRanges: violations, for: self)
+        if matches.isEmpty { return [] }
+
+        var contents = file.contents.bridge()
+        let description = type(of: self).description
+        var corrections = [Correction]()
+        for range in matches.reversed() {
+            contents = contents.replacingCharacters(in: range, with: "self.").bridge()
+            let location = Location(file: file, characterOffset: range.location)
+            corrections.append(Correction(ruleDescription: description, location: location))
+        }
+        file.write(contents.bridge())
+        return corrections
+    }
+
+    private func violationRanges(in file: File, compilerArguments: [String]) -> [NSRange] {
+        guard !compilerArguments.isEmpty else {
+            queuedPrintError("""
+                Attempted to lint file at path '\(file.path ?? "...")' with the \
+                \(type(of: self).description.identifier) rule without any compiler arguments.
+                """)
+            return []
+        }
+
+        let allCursorInfo: [[String: SourceKitRepresentable]]
+        do {
+            let byteOffsets = try binaryOffsets(file: file, compilerArguments: compilerArguments)
+            allCursorInfo = try file.allCursorInfo(compilerArguments: compilerArguments,
+                                                   atByteOffsets: byteOffsets)
+        } catch {
+            queuedPrintError(String(describing: error))
+            return []
+        }
+
+        let cursorsMissingExplicitSelf = allCursorInfo.filter { cursorInfo in
+            guard let kindString = cursorInfo["key.kind"] as? String else { return false }
+            return kindsToFind.contains(kindString)
+        }
+
+        guard !cursorsMissingExplicitSelf.isEmpty else {
+            return []
+        }
+
+        let contents = file.contents.bridge()
+
+        return cursorsMissingExplicitSelf.compactMap { cursorInfo in
+            guard let byteOffset = cursorInfo["swiftlint.offset"] as? Int64 else {
+                queuedPrintError("couldn't convert offsets")
+                return nil
+            }
+
+            return contents.byteRangeToNSRange(start: Int(byteOffset), length: 0)
+        }
+    }
+}
+
+private let kindsToFind: Set = [
+    "source.lang.swift.ref.function.method.instance",
+    "source.lang.swift.ref.var.instance"
+]
+
+private extension File {
+    func allCursorInfo(compilerArguments: [String], atByteOffsets byteOffsets: [Int]) throws
+        -> [[String: SourceKitRepresentable]] {
+        return try byteOffsets.compactMap { offset in
+            if contents.bridge().substringWithByteRange(start: offset - 1, length: 1)! == "." { return nil }
+            var cursorInfo = try Request.cursorInfo(file: self.path!, offset: Int64(offset),
+                                                    arguments: compilerArguments).send()
+            cursorInfo["swiftlint.offset"] = Int64(offset)
+            return cursorInfo
+        }
+    }
+}
+
+private extension NSString {
+    func byteOffset(forLine line: Int, column: Int) -> Int {
+        var byteOffset = 0
+        for line in lines()[..<(line - 1)] {
+            byteOffset += line.byteRange.length
+        }
+        return byteOffset + column - 1
+    }
+
+    func recursiveByteOffsets(_ dict: [String: Any]) -> [Int] {
+        let cur: [Int]
+        if let line = dict["key.line"] as? Int64,
+            let column = dict["key.column"] as? Int64,
+            let kindString = dict["key.kind"] as? String,
+            kindsToFind.contains(kindString) {
+            cur = [byteOffset(forLine: Int(line), column: Int(column))]
+        } else {
+            cur = []
+        }
+        if let entities = dict["key.entities"] as? [[String: Any]] {
+            return entities.flatMap(recursiveByteOffsets) + cur
+        }
+        return cur
+    }
+}
+
+private func binaryOffsets(file: File, compilerArguments: [String]) throws -> [Int] {
+    let absoluteFile = file.path!.bridge().absolutePathRepresentation()
+    let index = try Request.index(file: absoluteFile, arguments: compilerArguments).send()
+    let binaryOffsets = file.contents.bridge().recursiveByteOffsets(index)
+    return binaryOffsets.sorted()
+}

commit d064177cdd416bafece590487dbafbec2cbfe733

Fix `swiftlinit rules` output table formatting
Fixes https://github.com/realm/SwiftLint/issues/2787

Broken since https://github.com/realm/SwiftLint/pull/2379

Source/swiftlint/Commands/RulesCommand.swift
@@ -122,7 +122,7 @@ extension TextTable {
         func truncate(_ string: String) -> String {
             let stringWithNoNewlines = string.replacingOccurrences(of: "\n", with: "\\n")
             let minWidth = "configuration".count - "...".count
-            let configurationStartColumn = 112
+            let configurationStartColumn = 124
             let truncatedEndIndex = stringWithNoNewlines.index(
                 stringWithNoNewlines.startIndex,
                 offsetBy: max(minWidth, Terminal.currentWidth() - configurationStartColumn),
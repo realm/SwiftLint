3f670162775bd3d32f9e800bbc2f1322580e0363
Dan Loman <daniel.h.loman@gmail.com>
2019-10-18 16:53:32 -0700

    Enable updating of all configuration properties

--- Source/SwiftLintFramework/Rules/RuleConfigurations/ExpiringTodoConfiguration.swift
+++ Source/SwiftLintFramework/Rules/RuleConfigurations/ExpiringTodoConfiguration.swift
@@ -2,8 +2,8 @@ public struct ExpiringTodoConfiguration: RuleConfiguration, Equatable {
     public struct DelimiterConfiguration: Equatable {
         public static let `default`: DelimiterConfiguration = .init(opening: "[", closing: "]")
 
-        let opening: Character
-        let closing: Character
+        fileprivate(set) var opening: String
+        fileprivate(set) var closing: String
     }
 
     public var consoleDescription: String {
@@ -11,19 +11,20 @@ public struct ExpiringTodoConfiguration: RuleConfiguration, Equatable {
         "(reached_or_passed_expiry_severity) \(expiredSeverity.consoleDescription)"
     }
 
-    var approachingExpirySeverity: SeverityConfiguration
-    var expiredSeverity: SeverityConfiguration
+    private(set) var approachingExpirySeverity: SeverityConfiguration
+
+    private(set) var expiredSeverity: SeverityConfiguration
 
     // swiftlint:disable todo
     /// The number of days prior to expiry before the TODO emits a violation
     // swiftlint:enable todo
-    let approachingExpiryThreshold: Int
+    private(set) var approachingExpiryThreshold: Int
     /// The opening/closing characters used to surround the expiry-date string
-    let dateDelimiters: DelimiterConfiguration
+    private(set) var dateDelimiters: DelimiterConfiguration
     /// The format which should be used to the expiry-date string into a `Date` object
-    let dateFormat: String
+    private(set) var dateFormat: String
     /// The separator used for regex detection of the expiry-date string
-    let dateSeparator: Character
+    private(set) var dateSeparator: String
 
     public init(
         approachingExpirySeverity: SeverityConfiguration,
@@ -31,7 +32,7 @@ public struct ExpiringTodoConfiguration: RuleConfiguration, Equatable {
         approachingExpiryThreshold: Int = 15,
         dateFormat: String = "MM/dd/yyyy",
         dateDelimiters: DelimiterConfiguration = .default,
-        dateSeparator: Character = "/") {
+        dateSeparator: String = "/") {
         self.approachingExpirySeverity = approachingExpirySeverity
         self.expiredSeverity = expiredSeverity
         self.approachingExpiryThreshold = approachingExpiryThreshold
@@ -50,6 +51,23 @@ public struct ExpiringTodoConfiguration: RuleConfiguration, Equatable {
         }
         if let expiredConfiguration = configurationDict["expired_severity"] {
             try expiredSeverity.apply(configuration: expiredConfiguration)
+        }
+        if let approachingExpiryThreshold = configurationDict["approaching_expiry_threshold"] as? Int {
+            self.approachingExpiryThreshold = approachingExpiryThreshold
+        }
+        if let dateFormat = configurationDict["date_format"] as? String {
+            self.dateFormat = dateFormat
+        }
+        if let dateDelimiters = configurationDict["date_delimiters"] as? [String: String] {
+            if let openingDelimiter = dateDelimiters["opening"] {
+                self.dateDelimiters.opening = openingDelimiter
+            }
+            if let closingDelimiter = dateDelimiters["closing"] {
+                self.dateDelimiters.closing = closingDelimiter
+            }
+        }
+        if let dateSeparator = configurationDict["date_separator"] as? String {
+            self.dateSeparator = dateSeparator
         }
     }
 

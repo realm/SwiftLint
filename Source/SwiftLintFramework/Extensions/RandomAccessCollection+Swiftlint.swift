fa5360fefae1f33803bbcdd295b528b0515706c2
JP Simard <jp@jpsim.com>
2019-11-06 13:36:04 -0800

    Documentation edits to RandomAccessCollection+Swiftlint.swift

--- Source/SwiftLintFramework/Extensions/RandomAccessCollection+Swiftlint.swift
+++ Source/SwiftLintFramework/Extensions/RandomAccessCollection+Swiftlint.swift
@@ -1,9 +1,8 @@
-internal extension RandomAccessCollection where Index == Int {
-    /// Returns the first index in which an element of the collection satisfies
-    /// the given predicate. The collection assumed to be sorted. If collection is not have sorted values
-    /// the result is undefined
+extension RandomAccessCollection where Index == Int {
+    /// Returns the first index in which an element of the collection satisfies the given predicate.
+    /// The collection assumed to be sorted. If collection is not have sorted values the result is undefined.
     ///
-    /// The idea  is to get first index of a function where predicate starting to return true values.
+    /// The idea is to get first index of a function for which the given predicate evaluates to true.
     ///
     ///       let values = [1,2,3,4,5]
     ///       let idx = values.firstIndexAssumingSorted(where: { $0 > 3 })
@@ -15,6 +14,7 @@ internal extension RandomAccessCollection where Index == Int {
     /// - Parameter predicate: A closure that takes an element as its argument
     ///   and returns a Boolean value that indicates whether the passed element
     ///   represents a match.
+    ///
     /// - Returns: The index of the first element for which `predicate` returns
     ///   `true`. If no elements in the collection satisfy the given predicate,
     ///   returns `nil`.
@@ -22,20 +22,20 @@ internal extension RandomAccessCollection where Index == Int {
     /// - Complexity: O(log(*n*)), where *n* is the length of the collection.
     @inlinable
     func firstIndexAssumingSorted(where predicate: (Self.Element) throws -> Bool) rethrows -> Int? {
-        // Predicate should divide a collection to two pars of vaues
+        // Predicate should divide a collection to two pairs of values
         // "bad" values for which predicate returns `false``
         // "good" values for which predicate return `true`
 
         // false false false false false true true true
         //                               ^
-        // The idea is to get _first_ index which for which predicate returns `true`
+        // The idea is to get _first_ index which for which the predicate returns `true`
 
         let lastIndex = count
 
-        // The index that represetns where bad values starts.
+        // The index that represents where bad values start
         var badIndex = -1
 
-        // The index that represetns where good values starts
+        // The index that represents where good values start
         var goodIndex = lastIndex
         var midIndex = (badIndex + goodIndex) / 2
 
@@ -48,7 +48,7 @@ internal extension RandomAccessCollection where Index == Int {
             midIndex = (badIndex + goodIndex) / 2
         }
 
-        // Corner case, we' re out of bound, no good items in array
+        // We're out of bounds, no good items in array
         if midIndex == lastIndex {
             return nil
         }

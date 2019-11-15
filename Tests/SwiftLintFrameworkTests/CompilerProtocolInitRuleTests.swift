d86e7ab2ab5a0e4569b6131a40b08b6755becda1
JP Simard <jp@jpsim.com>
2019-11-15 10:07:45 -0800

    Speed up type checking testViolationMessageForExpressibleByIntegerLiteral()
    Before: 285ms
    After: 175ms

--- Tests/SwiftLintFrameworkTests/CompilerProtocolInitRuleTests.swift
+++ Tests/SwiftLintFrameworkTests/CompilerProtocolInitRuleTests.swift
@@ -8,22 +8,18 @@ class CompilerProtocolInitRuleTests: XCTestCase {
         verifyRule(CompilerProtocolInitRule.description)
     }
 
-    func testViolationMessageForExpressibleByIntegerLiteral() {
-        guard let config = makeConfig(nil, ruleID) else {
-            XCTFail("Failed to create configuration")
-            return
-        }
+    func testViolationMessageForExpressibleByIntegerLiteral() throws {
+        let config = try XCTUnwrap(makeConfig(nil, ruleID))
         let allViolations = violations("let a = NSNumber(integerLiteral: 1)", config: config)
 
         let compilerProtocolInitViolation = allViolations.first { $0.ruleDescription.identifier == ruleID }
-        if let violation = compilerProtocolInitViolation {
-            XCTAssertEqual(
-                violation.reason,
-                "The initializers declared in compiler protocol ExpressibleByIntegerLiteral " +
-                "shouldn't be called directly."
-            )
-        } else {
-            XCTFail("A compiler protocol init violation should have been triggered!")
-        }
+        let violation = try XCTUnwrap(
+            compilerProtocolInitViolation,
+            "A compiler protocol init violation should have been triggered!"
+        )
+        XCTAssertEqual(
+            violation.reason,
+            "The initializers declared in compiler protocol ExpressibleByIntegerLiteral shouldn't be called directly."
+        )
     }
 }

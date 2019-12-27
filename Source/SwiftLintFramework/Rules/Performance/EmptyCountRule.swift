00c8bf69004a2ad56b9379855b70c5d186af80a4
Ivan Vavilov <vavilov2@gmail.com>
2019-12-27 11:20:45 +0300

    Add checking for count >= 1
    Co-Authored-By: Virgilio Favero Neto <vfn@users.noreply.github.com>

--- Source/SwiftLintFramework/Rules/Performance/EmptyCountRule.swift
+++ Source/SwiftLintFramework/Rules/Performance/EmptyCountRule.swift
@@ -38,7 +38,7 @@ public struct EmptyCountRule: ConfigurationProviderRule, OptInRule, AutomaticTes
     )
 
     public func validate(file: File) -> [StyleViolation] {
-        let pattern = "\\bcount\\s*(((==|!=|<|<=|>|>=)\\s*0(\\b|([box][0_]+\\b){1}))|<\\s*(1|0[box]0*1){1}\\b)"
+        let pattern = "\\bcount\\s*(((==|!=|<|<=|>|>=)\\s*0(\\b|([box][0_]+\\b){1}))|(<|>=)\\s*(1|0[box]0*1){1}\\b)"
         let excludingKinds = SyntaxKind.commentAndStringKinds
         return file.match(pattern: pattern, excludingSyntaxKinds: excludingKinds).map {
             StyleViolation(ruleDescription: type(of: self).description,

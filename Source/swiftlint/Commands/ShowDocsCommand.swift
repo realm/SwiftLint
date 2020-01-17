623e69e62339c27482c6c1f7adc6f785a022fd06
417-72KI <417.72ki@gmail.com>
2020-01-17 17:07:15 +0900

    Add `show-docs` command

---
+++ Source/swiftlint/Commands/ShowDocsCommand.swift
@@ -0,0 +1,29 @@
+import Commandant
+import Foundation
+
+struct ShowDocsCommand: CommandProtocol {
+    let verb = "show-docs"
+    let function = "Open `SwiftLint Framework Docs` on web browser"
+
+    func run(_ options: NoOptions<CommandantError<()>>) -> Result<(), CommandantError<()>> {
+        let url = URL(string: "https://realm.github.io/SwiftLint/rule-directory.html")!
+        open(url)
+        return .success(())
+    }
+}
+
+private extension ShowDocsCommand {
+    func open(_ url: URL) {
+        let process = Process()
+        process.launchPath = "/usr/bin/env"
+        let command: String = {
+            #if os(Linux)
+            return "xdg-open"
+            #else
+            return "open"
+            #endif
+        }()
+        process.arguments = [command, url.absoluteString]
+        process.launch()
+    }
+}

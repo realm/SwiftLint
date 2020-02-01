0fcf9b65babb6ac154c27c648241d95671ad9071
Zev Eisenberg <zev@zeveisenberg.com>
2020-01-31 21:30:16 -0500

    Do not trigger if the type has @objc members.

--- Source/SwiftLintFramework/Rules/Idiomatic/ConvenienceTypeRule.swift
+++ Source/SwiftLintFramework/Rules/Idiomatic/ConvenienceTypeRule.swift
@@ -136,6 +136,15 @@ public struct ConvenienceTypeRule: ASTRule, OptInRule, ConfigurationProviderRule
             return []
         }
 
+        // Has @objc members?
+        let hasObjcMembers = dictionary.substructure.contains { dict in
+            return dict.enclosedSwiftAttributes.contains(.objc)
+        }
+
+        guard !hasObjcMembers else {
+            return []
+        }
+
         return [
             StyleViolation(ruleDescription: Self.description,
                            severity: configuration.severity,

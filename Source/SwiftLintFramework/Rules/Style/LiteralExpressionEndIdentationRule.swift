3349b4dfcf31d0375348a9b6ea2ef6c570ac6671
Zev Eisenberg <zev@zeveisenberg.com>
2020-01-31 21:25:18 -0500

    Delete typo'd quote mark.

--- Source/SwiftLintFramework/Rules/Style/LiteralExpressionEndIdentationRule.swift
+++ Source/SwiftLintFramework/Rules/Style/LiteralExpressionEndIdentationRule.swift
@@ -24,7 +24,7 @@ public struct LiteralExpressionEndIdentationRule: Rule, ConfigurationProviderRul
             [
                1,
                2
-            "]
+            ]
             """),
             Example("""
             [

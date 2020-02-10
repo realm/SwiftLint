c47db1b093ec4817f9379321ce063fcb74e21fb3
Marcelo Fabri <me@marcelofabri.com>
2020-02-09 16:20:52 -0800

    Fix testDetectSwiftVersion with Swift 5.2 (#3093)

--- Tests/SwiftLintFrameworkTests/LinterCacheTests.swift
+++ Tests/SwiftLintFrameworkTests/LinterCacheTests.swift
@@ -301,7 +301,9 @@ class LinterCacheTests: XCTestCase {
 
     // swiftlint:disable:next function_body_length
     func testDetectSwiftVersion() {
-        #if compiler(>=5.1.4)
+        #if compiler(>=5.2.0)
+            let version = "5.2.0"
+        #elseif compiler(>=5.1.4)
             let version = "5.1.4"
         #elseif compiler(>=5.1.3)
             let version = "5.1.3"

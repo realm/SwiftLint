9651735b45ce66bc1e2294bd971785e6089cd59d
JP Simard <jp@jpsim.com>
2020-03-18 14:33:47 -0700

    Fix test

--- Tests/SwiftLintFrameworkTests/LinterCacheTests.swift
+++ Tests/SwiftLintFrameworkTests/LinterCacheTests.swift
@@ -303,6 +303,8 @@ class LinterCacheTests: XCTestCase {
     func testDetectSwiftVersion() {
         #if compiler(>=5.2.0)
             let version = "5.2.0"
+        #elseif compiler(>=5.1.5)
+            let version = "5.1.5"
         #elseif compiler(>=5.1.4)
             let version = "5.1.4"
         #elseif compiler(>=5.1.3)

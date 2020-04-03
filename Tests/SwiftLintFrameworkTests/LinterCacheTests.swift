bbfc2be453d6c3de749a76822d7182113aefc48f
JP Simard <jp@jpsim.com>
2020-04-03 16:29:30 -0700

    fixup! Update CI to run jobs with Xcode 11.0 to 11.4

--- Tests/SwiftLintFrameworkTests/LinterCacheTests.swift
+++ Tests/SwiftLintFrameworkTests/LinterCacheTests.swift
@@ -301,6 +301,8 @@ class LinterCacheTests: XCTestCase {
 
     // swiftlint:disable:next function_body_length
     func testDetectSwiftVersion() {
+        #if compiler(>=5.2.1)
+            let version = "5.2.1"
         #if compiler(>=5.2.0)
             let version = "5.2.0"
         #elseif compiler(>=5.1.5)

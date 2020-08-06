4756ce6b69dece865c8f5608f4e36ec1b89d5b4a
Marcelo Fabri <me@marcelofabri.com>
2020-08-05 21:46:33 -0700

    Ignore local vars and params

--- Source/SwiftLintFramework/Rules/Idiomatic/ExtensionAccessModifierRule.swift
+++ Source/SwiftLintFramework/Rules/Idiomatic/ExtensionAccessModifierRule.swift
@@ -78,6 +78,16 @@ public struct ExtensionAccessModifierRule: ASTRule, ConfigurationProviderRule, O
                public ↓func bar() {}
                public ↓func baz() {}
             }
+            """),
+            Example("""
+            ↓extension Foo {
+               public var bar: Int {
+                  let value = 1
+                  return value
+               }
+
+               public var baz: Int { return 1 }
+            }
             """)
         ]
     )
@@ -92,9 +102,9 @@ public struct ExtensionAccessModifierRule: ASTRule, ConfigurationProviderRule, O
 
         let declarations = dictionary.substructure
             .compactMap { entry -> (acl: AccessControlLevel, offset: ByteCount)? in
-                guard entry.declarationKind != nil,
-                    let offset = entry.offset
-                else {
+                guard let kind = entry.declarationKind,
+                      kind != .varLocal, kind != .varParameter,
+                      let offset = entry.offset else {
                     return nil
                 }
 

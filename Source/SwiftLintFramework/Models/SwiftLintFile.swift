9b93b3efb8d63a5b562d63b082f0860d33b5f789
JP Simard <jp@jpsim.com>
2020-09-17 07:35:05 -0700

    [SwiftLintFile] Remove lock in favor of UUID (#3347)
    We were using this lock to guarantee a new ID for every file, but we can
    get that benefit by using values that are guaranteed to be unique
    without the need for locks, such as a UUID.

--- Source/SwiftLintFramework/Models/SwiftLintFile.swift
+++ Source/SwiftLintFramework/Models/SwiftLintFile.swift
@@ -3,25 +3,15 @@ import SourceKittenFramework
 
 /// A unit of Swift source code, either on disk or in memory.
 public final class SwiftLintFile {
-    private static var id = 0
-    private static var lock = NSLock()
-
-    private static func nextID () -> Int {
-        lock.lock()
-        defer { lock.unlock() }
-        id += 1
-        return id
-    }
-
     let file: File
-    let id: Int
+    let id: UUID
 
     /// Creates a `SwiftLintFile` with a SourceKitten `File`.
     ///
     /// - parameter file: A file from SourceKitten.
     public init(file: File) {
         self.file = file
-        self.id = SwiftLintFile.nextID()
+        self.id = UUID()
     }
 
     /// Creates a `SwiftLintFile` by specifying its path on disk.

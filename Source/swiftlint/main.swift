1aec09282d137b5424fcb5f2ea73325237ab3d4c
JP Simard <jp@jpsim.com>
2020-12-11 13:02:05 -0500

    fixup! Remove unused imports

--- Source/swiftlint/main.swift
+++ Source/swiftlint/main.swift
@@ -1,4 +1,11 @@
 import Dispatch
+#if canImport(Darwin)
+import Darwin
+#elseif canImport(Glibc)
+import Glibc
+#else
+#error("Unsupported platform")
+#endif
 
 DispatchQueue.global().async {
     SwiftLint.mainHandlingDeprecatedCommands()

61416d42a1ab1d3be64150f860f729fc56bc4406
JP Simard <jp@jpsim.com>
2021-02-23 13:55:06 -0500

    fixup! Make strong-iboutlet rule correctable

--- Source/SwiftLintFramework/Rules/Lint/StrongIBOutletRule.swift
+++ Source/SwiftLintFramework/Rules/Lint/StrongIBOutletRule.swift
@@ -34,7 +34,7 @@ public struct StrongIBOutletRule: ConfigurationProviderRule, ASTRule, Substituti
     public func validate(file: SwiftLintFile, kind: SwiftDeclarationKind,
                          dictionary: SourceKittenDictionary) -> [StyleViolation] {
         return violationRanges(in: file, kind: kind, dictionary: dictionary).map {
-            StyleViolation(ruleDescription: type(of: self).description,
+            StyleViolation(ruleDescription: Self.description,
                            severity: configuration.severity,
                            location: Location(file: file, characterOffset: $0.location))
         }

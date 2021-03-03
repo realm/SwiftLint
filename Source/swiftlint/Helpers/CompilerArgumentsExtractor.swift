eec4c58e1f68ad3a2eea4605a05071dd462321f6
JP Simard <jp@jpsim.com>
2021-03-03 10:14:51 -0500

    Fix parsing xcode logs for analyzer rules for target names with spaces

--- Source/swiftlint/Helpers/CompilerArgumentsExtractor.swift
+++ Source/swiftlint/Helpers/CompilerArgumentsExtractor.swift
@@ -2,61 +2,17 @@ import Foundation
 import SourceKittenFramework
 
 struct CompilerArgumentsExtractor {
-    static func allCompilerInvocations(compilerLogs: String) -> [String] {
-        var compilerInvocations = [String]()
+    static func allCompilerInvocations(compilerLogs: String) -> [[String]] {
+        var compilerInvocations = [[String]]()
         compilerLogs.enumerateLines { line, _ in
             if let swiftcIndex = line.range(of: "swiftc ")?.upperBound, line.contains(" -module-name ") {
-                let invocation = line[swiftcIndex...]
-                    .components(separatedBy: " ")
+                let invocation = parseCLIArguments(String(line[swiftcIndex...]))
                     .expandingResponseFiles
-                    .joined(separator: " ")
+                    .filteringCompilerArguments
                 compilerInvocations.append(invocation)
             }
         }
-
         return compilerInvocations
-    }
-
-    static func compilerArgumentsForFile(_ sourceFile: String, compilerInvocations: [String]) -> [String]? {
-        let escapedSourceFile = sourceFile.replacingOccurrences(of: " ", with: "\\ ")
-        guard let compilerInvocation = compilerInvocations.first(where: { $0.contains(escapedSourceFile) }) else {
-            return nil
-        }
-
-        return parseCLIArguments(compilerInvocation)
-    }
-
-    /**
-     Filters compiler arguments from `xcodebuild` to something that SourceKit/Clang will accept.
-
-     - parameter args: Compiler arguments, as parsed from `xcodebuild`.
-
-     - returns: Filtered compiler arguments.
-     */
-    static func filterCompilerArguments(_ args: [String]) -> [String] {
-        var args = args
-        // https://github.com/realm/SwiftLint/issues/3365
-        args = args.map { $0.replacingOccurrences(of: "\\=", with: "=") }
-        args.append(contentsOf: ["-D", "DEBUG"])
-        var shouldContinueToFilterArguments = true
-        while shouldContinueToFilterArguments {
-            (args, shouldContinueToFilterArguments) = partiallyFilter(arguments: args)
-        }
-        return args.filter {
-            ![
-                "-parseable-output",
-                "-incremental",
-                "-serialize-diagnostics",
-                "-emit-dependencies"
-            ].contains($0)
-        }.map {
-            if $0 == "-O" {
-                return "-Onone"
-            } else if $0 == "-DNDEBUG=1" {
-                return "-DDEBUG=1"
-            }
-            return $0
-        }
     }
 }
 
@@ -99,12 +55,10 @@ private func parseCLIArguments(_ string: String) -> [String] {
         _ = scanner.scanString("\"")
         didStart.toggle()
     }
-    return CompilerArgumentsExtractor.filterCompilerArguments(
-        str.trimmingCharacters(in: .whitespaces)
-            .replacingOccurrences(of: "\\ ", with: escapedSpacePlaceholder)
-            .components(separatedBy: " ")
-            .map { $0.replacingOccurrences(of: escapedSpacePlaceholder, with: " ") }
-    )
+    return str.trimmingCharacters(in: .whitespaces)
+        .replacingOccurrences(of: "\\ ", with: escapedSpacePlaceholder)
+        .components(separatedBy: " ")
+        .map { $0.replacingOccurrences(of: escapedSpacePlaceholder, with: " ") }
 }
 
 /**
@@ -125,9 +79,9 @@ private func partiallyFilter(arguments args: [String]) -> ([String], Bool) {
     return (args, true)
 }
 
-private extension Array where Element == String {
+extension Array where Element == String {
     /// Return the full list of compiler arguments, replacing any response files with their contents.
-    var expandingResponseFiles: [String] {
+    fileprivate var expandingResponseFiles: [String] {
         return flatMap { arg -> [String] in
             guard arg.starts(with: "@") else {
                 return [arg]
@@ -138,6 +92,38 @@ private extension Array where Element == String {
                   .components(separatedBy: "\n")
                   .expandingResponseFiles
             } ?? [arg]
+        }
+    }
+
+    /// Returns filtered compiler arguments from `xcodebuild` to something that SourceKit/Clang will accept.
+    var filteringCompilerArguments: [String] {
+        var args = self
+        if args.first == "swiftc" {
+            args.removeFirst()
+        }
+
+        // https://github.com/realm/SwiftLint/issues/3365
+        args = args.map { $0.replacingOccurrences(of: "\\=", with: "=") }
+        args.append(contentsOf: ["-D", "DEBUG"])
+        var shouldContinueToFilterArguments = true
+        while shouldContinueToFilterArguments {
+            (args, shouldContinueToFilterArguments) = partiallyFilter(arguments: args)
+        }
+
+        return args.filter {
+            ![
+                "-parseable-output",
+                "-incremental",
+                "-serialize-diagnostics",
+                "-emit-dependencies"
+            ].contains($0)
+        }.map {
+            if $0 == "-O" {
+                return "-Onone"
+            } else if $0 == "-DNDEBUG=1" {
+                return "-DDEBUG=1"
+            }
+            return $0
         }
     }
 }

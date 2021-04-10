3b2bb04f790af9e6a262831f4d1a56d59c503152
Iulian Onofrei <iulian.onofrei@yahoo.com>
2021-04-10 17:16:04 +0300

    Allow opening brace on newline in case of multiline class or struct

--- Source/SwiftLintFramework/Rules/RuleConfigurations/OpeningBraceConfiguration.swift
+++ Source/SwiftLintFramework/Rules/RuleConfigurations/OpeningBraceConfiguration.swift
@@ -1,14 +1,17 @@
 private enum ConfigurationKey: String {
     case severity = "severity"
+    case allowMultilineClassStruct = "allow_multiline_class_struct"
     case allowMultilineFunc = "allow_multiline_func"
 }
 
 public struct OpeningBraceConfiguration: RuleConfiguration, Equatable {
     private(set) var severityConfiguration = SeverityConfiguration(.warning)
+    private(set) var allowMultilineClassStruct = false
     private(set) var allowMultilineFunc = false
 
     public var consoleDescription: String {
         return [severityConfiguration.consoleDescription,
+                "\(ConfigurationKey.allowMultilineClassStruct): \(allowMultilineClassStruct)",
                 "\(ConfigurationKey.allowMultilineFunc): \(allowMultilineFunc)"].joined(separator: ", ")
     }
 
@@ -21,6 +24,7 @@ public struct OpeningBraceConfiguration: RuleConfiguration, Equatable {
             try severityConfiguration.apply(configuration: severityString)
         }
 
+        allowMultilineClassStruct = configuration[ConfigurationKey.allowMultilineClassStruct.rawValue] as? Bool ?? false
         allowMultilineFunc = configuration[ConfigurationKey.allowMultilineFunc.rawValue] as? Bool ?? false
     }
 }

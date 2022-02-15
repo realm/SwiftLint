51d97a20374d69671e7bbfb85fa135cc772085a2
JP Simard <jp@jpsim.com>
2022-02-15 09:24:19 -0500

    Add 5.5.3 to `testDetectSwiftVersion()`

--- Tests/SwiftLintFrameworkTests/SwiftVersionTests.swift
+++ Tests/SwiftLintFrameworkTests/SwiftVersionTests.swift
@@ -6,6 +6,8 @@ final class SwiftVersionTests: XCTestCase {
     func testDetectSwiftVersion() {
         #if compiler(>=5.6.0)
             let version = "5.6.0"
+        #elseif compiler(>=5.5.3)
+            let version = "5.5.3"
         #elseif compiler(>=5.5.2)
             let version = "5.5.2"
         #elseif compiler(>=5.5.1)

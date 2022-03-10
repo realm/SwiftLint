eaf955431d64a4203d291e24f8887bd24b12d8bb
funzin <nakazawa.fumito@gmail.com>
2022-03-10 23:02:47 +0900

    Use pathos as glob

--- Source/SwiftLintFramework/Helpers/Glob.swift
+++ Source/SwiftLintFramework/Helpers/Glob.swift
@@ -1,16 +1,5 @@
 import Foundation
-
-#if canImport(Darwin)
-import Darwin
-
-private let globFunction = Darwin.glob
-#elseif canImport(Glibc)
-import Glibc
-
-private let globFunction = Glibc.glob
-#else
-#error("Unsupported platform")
-#endif
+import Pathos
 
 struct Glob {
     static func resolveGlob(_ pattern: String) -> [String] {
@@ -19,22 +8,7 @@ struct Glob {
             return [pattern]
         }
 
-        var globResult = glob_t()
-        defer { globfree(&globResult) }
-
-        let flags = GLOB_TILDE | GLOB_BRACE | GLOB_MARK
-        guard globFunction(pattern.cString(using: .utf8)!, flags, nil, &globResult) == 0 else {
-            return []
-        }
-
-#if os(Linux)
-        let matchCount = globResult.gl_pathc
-#else
-        let matchCount = globResult.gl_matchc
-#endif
-
-        return (0..<Int(matchCount)).compactMap { index in
-            return globResult.gl_pathv[index].flatMap { String(validatingUTF8: $0) }
-        }
+        let paths = try? Path(pattern).glob()
+        return paths?.compactMap { $0.description } ?? []
     }
 }

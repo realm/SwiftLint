7d734cab7e3976ec244418a3fb555c6ca2eec0da
JP Simard <jp@jpsim.com>
2022-03-23 12:43:28 -0400

    Improve docstrings for `StringView+SwiftSyntax.swift`

--- Source/SwiftLintFramework/Extensions/StringView+SwiftSyntax.swift
+++ Source/SwiftLintFramework/Extensions/StringView+SwiftSyntax.swift
@@ -3,21 +3,23 @@ import SourceKittenFramework
 import SwiftSyntax
 
 extension StringView {
-    /// Converts two absolute positions from SwiftSyntax  to valid NSRange if possible
-    /// - Parameters:
-    ///   - start: starting poisiition
-    ///   - end: end position
-    /// - Returns: NSRange or nil in case of empty string
+    /// Converts two absolute positions from SwiftSyntax to a valid `NSRange` if possible.
+    ///
+    /// - parameter start: Starting position.
+    /// - parameter end:   End position.
+    ///
+    /// - returns: `NSRange` or nil in case of empty string.
     func NSRange(start: AbsolutePosition, end: AbsolutePosition) -> NSRange? {
-        precondition(end.utf8Offset >= start.utf8Offset, "End position should be beigger than start position")
+        precondition(end.utf8Offset >= start.utf8Offset, "End position should be bigger than start position")
         return NSRange(start: start, length: end.utf8Offset - start.utf8Offset)
     }
 
-    /// Converts absolute position with length from SwiftSyntax  to valid NSRange if possible
-    /// - Parameters:
-    ///   - start: starting position
-    ///   - length: length in bytes
-    /// - Returns: NSRange or nil in case of empty string
+    /// Converts absolute position with length from SwiftSyntax to a valid `NSRange` if possible.
+    ///
+    /// - parameter start:  Starting position.
+    /// - parameter length: Length in bytes.
+    ///
+    /// - returns: `NSRange` or nil in case of empty string.
     private func NSRange(start: AbsolutePosition, length: Int) -> NSRange? {
         let byteRange = ByteRange(location: ByteCount(start.utf8Offset), length: ByteCount(length))
         return byteRangeToNSRange(byteRange)

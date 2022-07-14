6b03baa42c8ee47b03868fd3c765e5ed741a02fe
Ryan Aveo <raveo@uber.com>
2022-07-13 17:26:16 -0700

    Delete unused parsing error function

--- Source/SwiftLintFramework/Helpers/SwiftSyntaxParsingErrors.swift
+++
@@ -1,8 +0,0 @@
-/// Emits an error when a source file cannot be parsed with SwiftSyntax.
-public func warnSwiftSyntaxParserFailure(for ruleName: String, in fileName: String?) {
-    var message: String = "Rule \(ruleName) is disabled because the Swift Syntax tree could not be parsed."
-    if let fileName = fileName {
-        message += "\n SwiftSyntax could not parse file: \(fileName)"
-    }
-    queuedPrintError(message)
-}

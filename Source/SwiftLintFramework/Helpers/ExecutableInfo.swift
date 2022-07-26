6dc8da85baa08febd0434ad5554677e0db71852d
JP Simard <jp@jpsim.com>
2022-07-26 12:10:11 -0400

    Use dlopen/dlsym to resolve the `_mh_execute_header` pointer
    https://stackoverflow.com/a/49438718/373262
    
    Co-authored-by: Keith Smiley <keithbsmiley@gmail.com>

--- Source/SwiftLintFramework/Helpers/ExecutableInfo.swift
+++ Source/SwiftLintFramework/Helpers/ExecutableInfo.swift
@@ -4,20 +4,15 @@ import MachO
 
 enum ExecutableInfo {
     static let buildID: String? = {
-        // It should be possible to read the UUID directly from the `_mh_execute_header` but that fails with
-        // Fatal error: load from misaligned raw pointer
-        //
-        //   var header = _mh_execute_header
-        //   let uuidString = withUnsafePointer(to: &header) { header in
-        //       return getUUID(pointer: header)?.uuidString
-        //   }
+        if let handle = dlopen(nil, RTLD_LAZY) {
+            defer { dlclose(handle) }
 
-        // This works in pure Swift but requires reading the entire executable from disk, so it's 100x slower than
-        // the Objective-C version
-        let execData = try? Data(contentsOf: URL(fileURLWithPath: ProcessInfo.processInfo.arguments[0]))
-        return execData?.withUnsafeBytes { pointer -> String? in
-            return getUUID(pointer: pointer.baseAddress!)?.uuidString
+            if let ptr = dlsym(handle, MH_EXECUTE_SYM) {
+                return getUUID(pointer: ptr)?.uuidString
+            }
         }
+
+        return nil
     }()
 
     private static func getUUID(pointer: UnsafeRawPointer) -> UUID? {

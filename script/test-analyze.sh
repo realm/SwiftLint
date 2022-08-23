ebe4b97a13a859e421463c0ea0f688e2e3d61e35
JP Simard <jp@jpsim.com>
2022-08-23 09:12:31 -0400

    Remove exit 1

--- script/test-analyze.sh
+++ script/test-analyze.sh
@@ -6,4 +6,3 @@ readonly swiftlint="$RUNFILES_DIR/SwiftLint/swiftlint"
 readonly swiftpm_yaml="$RUNFILES_DIR/SwiftLint/swiftpm.yaml"
 cd "$PROJECT_ROOT"
 "$swiftlint" analyze --strict --compile-commands "$swiftpm_yaml"
-exit 1

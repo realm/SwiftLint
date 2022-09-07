80c8e5d37a473c98f71832997ca2a41a83465d77
JP Simard <jp@jpsim.com>
2022-09-07 12:20:26 -0400

    Rewrite `closing_brace` with SwiftSyntax (#4173)

--- Source/SwiftLintFramework/Rules/Style/ClosingBraceRule.swift
+++ Source/SwiftLintFramework/Rules/Style/ClosingBraceRule.swift
@@ -1,7 +1,6 @@
-import Foundation
-import SourceKittenFramework
+import SwiftSyntax
 
-public struct ClosingBraceRule: SubstitutionCorrectableRule, ConfigurationProviderRule {
+public struct ClosingBraceRule: SwiftSyntaxCorrectableRule, ConfigurationProviderRule {
     public var configuration = SeverityConfiguration(.warning)
 
     public init() {}
@@ -9,8 +8,7 @@ public struct ClosingBraceRule: SubstitutionCorrectableRule, ConfigurationProvid
     public static let description = RuleDescription(
         identifier: "closing_brace",
         name: "Closing Brace Spacing",
-        description: "Closing brace with closing parenthesis " +
-                     "should not have any whitespaces in the middle.",
+        description: "Closing brace with closing parenthesis should not have any whitespaces in the middle.",
         kind: .style,
         nonTriggeringExamples: [
             Example("[].map({ })"),
@@ -25,19 +23,85 @@ public struct ClosingBraceRule: SubstitutionCorrectableRule, ConfigurationProvid
         ]
     )
 
-    public func validate(file: SwiftLintFile) -> [StyleViolation] {
-        return violationRanges(in: file).map {
-            StyleViolation(ruleDescription: Self.description,
-                           severity: configuration.severity,
-                           location: Location(file: file, characterOffset: $0.location))
+    public func makeVisitor(file: SwiftLintFile) -> ViolationsSyntaxVisitor? {
+        Visitor()
+    }
+
+    public func makeRewriter(file: SwiftLintFile) -> ViolationsSyntaxRewriter? {
+        file.locationConverter.map { locationConverter in
+            Rewriter(
+                locationConverter: locationConverter,
+                disabledRegions: disabledRegions(file: file)
+            )
+        }
+    }
+}
+
+private extension ClosingBraceRule {
+    private final class Visitor: SyntaxVisitor, ViolationsSyntaxVisitor {
+        private(set) var violationPositions: [AbsolutePosition] = []
+
+        override func visitPost(_ node: TokenSyntax) {
+            if node.hasClosingBraceViolation {
+                violationPositions.append(node.positionAfterSkippingLeadingTrivia)
+            }
         }
     }
 
-    public func violationRanges(in file: SwiftLintFile) -> [NSRange] {
-        return file.match(pattern: "(\\}[ \\t]+\\))", excludingSyntaxKinds: SyntaxKind.commentAndStringKinds)
+    private final class Rewriter: SyntaxRewriter, ViolationsSyntaxRewriter {
+        private(set) var correctionPositions: [AbsolutePosition] = []
+        let locationConverter: SourceLocationConverter
+        let disabledRegions: [SourceRange]
+
+        init(locationConverter: SourceLocationConverter, disabledRegions: [SourceRange]) {
+            self.locationConverter = locationConverter
+            self.disabledRegions = disabledRegions
+        }
+
+        override func visit(_ node: TokenSyntax) -> Syntax {
+            guard node.hasClosingBraceViolation else {
+                return super.visit(node)
+            }
+
+            let isInDisabledRegion = disabledRegions.contains { region in
+                region.contains(node.positionAfterSkippingLeadingTrivia, locationConverter: locationConverter)
+            }
+
+            guard !isInDisabledRegion else {
+                return super.visit(node)
+            }
+
+            correctionPositions.append(node.positionAfterSkippingLeadingTrivia)
+            return super.visit(node.withTrailingTrivia(.zero))
+        }
+    }
+}
+
+private extension TokenSyntax {
+    var hasClosingBraceViolation: Bool {
+        guard tokenKind == .rightBrace,
+           let nextToken = nextToken,
+           nextToken.tokenKind == .rightParen
+        else {
+            return false
+        }
+
+        let isImmediatelyNext = positionAfterSkippingLeadingTrivia
+            == nextToken.positionAfterSkippingLeadingTrivia - SourceLength(utf8Length: 1)
+        if isImmediatelyNext || nextToken.hasLeadingNewline {
+            return false
+        } else {
+            return true
+        }
     }
 
-    public func substitution(for violationRange: NSRange, in file: SwiftLintFile) -> (NSRange, String)? {
-        return (violationRange, "})")
+    private var hasLeadingNewline: Bool {
+        leadingTrivia.contains { piece in
+            if case .newlines = piece {
+                return true
+            } else {
+                return false
+            }
+        }
     }
 }

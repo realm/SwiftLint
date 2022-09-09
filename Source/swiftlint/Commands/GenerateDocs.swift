20b302f6928f3efa8a41d86cdc24759624f04e60
Kattouf <vasiliy.kattouf@gmail.com>
2022-09-09 15:43:38 +0300

    added rules filter functionality to GenerateDocs command

--- Source/swiftlint/Commands/GenerateDocs.swift
+++ Source/swiftlint/Commands/GenerateDocs.swift
@@ -4,13 +4,22 @@ import SwiftLintFramework
 
 extension SwiftLint {
     struct GenerateDocs: ParsableCommand {
-        static let configuration = CommandConfiguration(abstract: "Generates markdown documentation for all rules")
+        static let configuration = CommandConfiguration(
+          abstract: "Generates markdown documentation for selected group of rules"
+        )
 
         @Option(help: "The directory where the documentation should be saved")
         var path = "rule_docs"
+        @Option(help: "The path to a SwiftLint configuration file")
+        var config: String?
+        @OptionGroup var rulesFilterOptions: RulesFilterOptions
 
         func run() throws {
-            try RuleListDocumentation(primaryRuleList)
+            let configuration = Configuration(configurationFiles: [config].compactMap({ $0 }))
+            let rulesFilter = RulesFilter(enabledRules: configuration.rules)
+            let rules = rulesFilter.getRules(excluding: .excludingOptions(byCommandLineOptions: rulesFilterOptions))
+
+            try RuleListDocumentation(rules)
                 .write(to: URL(fileURLWithPath: path, isDirectory: true))
             ExitHelper.successfullyExit()
         }

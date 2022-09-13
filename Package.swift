fbe87eff74038fe9e5533fba4ea2780e1922835a
JP Simard <jp@jpsim.com>
2022-09-13 17:37:41 -0400

    Update SwiftSyntax & internal parser for Swift 5.7

--- Package.swift
+++ Package.swift
@@ -28,7 +28,7 @@ let package = Package(
     ],
     dependencies: [
         .package(name: "swift-argument-parser", url: "https://github.com/apple/swift-argument-parser.git", .upToNextMinor(from: "1.1.3")),
-        .package(name: "SwiftSyntax", url: "https://github.com/apple/swift-syntax.git", .exact("0.50600.1")),
+        .package(name: "SwiftSyntax", url: "https://github.com/apple/swift-syntax.git", branch: "swift-5.7-DEVELOPMENT-SNAPSHOT-2022-08-30-a"),
         .package(url: "https://github.com/jpsim/SourceKitten.git", from: "0.33.0"),
         .package(url: "https://github.com/jpsim/Yams.git", from: "5.0.1"),
         .package(url: "https://github.com/scottrhoyt/SwiftyTextTable.git", from: "0.9.0"),
@@ -63,7 +63,7 @@ let package = Package(
         ),
     ] + (staticSwiftSyntax ? [.binaryTarget(
             name: "lib_InternalSwiftSyntaxParser",
-            url: "https://github.com/keith/StaticInternalSwiftSyntaxParser/releases/download/5.6/lib_InternalSwiftSyntaxParser.xcframework.zip",
-            checksum: "88d748f76ec45880a8250438bd68e5d6ba716c8042f520998a438db87083ae9d"
+            url: "https://github.com/keith/StaticInternalSwiftSyntaxParser/releases/download/5.7/lib_InternalSwiftSyntaxParser.xcframework.zip",
+            checksum: "99803975d10b2664fc37cc223a39b4e37fe3c79d3d6a2c44432007206d49db15"
         )] : [])
 )

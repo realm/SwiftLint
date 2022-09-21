37213ce4698f6fb08bec5a2b153cf0b5c21e83b8
Martin Redington <martin.redington@hudl.com>
2022-09-21 18:39:14 +0100

    Added test_parent_classes option for empty_xctest_method

--- Source/SwiftLintFramework/Rules/Lint/EmptyXCTestMethodRule.swift
+++ Source/SwiftLintFramework/Rules/Lint/EmptyXCTestMethodRule.swift
@@ -1,7 +1,7 @@
 import SwiftSyntax
 
 public struct EmptyXCTestMethodRule: OptInRule, ConfigurationProviderRule, SwiftSyntaxRule {
-    public var configuration = SeverityConfiguration(.warning)
+    public var configuration = EmptyXCTestMethodConfiguration()
 
     public init() {}
 
@@ -15,15 +15,28 @@ public struct EmptyXCTestMethodRule: OptInRule, ConfigurationProviderRule, Swift
     )
 
     public func makeVisitor(file: SwiftLintFile) -> ViolationsSyntaxVisitor? {
-        EmptyXCTestMethodRuleVisitor()
+        EmptyXCTestMethodRuleVisitor(testParentClasses: configuration.testParentClasses)
+    }
+    
+    public func makeViolation(file: SwiftLintFile, position: AbsolutePosition) -> StyleViolation {
+        StyleViolation(
+            ruleDescription: Self.description,
+            severity: configuration.severity,
+            location: Location(file: file, position: position)
+        )
     }
 }
 
 private final class EmptyXCTestMethodRuleVisitor: SyntaxVisitor, ViolationsSyntaxVisitor {
     private(set) var violationPositions: [AbsolutePosition] = []
+    private let testParentClasses: Set<String>
 
+    init(testParentClasses: Set<String>) {
+        self.testParentClasses = testParentClasses
+    }
+    
     override func visit(_ node: ClassDeclSyntax) -> SyntaxVisitorContinueKind {
-        node.isXCTestCase ? .visitChildren : .skipChildren
+        isNodeATestCase(node) ? .visitChildren : .skipChildren
     }
 
     override func visit(_ node: ExtensionDeclSyntax) -> SyntaxVisitorContinueKind {
@@ -43,16 +56,18 @@ private final class EmptyXCTestMethodRuleVisitor: SyntaxVisitor, ViolationsSynta
             violationPositions.append(node.funcKeyword.positionAfterSkippingLeadingTrivia)
         }
     }
+    
+    private func isNodeATestCase(_ node: ClassDeclSyntax) -> Bool {
+        testParentClasses.intersection(node.inheritedTypes).isNotEmpty
+    }
 }
 
 private extension ClassDeclSyntax {
-    var isXCTestCase: Bool {
+    var inheritedTypes: [String] {
         guard let inheritanceList = inheritanceClause?.inheritedTypeCollection else {
-            return false
+            return []
         }
-        return inheritanceList.contains { type in
-            type.typeName.as(SimpleTypeIdentifierSyntax.self)?.name.text == "XCTestCase"
-        }
+        return inheritanceList.compactMap { $0.typeName.as(SimpleTypeIdentifierSyntax.self)?.name.text }
     }
 }
 

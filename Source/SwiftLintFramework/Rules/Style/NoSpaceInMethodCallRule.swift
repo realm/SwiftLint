a4c46f624ddc84e7d245e5ce230e7c8a965aeddb
Marcelo Fabri <me@marcelofabri.com>
2022-09-25 02:55:00 -0700

    Rewrite `no_space_in_method_call` with SwiftSyntax (#4236)

--- Source/SwiftLintFramework/Rules/Style/NoSpaceInMethodCallRule.swift
+++ Source/SwiftLintFramework/Rules/Style/NoSpaceInMethodCallRule.swift
@@ -1,7 +1,6 @@
-import Foundation
-import SourceKittenFramework
+import SwiftSyntax
 
-public struct NoSpaceInMethodCallRule: SubstitutionCorrectableASTRule, ConfigurationProviderRule {
+public struct NoSpaceInMethodCallRule: SwiftSyntaxCorrectableRule, ConfigurationProviderRule {
     public var configuration = SeverityConfiguration(.warning)
 
     public init() {}
@@ -11,7 +10,22 @@ public struct NoSpaceInMethodCallRule: SubstitutionCorrectableASTRule, Configura
         name: "No Space in Method Call",
         description: "Don't add a space between the method name and the parentheses.",
         kind: .style,
-        nonTriggeringExamples: nonTriggeringExamples,
+        nonTriggeringExamples: [
+            Example("foo()"),
+            Example("object.foo()"),
+            Example("object.foo(1)"),
+            Example("object.foo(value: 1)"),
+            Example("object.foo { print($0 }"),
+            Example("list.sorted { $0.0 < $1.0 }.map { $0.value }"),
+            Example("self.init(rgb: (Int) (colorInt))"),
+            Example("""
+            Button {
+                print("Button tapped")
+            } label: {
+                Text("Button")
+            }
+            """)
+        ],
         triggeringExamples: [
             Example("foo↓ ()"),
             Example("object.foo↓ ()"),
@@ -31,83 +45,75 @@ public struct NoSpaceInMethodCallRule: SubstitutionCorrectableASTRule, Configura
         ]
     )
 
-    private static let nonTriggeringExamples: [Example] = [
-        Example("foo()"),
-        Example("object.foo()"),
-        Example("object.foo(1)"),
-        Example("object.foo(value: 1)"),
-        Example("object.foo { print($0 }"),
-        Example("list.sorted { $0.0 < $1.0 }.map { $0.value }"),
-        Example("self.init(rgb: (Int) (colorInt))"),
-        Example("""
-        Button {
-            print("Button tapped")
-        } label: {
-            Text("Button")
-        }
-        """)
-    ]
-
-    // MARK: - ASTRule
-
-    public func validate(file: SwiftLintFile,
-                         kind: SwiftExpressionKind,
-                         dictionary: SourceKittenDictionary) -> [StyleViolation] {
-        return violationRanges(in: file, kind: kind, dictionary: dictionary).map {
-            StyleViolation(ruleDescription: Self.description,
-                           severity: configuration.severity,
-                           location: Location(file: file, characterOffset: $0.location))
-        }
+    public func makeVisitor(file: SwiftLintFile) -> ViolationsSyntaxVisitor? {
+        Visitor()
     }
 
-    // MARK: - SubstitutionCorrectableASTRule
-
-    public func substitution(for violationRange: NSRange, in file: SwiftLintFile) -> (NSRange, String)? {
-        return (violationRange, "")
-    }
-
-    public func violationRanges(in file: SwiftLintFile,
-                                kind: SwiftExpressionKind,
-                                dictionary: SourceKittenDictionary) -> [NSRange] {
-        guard kind == .call,
-            let bodyOffset = dictionary.bodyOffset,
-            let nameOffset = dictionary.nameOffset,
-            let nameLength = dictionary.nameLength,
-            nameLength > 0,
-            case let nameEndPosition = nameOffset + nameLength,
-            bodyOffset != nameEndPosition + 1,
-            case let contents = file.stringView,
-            case let byteRange = ByteRange(location: nameEndPosition, length: bodyOffset - nameEndPosition - 1),
-            let range = contents.byteRangeToNSRange(byteRange)
-        else {
-            return []
+    public func makeRewriter(file: SwiftLintFile) -> ViolationsSyntaxRewriter? {
+        file.locationConverter.map { locationConverter in
+            Rewriter(
+                locationConverter: locationConverter,
+                disabledRegions: disabledRegions(file: file)
+            )
         }
-
-        // Don't trigger if it's a single parameter trailing closure without parens
-        if let closureBodyOffset = dictionary.substructure.lazy.compactMap({ $0.closureBodyOffset }).first,
-            closureBodyOffset == bodyOffset {
-            return []
-        }
-
-        // Don't trigger if it's a typecast
-        if let name = dictionary.name, name.hasPrefix("("), name.hasSuffix(")") {
-            return []
-        }
-
-        return [range]
     }
 }
 
-private extension SourceKittenDictionary {
-    var closureBodyOffset: ByteCount? {
-        if expressionKind == .closure {
-            return bodyOffset
+private extension NoSpaceInMethodCallRule {
+    final class Visitor: SyntaxVisitor, ViolationsSyntaxVisitor {
+        private(set) var violationPositions: [AbsolutePosition] = []
+
+        override func visitPost(_ node: FunctionCallExprSyntax) {
+            guard node.hasNoSpaceInMethodCallViolation else {
+                return
+            }
+
+            violationPositions.append(node.calledExpression.endPositionBeforeTrailingTrivia)
+        }
+    }
+
+    final class Rewriter: SyntaxRewriter, ViolationsSyntaxRewriter {
+        private(set) var correctionPositions: [AbsolutePosition] = []
+        let locationConverter: SourceLocationConverter
+        let disabledRegions: [SourceRange]
+
+        init(locationConverter: SourceLocationConverter, disabledRegions: [SourceRange]) {
+            self.locationConverter = locationConverter
+            self.disabledRegions = disabledRegions
         }
 
-        if expressionKind == .argument, substructure.last?.expressionKind == .closure {
-            return substructure.last?.bodyOffset
+        override func visit(_ node: FunctionCallExprSyntax) -> ExprSyntax {
+            guard node.hasNoSpaceInMethodCallViolation else {
+                return super.visit(node)
+            }
+
+            let isInDisabledRegion = disabledRegions.contains { region in
+                region.contains(node.positionAfterSkippingLeadingTrivia, locationConverter: locationConverter)
+            }
+
+            guard !isInDisabledRegion else {
+                return super.visit(node)
+            }
+
+            correctionPositions.append(node.calledExpression.endPositionBeforeTrailingTrivia)
+
+            let newNode = node
+                .withCalledExpression(node.calledExpression.withoutTrailingTrivia())
+
+            return super.visit(newNode)
+        }
+    }
+}
+
+private extension FunctionCallExprSyntax {
+    var hasNoSpaceInMethodCallViolation: Bool {
+        guard leftParen != nil,
+            !calledExpression.is(TupleExprSyntax.self),
+            let trailingTrivia = calledExpression.trailingTrivia,
+            trailingTrivia.isNotEmpty else {
+            return false
         }
 
-        return nil
+        return true
     }
 }

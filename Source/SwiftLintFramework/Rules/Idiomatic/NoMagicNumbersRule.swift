3379e2c5f6715b3b5beef795807142073c361484
Henrik Storch <thisisthefoxe@gmail.com>
2022-10-03 22:02:42 +1300

    Rewrite no-magic-numbers logic
    - update description & tests
    
    Don't use regex.
    Instead:
    1. get typed number
    2. check if it's excluded (0,1,-1)
    3. check if it's a declaration (i.e. if it follows a: `<keyword> <identifier> <number>` pattern)
    if not, trigger violation.
    
    Using flatMap/compactMap instead of nested `for-in` for speed and to keep maintainable code small. Same with `guard-else`..

--- Source/SwiftLintFramework/Rules/Idiomatic/NoMagicNumbersRule.swift
+++ Source/SwiftLintFramework/Rules/Idiomatic/NoMagicNumbersRule.swift
@@ -10,13 +10,13 @@ public struct NoMagicNumbersRule: ASTRule, OptInRule, ConfigurationProviderRule 
     public static let description = RuleDescription(
         identifier: "no_magic_numbers",
         name: "No Magic Numbers",
-        description: """
-        ‘Magic numbers’ are numbers that occur multiple times in code without an explicit meaning.
-        They should preferably be replaced by named constants.
-        """,
+        description: "‘Magic numbers’ should be replaced by named constants.",
         kind: .idiomatic,
         nonTriggeringExamples: [
-            Example("var x = 123\nfoo(x)"),
+            Example("""
+var x = 123
+foo(x)
+"""),
             Example("array[0] + array[1]"),
             Example("static let foo = 0.123)")
         ],
@@ -29,13 +29,21 @@ public struct NoMagicNumbersRule: ASTRule, OptInRule, ConfigurationProviderRule 
 
     public func validate(file: SwiftLintFile, kind: SwiftExpressionKind,
                          dictionary: SourceKittenDictionary) -> [StyleViolation] {
-        // ignore 0, 0.0, 1, and 1.0, because they're used so often
-        let pattern = "([01].(0[0-9]|[1-9])[0-9_]*)|([01][0-9]|[2-9])[0-9]*\\.?[0-9_]*"
-        let excludingKinds = SyntaxKind.commentAndStringKinds
-        return file.match(pattern: pattern, excludingSyntaxKinds: excludingKinds).map {
-            StyleViolation(ruleDescription: Self.description,
-                           severity: configuration.severity,
-                           location: Location(file: file, characterOffset: $0.location))
+        file.syntaxTokensByLines.flatMap { line -> [StyleViolation] in
+            line.enumerated().compactMap { tokenIx, token -> StyleViolation? in
+                guard token.kind == .number,
+                      let nrString = file.contents(for: token)?.replacingOccurrences(of: "_", with: ""),
+                      let number = Double(nrString),
+                      ![0, 1, -1].contains(number),
+                      // if it follows a `<keyword> <identifier> <number>` pattern it's (probably) a declaration (i.e. `var x = 2`)
+                      tokenIx < 2 || line[tokenIx - 1].kind != .identifier || line[tokenIx - 2].kind != .keyword else {
+                    return nil
+                }
+                return StyleViolation(
+                    ruleDescription: Self.description,
+                    severity: configuration.severity,
+                    location: Location(file: file, characterOffset: token.offset.value))
+            }
         }
     }
 }
--- Tests/SwiftLintFrameworkTests/NoMagicNumbersRuleTests.swift
+++ Tests/SwiftLintFrameworkTests/NoMagicNumbersRuleTests.swift
@@ -2,15 +2,21 @@ import SwiftLintFramework
 import XCTest
 
 class NoMagicNumbersRuleTests: XCTestCase {
-    func testNumberSeparatorWithMinimumLength() {
+    func testNoMagicNumbers() {
         let nonTriggeringExamples = [
-            Example("let foo = 123"),
-            Example("static let foo = 0.123"),
+            Example("var foo = 123"),
+            Example("static let bar = 0.123"),
             Example("array[1]"),
             Example("array[0]"),
             Example("let foo = 1_000.000_01"),
             Example("// array[1337]"),
-            Example("let s = \"9999\""),
+            Example("baz(\"9999\")"),
+            Example("""
+func foo() {
+    let x = 2
+    let y = [0, 0, 0][x]
+}
+"""),
             Example(
 """
 class A {
@@ -21,6 +27,7 @@ class A {
         ]
         let triggeringExamples = [
             Example("foo(123)"),
+            Example("bar(1_000.005_01)"),
             Example("Color.primary.opacity(isAnimate ? 0.1 : 1.0)"),
             Example("array[42]"),
             Example("let box = array[1337]")

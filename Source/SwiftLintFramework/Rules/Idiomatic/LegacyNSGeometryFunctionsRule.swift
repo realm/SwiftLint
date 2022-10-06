5ba9d60f8d1d7cf873be3b3cf15a5c9a4ab9cc96
JP Simard <jp@jpsim.com>
2022-10-06 12:46:27 -0400

    Rewrite `legacy_nsgeometry_functions` with SwiftSyntax (#4307)

--- Source/SwiftLintFramework/Rules/Idiomatic/LegacyNSGeometryFunctionsRule.swift
+++ Source/SwiftLintFramework/Rules/Idiomatic/LegacyNSGeometryFunctionsRule.swift
@@ -1,7 +1,7 @@
-import Foundation
-import SourceKittenFramework
+import SwiftSyntax
+import SwiftSyntaxBuilder
 
-public struct LegacyNSGeometryFunctionsRule: CorrectableRule, ConfigurationProviderRule {
+public struct LegacyNSGeometryFunctionsRule: SwiftSyntaxCorrectableRule, ConfigurationProviderRule {
     public var configuration = SeverityConfiguration(.warning)
 
     public init() {}
@@ -76,53 +76,150 @@ public struct LegacyNSGeometryFunctionsRule: CorrectableRule, ConfigurationProvi
             Example("↓NSPointInRect(point  ,rect)\n"): Example("rect.contains(point)\n"), // note order of arguments
             Example("↓NSIntersectsRect(  rect1,rect2 )\n"): Example("rect1.intersects(rect2)\n"),
             Example("↓NSIntersectsRect(rect1, rect2 )\n↓NSWidth(rect  )\n"):
-            Example("rect1.intersects(rect2)\nrect.width\n")
+                Example("rect1.intersects(rect2)\nrect.width\n"),
+            Example("↓NSIntersectionRect(rect1, rect2)"): Example("rect1.intersection(rect2)")
         ]
     )
 
-    public func validate(file: SwiftLintFile) -> [StyleViolation] {
-        let functions = ["NSWidth", "NSHeight", "NSMinX", "NSMidX",
-                         "NSMaxX", "NSMinY", "NSMidY", "NSMaxY",
-                         "NSEqualRects", "NSEqualSizes", "NSEqualPoints", "NSEdgeInsetsEqual",
-                         "NSIsEmptyRect", "NSIntegralRect", "NSInsetRect",
-                         "NSOffsetRect", "NSUnionRect", "NSIntersectionRect",
-                         "NSContainsRect", "NSPointInRect", "NSIntersectsRect"]
+    public func makeVisitor(file: SwiftLintFile) -> ViolationsSyntaxVisitor? {
+        Visitor(viewMode: .sourceAccurate)
+    }
 
-        let pattern = "\\b(" + functions.joined(separator: "|") + ")\\b"
+    public func makeRewriter(file: SwiftLintFile) -> ViolationsSyntaxRewriter? {
+        file.locationConverter.map { locationConverter in
+            Rewriter(
+                locationConverter: locationConverter,
+                disabledRegions: disabledRegions(file: file)
+            )
+        }
+    }
+}
 
-        return file.match(pattern: pattern, with: [.identifier]).map {
-            StyleViolation(ruleDescription: Self.description,
-                           severity: configuration.severity,
-                           location: Location(file: file, characterOffset: $0.location))
+private extension LegacyNSGeometryFunctionsRule {
+    final class Visitor: SyntaxVisitor, ViolationsSyntaxVisitor {
+        private(set) var violationPositions: [AbsolutePosition] = []
+
+        override func visitPost(_ node: FunctionCallExprSyntax) {
+            if node.isLegacyNSGeometryExpression {
+                violationPositions.append(node.positionAfterSkippingLeadingTrivia)
+            }
         }
     }
 
-    public func correct(file: SwiftLintFile) -> [Correction] {
-        let varName = RegexHelpers.varNameGroup
-        let twoVars = RegexHelpers.twoVars
-        let twoVariableOrNumber = RegexHelpers.twoVariableOrNumber
-        let patterns: [String: String] = [
-            "NSWidth\\(\(varName)\\)": "$1.width",
-            "NSHeight\\(\(varName)\\)": "$1.height",
-            "NSMinX\\(\(varName)\\)": "$1.minX",
-            "NSMidX\\(\(varName)\\)": "$1.midX",
-            "NSMaxX\\(\(varName)\\)": "$1.maxX",
-            "NSMinY\\(\(varName)\\)": "$1.minY",
-            "NSMidY\\(\(varName)\\)": "$1.midY",
-            "NSMaxY\\(\(varName)\\)": "$1.maxY",
-            "NSEqualRects\\(\(twoVars)\\)": "$1 == $2",
-            "NSEqualSizes\\(\(twoVars)\\)": "$1 == $2",
-            "NSEqualPoints\\(\(twoVars)\\)": "$1 == $2",
-            "NSEdgeInsetsEqual\\(\(twoVars)\\)": "$1 == $2",
-            "NSIsEmptyRect\\(\(varName)\\)": "$1.isEmpty",
-            "NSIntegralRect\\(\(varName)\\)": "$1.integral",
-            "NSInsetRect\\(\(varName),\(twoVariableOrNumber)\\)": "$1.insetBy(dx: $2, dy: $3)",
-            "NSOffsetRect\\(\(varName),\(twoVariableOrNumber)\\)": "$1.offsetBy(dx: $2, dy: $3)",
-            "NSUnionRect\\(\(twoVars)\\)": "$1.union($2)",
-            "NSContainsRect\\(\(twoVars)\\)": "$1.contains($2)",
-            "NSPointInRect\\(\(twoVars)\\)": "$2.contains($1)", // note order of arguments
-            "NSIntersectsRect\\(\(twoVars)\\)": "$1.intersects($2)"
-        ]
-        return file.correct(legacyRule: self, patterns: patterns)
+    enum RewriteStrategy {
+        case equal
+        case property(name: String)
+        case function(name: String, argumentLabels: [String], reversed: Bool = false)
+
+        var expectedInitialArguments: Int {
+            switch self {
+            case .equal:
+                return 2
+            case .property:
+                return 1
+            case .function(name: _, argumentLabels: let argumentLabels, reversed: _):
+                return argumentLabels.count + 1
+            }
+        }
+    }
+
+    static let legacyFunctions: [String: RewriteStrategy] = [
+        "NSHeight": .property(name: "height"),
+        "NSIntegralRect": .property(name: "integral"),
+        "NSIsEmptyRect": .property(name: "isEmpty"),
+        "NSMaxX": .property(name: "maxX"),
+        "NSMaxY": .property(name: "maxY"),
+        "NSMidX": .property(name: "midX"),
+        "NSMidY": .property(name: "midY"),
+        "NSMinX": .property(name: "minX"),
+        "NSMinY": .property(name: "minY"),
+        "NSWidth": .property(name: "width"),
+        "NSEqualPoints": .equal,
+        "NSEqualSizes": .equal,
+        "NSEqualRects": .equal,
+        "NSEdgeInsetsEqual": .equal,
+        "NSInsetRect": .function(name: "insetBy", argumentLabels: ["dx", "dy"]),
+        "NSOffsetRect": .function(name: "offsetBy", argumentLabels: ["dx", "dy"]),
+        "NSUnionRect": .function(name: "union", argumentLabels: [""]),
+        "NSContainsRect": .function(name: "contains", argumentLabels: [""]),
+        "NSIntersectsRect": .function(name: "intersects", argumentLabels: [""]),
+        "NSIntersectionRect": .function(name: "intersection", argumentLabels: [""]),
+        "NSPointInRect": .function(name: "contains", argumentLabels: [""], reversed: true)
+    ]
+
+    final class Rewriter: SyntaxRewriter, ViolationsSyntaxRewriter {
+        private(set) var correctionPositions: [AbsolutePosition] = []
+        let locationConverter: SourceLocationConverter
+        let disabledRegions: [SourceRange]
+
+        init(locationConverter: SourceLocationConverter, disabledRegions: [SourceRange]) {
+            self.locationConverter = locationConverter
+            self.disabledRegions = disabledRegions
+        }
+
+        override func visit(_ node: FunctionCallExprSyntax) -> ExprSyntax {
+            guard
+                node.isLegacyNSGeometryExpression,
+                let funcName = node.calledExpression.as(IdentifierExprSyntax.self)?.identifier.text,
+                !isInDisabledRegion(node)
+            else {
+                return super.visit(node)
+            }
+
+            correctionPositions.append(node.positionAfterSkippingLeadingTrivia)
+
+            let trimmedArguments = node.argumentList.map { $0.trimmed() }
+            let rewriteStrategy = LegacyNSGeometryFunctionsRule.legacyFunctions[funcName]
+
+            let expr: ExprSyntax
+            switch rewriteStrategy {
+            case .equal:
+                expr = "\(trimmedArguments[0]) == \(trimmedArguments[1])"
+            case let .property(name: propertyName):
+                expr = "\(trimmedArguments[0]).\(propertyName)"
+            case let .function(name: functionName, argumentLabels: argumentLabels, reversed: reversed):
+                let arguments = reversed ? trimmedArguments.reversed() : trimmedArguments
+                let params = zip(argumentLabels, arguments.dropFirst())
+                    .map { $0.isEmpty ? "\($1)" : "\($0): \($1)" }
+                    .joined(separator: ", ")
+                expr = "\(arguments[0]).\(functionName)(\(params))"
+            case .none:
+                return super.visit(node)
+            }
+
+            return expr
+                .withLeadingTrivia(node.leadingTrivia ?? .zero)
+                .withTrailingTrivia(node.trailingTrivia ?? .zero)
+        }
+
+        private func isInDisabledRegion<T: SyntaxProtocol>(_ node: T) -> Bool {
+            disabledRegions.contains { region in
+                region.contains(node.positionAfterSkippingLeadingTrivia, locationConverter: locationConverter)
+            }
+        }
+    }
+}
+
+private extension FunctionCallExprSyntax {
+    var isLegacyNSGeometryExpression: Bool {
+        guard
+            let calledExpression = calledExpression.as(IdentifierExprSyntax.self),
+            case let funcName = calledExpression.identifier.text,
+            let rewriteStrategy = LegacyNSGeometryFunctionsRule.legacyFunctions[funcName],
+            argumentList.count == rewriteStrategy.expectedInitialArguments
+        else {
+            return false
+        }
+
+        return true
+    }
+}
+
+private extension TupleExprElementSyntax {
+    func trimmed() -> TupleExprElementSyntax {
+        self
+            .withoutTrivia()
+            .withTrailingComma(nil)
+            .withoutTrivia()
     }
 }

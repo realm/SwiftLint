492ba9b65f7420a5dfbe7e74709c6f24f596eb88
Marcelo Fabri <me@marcelofabri.com>
2022-10-09 11:01:46 -0700

    Add test for negative exponential

--- Source/SwiftLintFramework/Rules/Style/NumberSeparatorRuleExamples.swift
+++ Source/SwiftLintFramework/Rules/Style/NumberSeparatorRuleExamples.swift
@@ -15,7 +15,8 @@ internal struct NumberSeparatorRuleExamples {
                 Example("let octal = \(sign)0o21_1"),
                 Example("let exp = \(sign)1_000_000.000_000e2"),
                 Example("let foo: Double = \(sign)(200)"),
-                Example("let foo: Double = \(sign)(200 / 447.214)")
+                Example("let foo: Double = \(sign)(200 / 447.214)"),
+                Example("let foo = \(sign)6.283_2e-6"),
             ]
         }
     }()
@@ -35,7 +36,8 @@ internal struct NumberSeparatorRuleExamples {
                 Example("let foo = \(sign)1__000"),
                 Example("let foo = \(sign)1.0001"),
                 Example("let foo = \(sign)1_000_000.000000_1"),
-                Example("let foo = \(sign)1000000.000000_1")
+                Example("let foo = \(sign)1000000.000000_1"),
+                Example("let foo = \(sign)6.2832e-6"),
             ]
         }
     }
@@ -64,6 +66,7 @@ internal struct NumberSeparatorRuleExamples {
             // swiftlint:disable:next line_length
             result[Example("let foo = \(violation)1_000_000.000000_1")] = Example("let foo = \(sign)1_000_000.000_000_1")
             result[Example("let foo = \(violation)1000000.000000_1")] = Example("let foo = \(sign)1_000_000.000_000_1")
+            result[Example("let foo = \(sign)6.2832e-6")] = Example("let foo = \(sign)6.283_2e-6")
         }
 
         return result

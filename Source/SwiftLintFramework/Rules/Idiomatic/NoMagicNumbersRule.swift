18a35e2a2e71bed2f3432880a9ece7e925f97b31
Henrik Storch <thisisthefoxe@gmail.com>
2022-10-10 19:21:13 +1300

    Fix lint issues for NMN rule

--- Source/SwiftLintFramework/Rules/Idiomatic/NoMagicNumbersRule.swift
+++ Source/SwiftLintFramework/Rules/Idiomatic/NoMagicNumbersRule.swift
@@ -32,16 +32,24 @@ foo(x)
         file.syntaxTokensByLines.flatMap { line -> [StyleViolation] in
             guard let firstToken = line.first, firstToken.kind != .attributeBuiltin,    // @available
                   line.count > 1,
-                  file.contents(for: firstToken) != "if" || line[1].kind != .keyword || file.contents(for: line[1]) != "#available"
-            else { return [] }
+                  file.contents(for: firstToken) != "if" ||
+                    line[1].kind != .keyword ||
+                    file.contents(for: line[1]) != "#available" else {
+                return []
+            }
             return line.enumerated().compactMap { tokenIx, token -> StyleViolation? in
                 guard token.kind == .number,
                       let nrString = file.contents(for: token)?.replacingOccurrences(of: "_", with: ""),
                       let number = Double(nrString),
                       ![0, 1, -1].contains(number),
                       // allow declarations for local vars
-                      tokenIx < 2 || line[tokenIx - 1].kind != .identifier || line[tokenIx - 2].kind != .keyword,
-                      tokenIx < 3 || line[tokenIx - 1].kind != .typeidentifier || line[tokenIx - 2].kind != .identifier || line[tokenIx - 3].kind != .keyword  else {
+                      tokenIx < 2 ||
+                        line[tokenIx - 1].kind != .identifier ||
+                        line[tokenIx - 2].kind != .keyword,
+                      tokenIx < 3 ||
+                        line[tokenIx - 1].kind != .typeidentifier ||
+                        line[tokenIx - 2].kind != .identifier ||
+                        line[tokenIx - 3].kind != .keyword  else {
                     return nil
                 }
                 return StyleViolation(
--- Tests/SwiftLintFrameworkTests/NoMagicNumbersRuleTests.swift
+++ Tests/SwiftLintFrameworkTests/NoMagicNumbersRuleTests.swift
@@ -2,6 +2,30 @@ import SwiftLintFramework
 import XCTest
 
 class NoMagicNumbersRuleTests: XCTestCase {
+    private let functionExample = Example("""
+func foo() {
+    let x: Int = 2
+    let y = 3
+    let vector = [x, y, 0]
+}
+""")
+
+    private let classExample = Example("""
+class A {
+    var foo: Double = 132
+    static let bar: Double = 0.98
+}
+""")
+
+    private let availableExample = Example("""
+@available(iOS 13, *)
+func version() {
+    if #available(iOS 13, OSX 10.10, *) {
+        return
+    }
+}
+""")
+
     func testNoMagicNumbers() {
         let nonTriggeringExamples = [
             Example("0.123"),
@@ -13,27 +37,9 @@ class NoMagicNumbersRuleTests: XCTestCase {
             Example("let foo = 1_000.000_01"),
             Example("// array[1337]"),
             Example("baz(\"9999\")"),
-            Example("""
-func foo() {
-    let x: Int = 2
-    let y = 3
-    let vector = [x, y, 0]
-}
-"""),
-            Example("""
-class A {
-    var foo: Double = 132
-    static let bar: Double = 0.98
-}
-"""),
-            Example("""
-@available(iOS 13, *)
-func version() {
-    if #available(iOS 13, OSX 10.10, *) {
-        return
-    }
-}
-""")
+            functionExample,
+            classExample,
+            availableExample
         ]
         let triggeringExamples = [
             Example("foo(123)"),

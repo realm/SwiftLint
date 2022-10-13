55d63f7da808b84f770762fc59daf39b238a9956
JP Simard <jp@jpsim.com>
2022-10-13 09:55:24 -0400

    Update SwiftSyntax
    Noteworthy:
    
    * https://github.com/apple/swift-syntax/pull/912
    * https://github.com/apple/swift-syntax/pull/932

--- Package.swift
+++ Package.swift
@@ -27,7 +27,7 @@ let package = Package(
     ],
     dependencies: [
         .package(name: "swift-argument-parser", url: "https://github.com/apple/swift-argument-parser.git", .upToNextMinor(from: "1.1.3")),
-        .package(name: "SwiftSyntax", url: "https://github.com/apple/swift-syntax.git", .revision("26bf2172cc5b7d54f28b10d18e8b5de351b5c4a3")),
+        .package(name: "SwiftSyntax", url: "https://github.com/apple/swift-syntax.git", .revision("5296d9328c863e07ebb920008d4560dd7a2bc035")),
         .package(url: "https://github.com/jpsim/SourceKitten.git", .revision("a9e6df65d8e31e0fa6e8a05ffe40ecd54a645871")),
         .package(url: "https://github.com/jpsim/Yams.git", from: "5.0.1"),
         .package(url: "https://github.com/scottrhoyt/SwiftyTextTable.git", from: "0.9.0"),

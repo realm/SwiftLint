9a7c10b6002c2b2fd1e121b8c23a9147bf89d9f5
JP Simard <jp@jpsim.com>
2022-10-20 17:16:03 -0400

    Rewrite `lower_acl_than_parent` with SwiftSyntax
    And fix violations in SwiftLint.

--- Source/SwiftLintFramework/Rules/Lint/LowerACLThanParentRule.swift
+++ Source/SwiftLintFramework/Rules/Lint/LowerACLThanParentRule.swift
@@ -1,6 +1,6 @@
-import SourceKittenFramework
+import SwiftSyntax
 
-public struct LowerACLThanParentRule: OptInRule, ConfigurationProviderRule {
+public struct LowerACLThanParentRule: OptInRule, ConfigurationProviderRule, SwiftSyntaxRule {
     public var configuration = SeverityConfiguration(.warning)
 
     public init() {}
@@ -8,7 +8,7 @@ public struct LowerACLThanParentRule: OptInRule, ConfigurationProviderRule {
     public static let description = RuleDescription(
         identifier: "lower_acl_than_parent",
         name: "Lower ACL than parent",
-        description: "Ensure definitions have a lower access control level than their enclosing parent",
+        description: "Ensure declarations have a lower access control level than their enclosing parent",
         kind: .lint,
         nonTriggeringExamples: [
             Example("public struct Foo { public func bar() {} }"),
@@ -24,69 +24,104 @@ public struct LowerACLThanParentRule: OptInRule, ConfigurationProviderRule {
             Example("private class Foo { func bar() {} }")
         ],
         triggeringExamples: [
-            Example("struct Foo { public ↓func bar() {} }"),
-            Example("enum Foo { public ↓func bar() {} }"),
-            Example("public class Foo { open ↓func bar() }"),
-            Example("class Foo { public private(set) ↓var bar: String? }"),
-            Example("private class Foo { internal ↓func bar() {} }")
+            Example("struct Foo { ↓public func bar() {} }"),
+            Example("enum Foo { ↓public func bar() {} }"),
+            Example("public class Foo { ↓open func bar() }"),
+            Example("class Foo { ↓public private(set) var bar: String? }"),
+            Example("private struct Foo { ↓public func bar() {} }"),
+            Example("private class Foo { ↓public func bar() {} }"),
+            Example("private actor Foo { ↓public func bar() {} }"),
+            Example("struct Foo { ↓public func bar() {} }"),
+            Example("class Foo { ↓public func bar() {} }"),
+            Example("actor Foo { ↓public func bar() {} }")
         ]
     )
 
-    public func validate(file: SwiftLintFile) -> [StyleViolation] {
-        return validateACL(isHigherThan: .open, in: file.structureDictionary).map {
-            StyleViolation(ruleDescription: Self.description,
-                           severity: configuration.severity,
-                           location: Location(file: file, byteOffset: $0))
-        }
+    public func makeVisitor(file: SwiftLintFile) -> ViolationsSyntaxVisitor {
+        Visitor(viewMode: .sourceAccurate)
     }
+}
 
-    private func validateACL(isHigherThan parentAccessibility: AccessControlLevel,
-                             in substructure: SourceKittenDictionary) -> [ByteCount] {
-        return substructure.substructure.flatMap { element -> [ByteCount] in
-            guard let elementKind = element.declarationKind,
-                elementKind.isRelevantDeclaration else {
-                return []
+private extension LowerACLThanParentRule {
+    private final class Visitor: ViolationsSyntaxVisitor {
+        override func visitPost(_ node: DeclModifierSyntax) {
+            guard let nearestNominalParent = node.parent?.nearestNominalParent() else {
+                return
             }
 
-            var violationOffset: ByteCount?
-            let accessibility = element.accessibility ?? .internal
-            // Swift 5 infers members of private types with no explicit ACL attribute to be `internal`.
-            let isInferredACL = accessibility == .internal && !element.enclosedSwiftAttributes.contains(.internal)
-            if !isInferredACL, accessibility.priority > parentAccessibility.priority {
-                violationOffset = element.offset
+            switch node.name.tokenKind {
+            case .internalKeyword
+                where nearestNominalParent.modifiers.isPrivate ||
+                    nearestNominalParent.modifiers.isFileprivate:
+                break
+            case .publicKeyword
+                where nearestNominalParent.modifiers.isPrivate || nearestNominalParent.modifiers.isInternal:
+                break
+            case .contextualKeyword("open") where !nearestNominalParent.modifiers.isOpen:
+                break
+            default:
+                return
             }
 
-            return [violationOffset].compactMap { $0 } + self.validateACL(isHigherThan: accessibility, in: element)
+            violations.append(node.positionAfterSkippingLeadingTrivia)
         }
     }
 }
 
-private extension SwiftDeclarationKind {
-    var isRelevantDeclaration: Bool {
-        switch self {
-        case .associatedtype, .enumcase, .enumelement, .extension, .extensionClass, .extensionEnum,
-             .extensionProtocol, .extensionStruct, .functionAccessorAddress, .functionAccessorDidset,
-             .functionAccessorRead, .functionAccessorModify, .functionAccessorGetter,
-             .functionAccessorMutableaddress, .functionAccessorSetter, .functionAccessorWillset,
-             .functionDestructor, .genericTypeParam, .module, .precedenceGroup, .varLocal, .varParameter, .opaqueType:
-            return false
-        case .class, .enum, .functionConstructor, .functionFree, .functionMethodClass, .functionMethodInstance,
-             .functionMethodStatic, .functionOperator, .functionOperatorInfix, .functionOperatorPostfix,
-             .functionOperatorPrefix, .functionSubscript, .protocol, .struct, .typealias, .varClass, .varGlobal,
-             .varInstance, .varStatic:
-            return true
+private extension SyntaxProtocol {
+    func nearestNominalParent() -> Syntax? {
+        guard let parent = parent else {
+            return nil
+        }
+
+        return parent.isNominalTypeDecl ? parent : parent.nearestNominalParent()
+    }
+}
+
+private extension Syntax {
+    var isNominalTypeDecl: Bool {
+        self.is(StructDeclSyntax.self) ||
+            self.is(ClassDeclSyntax.self) ||
+            self.is(ActorDeclSyntax.self) ||
+            self.is(EnumDeclSyntax.self)
+    }
+
+    var modifiers: ModifierListSyntax? {
+        if let node = self.as(StructDeclSyntax.self) {
+            return node.modifiers
+        } else if let node = self.as(ClassDeclSyntax.self) {
+            return node.modifiers
+        } else if let node = self.as(ActorDeclSyntax.self) {
+            return node.modifiers
+        } else if let node = self.as(EnumDeclSyntax.self) {
+            return node.modifiers
+        } else {
+            return nil
         }
     }
 }
 
-private extension AccessControlLevel {
-    var priority: Int {
-        switch self {
-        case .private: return 1
-        case .fileprivate: return 1
-        case .internal: return 2
-        case .public: return 3
-        case .open: return 4
-        }
+private extension ModifierListSyntax? {
+    var isFileprivate: Bool {
+        self?.contains(where: { $0.name.tokenKind == .fileprivateKeyword }) == true
+    }
+
+    var isPrivate: Bool {
+        self?.contains(where: { $0.name.tokenKind == .privateKeyword }) == true
+    }
+
+    var isInternal: Bool {
+        self?.allSatisfy { modifier in
+            switch modifier.name.tokenKind {
+            case .fileprivateKeyword, .privateKeyword, .publicKeyword, .contextualKeyword("open"):
+                return false
+            default:
+                return true
+            }
+        } != false
+    }
+
+    var isOpen: Bool {
+        self?.contains(where: { $0.name.tokenKind == .contextualKeyword("open") }) == true
     }
 }

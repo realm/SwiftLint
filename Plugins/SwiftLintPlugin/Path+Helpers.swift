aa245964fb6d5b329e22c85a80505d47dec2108e
Tony Arnold <tony@thecocoabots.com>
2022-11-30 08:58:54 +1100

    Use a lower-level method of checking if a file is accessible
    This shouldn’t trigger sandbox violations, I hope…

--- Plugins/SwiftLintPlugin/Path+Helpers.swift
+++ Plugins/SwiftLintPlugin/Path+Helpers.swift
@@ -1,6 +1,12 @@
 import Foundation
 import PackagePlugin
 
+#if os(Linux)
+import GlibC
+#else
+import Darwin
+#endif
+
 extension Path {
     /// Scans the receiver, then all of its parents looking for a configuration file with the name ".swiftlint.yml".
     ///
@@ -9,8 +15,17 @@ extension Path {
         let defaultConfigurationFileName = ".swiftlint.yml"
         let proposedDirectory = sequence(first: self, next: { $0.removingLastComponent() }).first { path in
             let potentialConfigurationFile = path.appending(subpath: defaultConfigurationFileName)
-            return FileManager.default.isReadableFile(atPath: potentialConfigurationFile.string)
+            return potentialConfigurationFile.isAccessible()
         }
         return proposedDirectory?.appending(subpath: defaultConfigurationFileName)
+    }
+
+    /// Safe way to check if the file is accessible from within the current process sandbox.
+    private func isAccessible() -> Bool {
+        let result = string.withCString { pointer in
+            access(pointer, R_OK)
+        }
+
+        return result == 0
     }
 }

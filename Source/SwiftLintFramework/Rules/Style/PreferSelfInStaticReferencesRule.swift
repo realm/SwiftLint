71effba7f9a1d99c10c4163d9607b42159981420
JP Simard <jp@jpsim.com>
2022-12-14 10:08:12 -0500

    Fix `#selector()`
    And add a regression test.

--- Source/SwiftLintFramework/Rules/Style/PreferSelfInStaticReferencesRule.swift
+++ Source/SwiftLintFramework/Rules/Style/PreferSelfInStaticReferencesRule.swift
@@ -84,6 +84,17 @@ struct PreferSelfInStaticReferencesRule: SwiftSyntaxRule, CorrectableRule, Confi
         ],
         triggeringExamples: [
             Example("""
+            final class CheckCellView: NSTableCellView {
+              @IBOutlet var checkButton: NSButton!
+
+              override func awakeFromNib() {
+                checkButton.action = #selector(↓CheckCellView.check(_:))
+              }
+
+              @objc func check(_ button: AnyObject?) {}
+            }
+            """),
+            Example("""
                 class C {
                     struct S {
                         static let i = 2
@@ -146,6 +157,28 @@ struct PreferSelfInStaticReferencesRule: SwiftSyntaxRule, CorrectableRule, Confi
             """, excludeFromDocumentation: true)
         ],
         corrections: [
+            Example("""
+            final class CheckCellView: NSTableCellView {
+              @IBOutlet var checkButton: NSButton!
+
+              override func awakeFromNib() {
+                checkButton.action = #selector(↓CheckCellView.check(_:))
+              }
+
+              @objc func check(_ button: AnyObject?) {}
+            }
+            """):
+                Example("""
+                final class CheckCellView: NSTableCellView {
+                  @IBOutlet var checkButton: NSButton!
+
+                  override func awakeFromNib() {
+                    checkButton.action = #selector(Self.check(_:))
+                  }
+
+                  @objc func check(_ button: AnyObject?) {}
+                }
+                """),
             Example("""
                 struct S {
                     static let i = 1
@@ -297,7 +330,7 @@ private class Visitor: ViolationsSyntaxVisitor {
     }
 
     override func visit(_ node: MacroExpansionExprSyntax) -> SyntaxVisitorContinueKind {
-        if case .likeStruct = parentDeclScopes.last {
+        if case .likeClass = parentDeclScopes.last, case .identifier("selector") = node.macro.tokenKind {
             return .visitChildren
         }
         return .skipChildren

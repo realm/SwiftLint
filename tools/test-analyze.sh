247b4520f0eb4b63a1aa7ed17eb1c36f0c78b366
Keith Smiley <keithbsmiley@gmail.com>
2023-01-19 09:53:07 -0800

    fix analyze

--- tools/test-analyze.sh
+++ tools/test-analyze.sh
@@ -2,8 +2,12 @@
 
 set -euo pipefail
 
-readonly swiftlint="$RUNFILES_DIR/SwiftLint/swiftlint"
-readonly swiftpm_yaml="$RUNFILES_DIR/SwiftLint/swiftpm.yaml"
+readonly swiftlint="$RUNFILES_DIR/_main/swiftlint"
+if [[ ! -x "$swiftlint" ]]; then
+  echo "SwiftLint not found at $swiftlint"
+  exit 1
+fi
+
 # Change to workspace directory
 cd "$(dirname "$(readlink Package.swift)")"
 swift build --build-tests

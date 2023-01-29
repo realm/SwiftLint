82b79cf17bef4071ce05485c7b21d15c208a570d
Martin Redington <mildm8nnered@gmail.com>
2023-01-29 17:34:37 +0000

    Started to add the rule

---
+++ Source/SwiftLintFramework/Rules/Lint/NoBlanketDisablesRule.swift
@@ -0,0 +1,40 @@
+import SwiftSyntax
+
+struct NoBlanketDisablesRule: SwiftSyntaxRule, ConfigurationProviderRule {
+    var configuration = SeverityConfiguration(.warning)
+
+    init() {}
+
+    static let description = RuleDescription(
+        identifier: "no blanket disables",
+        name: "No Blanket Disables",
+        description: "swiftlint:disable commands should be re-enabled before the end of the file",
+        kind: .lint,
+        nonTriggeringExamples: [
+            Example("""
+            // swiftlint:disable unused_import
+            // swiftlint:enable unused_import
+            """),
+            Example("""
+            // swiftlint:disable unused_import unused_declaration
+            // swiftlint:enable unused_import
+            // swiftlint:enable unused_declaration
+            """),
+            Example("// swiftlint:disable:this unused_import"),
+            Example("// swiftlint:disable:next unused_import"),
+            Example("// swiftlint:disable:previous unused_import")
+        ],
+        triggeringExamples: [
+            Example("// swiftlint:disable unused_import"),
+            Example("""
+            // swiftlint:disable unused_import unused_declaration
+            // swiftlint:enable unused_import
+            """)
+        ]
+     )
+
+    @available(*, unavailable)
+    func makeVisitor(file: SwiftLintFile) -> ViolationsSyntaxVisitor {
+        queuedFatalError("no implemented yet")
+    }
+}

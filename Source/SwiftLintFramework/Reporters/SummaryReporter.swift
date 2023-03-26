184ce4baf501269a5cdf902f4b76f1fdc4532317
Martin Redington <mildm8nnered@gmail.com>
2023-03-26 22:34:19 +0100

    Fixed display of optin and correctable

--- Source/SwiftLintFramework/Reporters/SummaryReporter.swift
+++ Source/SwiftLintFramework/Reporters/SummaryReporter.swift
@@ -70,8 +70,8 @@ private extension TextTable {
 
             addRow(values: [
                 ruleIdentifier,
-                rule is OptInRule ? "yes" : "no",
-                rule is CorrectableRule ? "yes" : "no",
+                rule is OptInRule.Type ? "yes" : "no",
+                rule is CorrectableRule.Type ? "yes" : "no",
                 rule == nil ? "yes" : "no",
                 numberOfWarnings.formattedString.leftPadded(forHeader: numberOfWarningsHeader),
                 numberOfErrors.formattedString.leftPadded(forHeader: numberOfErrorsHeader),

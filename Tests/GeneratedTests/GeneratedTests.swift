3541ef4d02e3441ce608f527a0b605b7542c5e8a
JP Simard <jp@jpsim.com>
2023-04-26 12:07:29 -0400

    Import SwiftLintCore in generated tests
    In preparation for it containing the `superfluous_disable_command` rule.

--- Tests/GeneratedTests/GeneratedTests.swift
+++ Tests/GeneratedTests/GeneratedTests.swift
@@ -1,5 +1,6 @@
 // Generated using Sourcery 2.0.2 — https://github.com/krzysztofzablocki/Sourcery
 // DO NOT EDIT
+@testable import SwiftLintCore
 @_spi(TestHelper)
 @testable import SwiftLintFramework
 import SwiftLintTestHelpers

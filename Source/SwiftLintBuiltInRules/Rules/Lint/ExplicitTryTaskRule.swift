b89e96c2ceda593f067c056241d32dfffe66946e
JP Simard <jp@jpsim.com>
2023-05-03 19:48:34 -0400

    Naive implementation

--- Source/SwiftLintBuiltInRules/Rules/Lint/ExplicitTryTaskRule.swift
+++ Source/SwiftLintBuiltInRules/Rules/Lint/ExplicitTryTaskRule.swift
@@ -61,5 +61,22 @@ struct ExplicitTryTaskRule: ConfigurationProviderRule, SwiftSyntaxRule {
 
 private extension ExplicitTryTaskRule {
     final class Visitor: ViolationsSyntaxVisitor {
+        override func visitPost(_ node: FunctionCallExprSyntax) {
+            // TODO: Replace 'trimmedDescription' with more optimized check
+            if node.calledExpression.trimmedDescription == "Task" {
+                let tryVisitor = TryVisitor(viewMode: .sourceAccurate)
+                tryVisitor.walk(node)
+                if tryVisitor.hasTry {
+                    violations.append(node.calledExpression.positionAfterSkippingLeadingTrivia)
+                }
+            }
+        }
+    }
+}
+
+private final class TryVisitor: SyntaxVisitor {
+    var hasTry = false
+    override func visitPost(_ node: TryExprSyntax) {
+        hasTry = true
     }
 }

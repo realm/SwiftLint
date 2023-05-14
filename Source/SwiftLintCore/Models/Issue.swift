df828370e8d154eccded52c68da85ab68b1b87e5
Danny Mösch <danny.moesch@icloud.com>
2023-05-14 17:34:47 +0200

    Remove error wrapper

--- Source/SwiftLintCore/Models/Issue.swift
+++ Source/SwiftLintCore/Models/Issue.swift
@@ -8,6 +8,9 @@ public enum Issue: LocalizedError, Equatable {
     /// A generic warning specified by a string.
     case genericWarning(String)
 
+    /// A generic error specified by a string.
+    case genericError(String)
+
     /// A deprecation warning for a rule.
     case deprecation(ruleID: String)
 
@@ -17,9 +20,28 @@ public enum Issue: LocalizedError, Equatable {
     /// An error that occurred when parsing YAML.
     case yamlParsing(String)
 
+    /// Wraps any `Error` into a `SwiftLintError.genericWarning` if it is not already a `SwiftLintError`.
+    ///
+    /// - parameter error: Any `Error`.
+    ///
+    /// - returns: A `SwiftLintError.genericWarning` containig the message of the `error` argument.
+    static func wrap(error: Error) -> Self {
+        if let this = error as? Issue {
+            return this
+        }
+        return Self.genericWarning(error.localizedDescription)
+    }
+
+    /// Make this issue an error.
+    var asError: Self {
+        Self.genericError(message)
+    }
+
     /// The issues description which is ready to be printed to the console.
     var errorDescription: String {
         switch self {
+        case .genericError:
+            return "error: \(message)"
         case .genericWarning:
             return "warning: \(message)"
         default:
@@ -31,8 +53,8 @@ public enum Issue: LocalizedError, Equatable {
         switch self {
         case .unknownConfiguration:
             return "Invalid configuration. Falling back to default."
-        case .genericWarning(let message):
-            return "\(message)"
+        case .genericWarning(let message), .genericError(let message):
+            return message
         case .deprecation(let ruleID):
             return """
                 The `\(ruleID)` rule is now deprecated and will be \

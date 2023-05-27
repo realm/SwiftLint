7d2aceb761d282eb4368ee2ebae903aa3eca5841
Martin Redington <mildm8nnered@gmail.com>
2023-05-27 18:54:44 +0100

    More setup

--- Source/swiftlint/Commands/Configure.swift
+++ Source/swiftlint/Commands/Configure.swift
@@ -1,13 +1,27 @@
 import ArgumentParser
 import SwiftLintFramework
+#if os(Linux)
+    import Glibc
+#else
+    import Darwin.C
+#endif
 
 extension SwiftLint {
     struct Configure: ParsableCommand {
         static let configuration = CommandConfiguration(abstract: "Configure SwiftLint")
 
         func run() throws {
-            print("Hello")
+            doYouWantToContinue("Welcome to SwiftLint! Do you want to continue? (Y/n)")
             ExitHelper.successfullyExit()
         }
     }
+}
+
+private func print(_ message: String, terminator: String = "\n") {
+    Swift.print(message, terminator: terminator)
+    fflush(stdout)
+}
+
+private func doYouWantToContinue(_ message: String) {
+    print(message, terminator: " ")
 }

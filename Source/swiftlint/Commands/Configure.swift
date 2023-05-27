0faa2d70f653ddf447ec902acda9d56f7c04e1a0
Martin Redington <mildm8nnered@gmail.com>
2023-05-27 19:08:23 +0100

    Better prompting

--- Source/swiftlint/Commands/Configure.swift
+++ Source/swiftlint/Commands/Configure.swift
@@ -22,6 +22,17 @@ private func print(_ message: String, terminator: String = "\n") {
     fflush(stdout)
 }
 
-private func doYouWantToContinue(_ message: String) {
-    print(message, terminator: " ")
+private func doYouWantToContinue(_ message: String) -> Bool {
+    while true {
+        print(message, terminator: " ")
+        if let character = readLine() {
+            if character == "" || character.lowercased() == "y" {
+                return true
+            } else if character.lowercased() == "n" {
+                return false
+            } else {
+                print("Invalid Response")
+            }
+        }
+    }
 }

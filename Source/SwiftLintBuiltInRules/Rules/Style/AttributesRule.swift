03ab0e6fc76dac90042dd1e5a6cd5b310517c4d8
chrisngabp <christian@rayatheapp.com>
2023-07-07 13:20:18 -0300

    Removing period

--- Source/SwiftLintBuiltInRules/Rules/Style/AttributesRule.swift
+++ Source/SwiftLintBuiltInRules/Rules/Style/AttributesRule.swift
@@ -9,7 +9,7 @@ struct AttributesRule: SwiftSyntaxRule, OptInRule, ConfigurationProviderRule {
         description: """
             Attributes should be on their own lines in functions and types, but on the same line as variables and \
             imports except when they have properties and attributes_with_arguments_always_on_line_above is true \
-            which is the default.
+            which is the default
             """,
         kind: .style,
         nonTriggeringExamples: AttributesRuleExamples.nonTriggeringExamples,

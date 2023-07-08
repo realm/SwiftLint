9881930be51e3bd6a27d95b08cb3e111da38e10b
Danny Mösch <danny.moesch@icloud.com>
2023-07-08 13:52:17 +0200

    Implement conformances in extensions (#5109)

--- Source/SwiftLintCore/Extensions/NSRegularExpression+SwiftLint.swift
+++ Source/SwiftLintCore/Extensions/NSRegularExpression+SwiftLint.swift
@@ -14,6 +14,12 @@ private struct RegexCacheKey: Hashable {
     }
 }
 
+extension NSRegularExpression: Comparable {
+    public static func < (lhs: NSRegularExpression, rhs: NSRegularExpression) -> Bool {
+        lhs.pattern < rhs.pattern
+    }
+}
+
 public extension NSRegularExpression {
     static func cached(pattern: String, options: Options? = nil) throws -> NSRegularExpression {
         let options = options ?? [.anchorsMatchLines, .dotMatchesLineSeparators]

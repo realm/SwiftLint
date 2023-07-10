fd4d6ad1bea9a04334c87fce6ef3b827243cd88d
Danny Mösch <danny.moesch@icloud.com>
2023-07-10 23:06:55 +0200

    Use string blocks for examples

--- Source/SwiftLintBuiltInRules/Rules/Style/StatementPositionRuleExamples.swift
+++ Source/SwiftLintBuiltInRules/Rules/Style/StatementPositionRuleExamples.swift
@@ -1,30 +1,152 @@
 internal struct StatementPositionRuleExamples {
     static let nonTriggeringExamples = [
-        Example("if true {\n    foo()\n} else {\n    bar()\n}"),
-        Example("if true {\n    foo\n} else if true {\n    bar()\n} else {\n    return\n}"),
-        Example("do {\n    foo()\n} catch {\n    bar()\n}"),
-        Example("do {\n    foo()\n} catch let error {\n    bar()\n} catch {\n    return\n}"),
-        Example("struct A { let catchphrase: Int }\nlet a = A(\n catchphrase: 0\n)"),
-        Example("struct A { let `catch`: Int }\nlet a = A(\n `catch`: 0\n)")
+        Example("""
+            if true {
+                foo()
+            } else {
+                bar()
+            }
+        """),
+        Example("""
+            if true {
+                foo
+            } else if true {
+                bar()
+            } else {
+                return
+            }
+        """),
+        Example("""
+            do {
+                foo()
+            } catch {
+                bar()
+            }
+        """),
+        Example("""
+            do {
+                foo()
+            } catch let error {
+                bar()
+            } catch {
+                return
+            }
+        """),
+        Example("""
+            struct A { let catchphrase: Int }
+            let a = A(
+                catchphrase: 0
+            )
+        """),
+        Example("""
+            struct A { let `catch`: Int }
+            let a = A(
+                `catch`: 0
+            )
+        """)
     ]
 
     static let triggeringExamples = [
-        Example("if true {\n    foo()\n}↓else {\n    bar()\n}"),
-        Example("if true {\n    foo()\n}↓    else if true {\n    bar()\n}"),
-        Example("if true {\n    foo()\n}↓\nelse true {\n    bar()\n}"),
-        Example("do {\n    foo()\n}↓catch {\n    bar()\n}"),
-        Example("do {\n    foo()\n}↓    catch {\n    bar()\n}"),
-        Example("do {\n    foo()\n}↓\ncatch {\n    bar()\n}")
+        Example("""
+            if true {
+                foo()
+            }↓else {
+                bar()
+            }
+        """),
+        Example("""
+            if true {
+                foo()
+            }↓    else if true {
+                bar()
+            }
+        """),
+        Example("""
+            if true {
+                foo()
+            }↓
+            else true {
+                bar()
+            }
+        """),
+        Example("""
+            do {
+                foo()
+            }↓catch {
+                bar()
+            }
+        """),
+        Example("""
+            do {
+                foo()
+            }↓    catch {
+                bar()
+            }
+        """),
+        Example("""
+            do {
+                foo()
+            }↓
+            catch {
+                bar()
+            }
+        """)
     ]
 
     static let corrections = [
-         Example("if true {\n    foo()\n}↓\nelse {\n    bar()\n}"):
-             Example("if true {\n    foo()\n} else {\n    bar()\n}"),
-         Example("if true {\n    foo()\n}↓   else if true {\n    bar()\n}"):
-             Example("if true {\n    foo()\n} else if true {\n    bar()\n}"),
-         Example("do {\n    foo()\n}↓\ncatch {\n    bar()\n}"):
-             Example("do {\n    foo()\n} catch {\n    bar()\n}"),
-         Example("do {\n    foo()\n}↓    catch {\n    bar()\n}"):
-             Example("do {\n    foo()\n} catch {\n    bar()\n}")
-     ]
- }
+        Example("""
+            if true {
+                foo()
+            }↓
+            else {
+                bar()
+            }
+        """): Example("""
+                if true {
+                    foo()
+                } else {
+                    bar()
+                }
+            """),
+        Example("""
+            if true {
+                foo()
+            }↓   else if true {
+                bar()
+            }
+        """): Example("""
+                if true {
+                    foo()
+                } else if true {
+                    bar()
+                }
+            """),
+        Example("""
+            do {
+                foo()
+            }↓
+            catch {
+                bar()
+            }
+        """): Example("""
+                do {
+                    foo()
+                } catch {
+                    bar()
+                }
+            """),
+        Example("""
+            do {
+                foo()
+            }↓    catch {
+                bar()
+            }
+        """): Example("""
+                do {
+                    foo()
+                } catch {
+                    bar()
+                }
+            """)
+    ]
+}
--- Tests/SwiftLintFrameworkTests/StatementPositionRuleTests.swift
+++ Tests/SwiftLintFrameworkTests/StatementPositionRuleTests.swift
@@ -2,38 +2,199 @@
 
 class StatementPositionRuleTests: SwiftLintTestCase {
     let nonTriggeringExamples = [
-        Example("if true {\n    foo()\n}\nelse {\n    bar()\n}"),
-        Example("if true {\n    foo()\n}\nelse if true {\n    bar()\n}\nelse {\n    return\n}"),
-        Example("if true { foo() }\nelse { bar() }"),
-        Example("if true { foo() }\nelse if true { bar() }\nelse { return }"),
-        Example("do {\n    foo()\n}\ncatch {\n    bar()\n}"),
-        Example("do {\n    foo()\n}\ncatch {\n    bar()\n}\ncatch {\n    return\n}"),
-        Example("do { foo() }\ncatch { bar() }"),
-        Example("do { foo() }\ncatch { bar() }\ncatch { return }")
+        Example("""
+            if true {
+                foo()
+            }
+            else {
+                bar()
+            }
+        """),
+        Example("""
+            if true {
+                foo()
+            }
+            else if true {
+                bar()
+            }
+            else {
+                return
+            }
+        """),
+        Example("""
+            if true { foo() }
+            else { bar() }
+        """),
+        Example("""
+            if true { foo() }
+            else if true { bar() }
+            else { return }
+        """),
+        Example("""
+            do {
+                foo()
+            }
+            catch {
+                bar()
+            }
+        """),
+        Example("""
+            do {
+                foo()
+            }
+            catch {
+                bar()
+            }
+            catch {
+                return
+            }
+        """),
+        Example("""
+            do { foo() }
+            catch { bar() }
+        """),
+        Example("""
+            do { foo() }
+            catch { bar() }
+            catch { return }
+        """)
     ]
 
     let triggeringExamples = [
-        Example("if true {\n    foo()\n}↓ else {\n    bar()\n}"),
-        Example("if true {\n    foo()\n}↓ else if true {\n    bar()\n}↓ else {\n    return\n}"),
-        Example("if true {\n    foo()\n}↓\n    else {\n    bar()\n}"),
-        Example("do {\n    foo()\n}↓ catch {\n    bar()\n}"),
-        Example("do {\n    foo()\n}↓ catch let error {\n    bar()\n}↓ catch {\n    return\n}"),
-        Example("do {\n    foo()\n}↓\n    catch {\n    bar()\n}")
+        Example("""
+            if true {
+                foo()
+            }↓ else {
+                bar()
+            }
+        """),
+        Example("""
+            if true {
+                foo()
+            }↓ else if true {
+                bar()
+            }↓ else {
+                return
+            }
+        """),
+        Example("""
+            if true {
+                foo()
+            }↓
+                else {
+                bar()
+            }
+        """),
+        Example("""
+            do {
+                foo()
+            }↓ catch {
+                bar()
+            }
+        """),
+        Example("""
+            do {
+                foo()
+            }↓ catch let error {
+                bar()
+            }↓ catch {
+                return
+            }
+        """),
+        Example("""
+            do {
+                foo()
+            }↓
+                catch {
+                bar()
+            }
+        """)
     ]
 
     let corrections = [
-        Example("if true {\n    foo()\n}↓\n    else {\n    bar()\n}"):
-            Example("if true {\n    foo()\n}\nelse {\n    bar()\n}"),
-        Example("if true {\n    foo()\n}↓ else if true {\n    bar()\n}↓ else {\n    bar()\n}"):
-            Example("if true {\n    foo()\n}\nelse if true {\n    bar()\n}\nelse {\n    bar()\n}"),
-        Example("  if true {\n    foo()\n  }↓\nelse if true {\n    bar()\n  }"):
-            Example("  if true {\n    foo()\n  }\n  else if true {\n    bar()\n  }"),
-        Example("do {\n    foo()\n}↓ catch {\n    bar()\n}"):
-            Example("do {\n    foo()\n}\ncatch {\n    bar()\n}"),
-        Example("do {\n    foo()\n}↓\n    catch {\n    bar()\n}"):
-            Example("do {\n    foo()\n}\ncatch {\n    bar()\n}"),
-        Example("  do {\n    foo()\n  }↓\ncatch {\n    bar()\n  }"):
-            Example("  do {\n    foo()\n  }\n  catch {\n    bar()\n  }")
+        Example("""
+            if true {
+                foo()
+            }↓
+                else {
+                bar()
+            }
+        """):
+            Example("""
+                if true {
+                    foo()
+                }
+                else {
+                    bar()
+                }
+            """),
+        Example("""
+            if true {
+                foo()
+            }↓ else if true {
+                bar()
+            }↓ else {
+                bar()
+            }
+        """):
+            Example("""
+                if true {
+                    foo()
+                }
+                else if true {
+                    bar()
+                }
+                else {
+                    bar()
+                }
+            """),
+        Example("""
+            if true {
+                foo()
+              }
+            else if true {
+                bar()
+              }
+        """):
+            Example("""
+                if true {
+                    foo()
+                  }
+                else if true {
+                    bar()
+                  }
+            """),
+        Example("""
+            do {
+                foo()
+            }↓ catch {
+                bar()
+            }
+        """):
+            Example("""
+                do {
+                    foo()
+                }
+                catch {
+                    bar()
+                }
+            """),
+        Example("""
+            do {
+                foo()
+            }↓
+          catch {
+                bar()
+            }
+        """):
+            Example("""
+                do {
+                    foo()
+                }
+                catch {
+                    bar()
+                }
+            """)
     ]
 
     func testUncuddled() {

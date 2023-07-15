3b31e40a2eaab05990144da77be5abfa186c25ab
Danny Mösch <danny.moesch@icloud.com>
2023-07-16 00:24:28 +0200

    Merge extension methods using default arguments (#5126)

--- Tests/SwiftLintTestHelpers/RuleDescription+Examples.swift
+++ Tests/SwiftLintTestHelpers/RuleDescription+Examples.swift
@@ -1,36 +1,16 @@
 import SwiftLintFramework
 
 public extension RuleDescription {
-    func with(nonTriggeringExamples: [Example],
-              triggeringExamples: [Example]) -> RuleDescription {
-        return RuleDescription(identifier: identifier,
-                               name: name,
-                               description: description,
-                               kind: kind,
-                               nonTriggeringExamples: nonTriggeringExamples,
-                               triggeringExamples: triggeringExamples,
-                               corrections: corrections,
-                               deprecatedAliases: deprecatedAliases)
-    }
-
-    func with(nonTriggeringExamples: [Example]) -> RuleDescription {
-        return with(nonTriggeringExamples: nonTriggeringExamples,
-                    triggeringExamples: triggeringExamples)
-    }
-
-    func with(triggeringExamples: [Example]) -> RuleDescription {
-        return with(nonTriggeringExamples: nonTriggeringExamples,
-                    triggeringExamples: triggeringExamples)
-    }
-
-    func with(corrections: [Example: Example]) -> RuleDescription {
-        return RuleDescription(identifier: identifier,
-                               name: name,
-                               description: description,
-                               kind: kind,
-                               nonTriggeringExamples: nonTriggeringExamples,
-                               triggeringExamples: triggeringExamples,
-                               corrections: corrections,
-                               deprecatedAliases: deprecatedAliases)
+    func with(nonTriggeringExamples: [Example]? = nil,
+              triggeringExamples: [Example]? = nil,
+              corrections: [Example: Example]? = nil) -> RuleDescription {
+        RuleDescription(identifier: identifier,
+                        name: name,
+                        description: description,
+                        kind: kind,
+                        nonTriggeringExamples: nonTriggeringExamples ?? self.nonTriggeringExamples,
+                        triggeringExamples: triggeringExamples ?? self.triggeringExamples,
+                        corrections: corrections ?? self.corrections,
+                        deprecatedAliases: deprecatedAliases)
     }
 }

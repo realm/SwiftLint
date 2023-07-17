88f9d0262afa2523085bbd29942aea27f9bc5d2b
Matthew Thompson <thompson.matthew@heb.com>
2023-07-17 09:49:48 -0500

    Update deprecated types from previous SwiftSyntax version and relocate AttribteListSyntax extension to rule declaration to prevent linter integration warning in project

--- Source/SwiftLintBuiltInRules/Rules/Lint/PrivateSwiftUIStatePropertyRule.swift
+++ Source/SwiftLintBuiltInRules/Rules/Lint/PrivateSwiftUIStatePropertyRule.swift
@@ -3,54 +3,16 @@ import SwiftSyntax
 /// Rule to require that any state properties in SwiftUI be declared as private.
 /// State properties should only be accessible from inside the View's body, or from methods called by it
 struct PrivateSwiftUIStatePropertyRule: SwiftSyntaxRule, OptInRule, ConfigurationProviderRule {
-    var configuration = SeverityConfiguration(.warning)
+    var configuration = SeverityConfiguration<Self>(.warning)
 
     static let description = RuleDescription(
         identifier: "private_swiftui_state",
         name: "Private SwiftUI @State Properties",
         description: "SwiftUI's state properties should be private",
         kind: .lint,
-        nonTriggeringExamples: [
-            Example(
-                """
-                struct ContentView: View {
-                    @State private var isPlaying: Bool = false
-                }
-                """
-            ),
-            Example(
-                """
-                struct ContentView: View {
-                    @State fileprivate var isPlaying: Bool = false
-                }
-                """
-            ),
-            Example(
-                """
-                struct ContentView: View {
-                    var isPlaying = false
-                }
-                """
-            ),
-            Example(
-                """
-                struct ContentView: View {
-                    @StateObject var foo = Foo()
-                }
-                """
-            )
-        ],
-        triggeringExamples: [
-            Example(
-                """
-                struct ContentView: View {
-                    @State var isPlaying: Bool = false
-                }
-                """
-            )
-        ])
-
-    init() {}
+        nonTriggeringExamples: PrivateSwiftUIStatePropertyRuleExamples.nonTriggeringExamples,
+        triggeringExamples: PrivateSwiftUIStatePropertyRuleExamples.triggeringExamples
+    )
 
     func makeVisitor(file: SwiftLintFile) -> ViolationsSyntaxVisitor {
         Visitor(viewMode: .sourceAccurate)
@@ -68,7 +30,23 @@ private extension PrivateSwiftUIStatePropertyRule {
                 return
             }
 
-            violations.append(decl.letOrVarKeyword.positionAfterSkippingLeadingTrivia)
+            violations.append(decl.bindingKeyword.positionAfterSkippingLeadingTrivia)
+        }
+    }
+}
+
+private extension AttributeListSyntax? {
+    /// Returns `true` if the attribute's identifier is equal to "State"
+    var hasStateAttribute: Bool {
+        guard let attributes = self else { return false }
+
+        return attributes.contains { attr in
+            guard let stateAttr = attr.as(AttributeSyntax.self),
+                  let identifier = stateAttr.attributeName.as(SimpleTypeIdentifierSyntax.self) else {
+                return false
+            }
+
+            return identifier.name.text == "State"
         }
     }
 }

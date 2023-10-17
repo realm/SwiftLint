b86ea23758a158eea1f198aa3985a124e56b3e95
JP Simard <jp@jpsim.com>
2023-10-17 13:37:46 -0400

    Update Sourcery to 2.1.2

--- Tests/GeneratedTests/GeneratedTests.swift
+++ Tests/GeneratedTests/GeneratedTests.swift
@@ -1,4 +1,4 @@
-// Generated using Sourcery 2.0.2 — https://github.com/krzysztofzablocki/Sourcery
+// Generated using Sourcery 2.1.2 — https://github.com/krzysztofzablocki/Sourcery
 // DO NOT EDIT
 @testable import SwiftLintBuiltInRules
 @_spi(TestHelper)

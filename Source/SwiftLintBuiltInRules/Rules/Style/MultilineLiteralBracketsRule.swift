ff1e966e3ae8ef9876ec5e80c060a46b5faae8f4
Marcelo Fabri <me@marcelofabri.com>
2023-11-19 12:23:26 -0800

    Rewrite multiline_literal_brackets using SwiftSyntax (#5333)

--- Source/SwiftLintBuiltInRules/Rules/Style/MultilineLiteralBracketsRule.swift
+++ Source/SwiftLintBuiltInRules/Rules/Style/MultilineLiteralBracketsRule.swift
@@ -1,7 +1,8 @@
 import Foundation
-import SourceKittenFramework
+import SwiftSyntax
 
-struct MultilineLiteralBracketsRule: ASTRule, OptInRule {
+@SwiftSyntaxRule
+struct MultilineLiteralBracketsRule: OptInRule {
     var configuration = SeverityConfiguration<Self>(.warning)
 
     static let description = RuleDescription(
@@ -97,43 +98,79 @@ struct MultilineLiteralBracketsRule: ASTRule, OptInRule {
                      4, 5, 6,
                      7, 8, 9
                 ]
+            """),
+            Example("""
+            class Hogwarts {
+                let houseCup = [
+                    "gryffindor": 460, "hufflepuff": 370,
+                    "ravenclaw": 410, "slytherin": slytherinPoints.filter {
+                        $0.isValid
+                    }.sum()↓]
+            }
             """)
         ]
     )
+}
 
-    func validate(file: SwiftLintFile,
-                  kind: SwiftExpressionKind,
-                  dictionary: SourceKittenDictionary) -> [StyleViolation] {
-        guard
-            [.array, .dictionary].contains(kind),
-            let bodyByteRange = dictionary.bodyByteRange,
-            let body = file.stringView.substringWithByteRange(bodyByteRange)
-        else {
-            return []
-        }
-
-        let isMultiline = body.contains("\n")
-        guard isMultiline else {
-            return []
-        }
-
-        let expectedBodyBeginRegex = regex("\\A[ \\t]*\\n")
-        let expectedBodyEndRegex = regex("\\n[ \\t]*\\z")
-
-        var violatingByteOffsets = [ByteCount]()
-        if expectedBodyBeginRegex.firstMatch(in: body, options: [], range: body.fullNSRange) == nil {
-            violatingByteOffsets.append(bodyByteRange.location)
-        }
-
-        if expectedBodyEndRegex.firstMatch(in: body, options: [], range: body.fullNSRange) == nil {
-            violatingByteOffsets.append(bodyByteRange.upperBound)
-        }
-
-        return violatingByteOffsets.map { byteOffset in
-            StyleViolation(
-                ruleDescription: Self.description, severity: configuration.severity,
-                location: Location(file: file, byteOffset: byteOffset)
+private extension MultilineLiteralBracketsRule {
+    final class Visitor: ViolationsSyntaxVisitor<ConfigurationType> {
+        override func visitPost(_ node: ArrayExprSyntax) {
+            validate(
+                node,
+                openingToken: node.leftSquare,
+                closingToken: node.rightSquare,
+                firstElement: node.elements.first?.expression,
+                lastElement: node.elements.last?.expression
             )
+        }
+
+        override func visitPost(_ node: DictionaryExprSyntax) {
+            switch node.content {
+            case .colon:
+                break
+            case .elements(let elements):
+                validate(
+                    node,
+                    openingToken: node.leftSquare,
+                    closingToken: node.rightSquare,
+                    firstElement: elements.first?.key,
+                    lastElement: elements.last?.value
+                )
+            }
+        }
+
+        private func validate(_ node: some ExprSyntaxProtocol,
+                              openingToken: TokenSyntax,
+                              closingToken: TokenSyntax,
+                              firstElement: (some ExprSyntaxProtocol)?,
+                              lastElement: (some ExprSyntaxProtocol)?) {
+            guard let firstElement, let lastElement,
+                    isMultiline(node) else {
+                return
+            }
+
+            if areOnTheSameLine(openingToken, firstElement) {
+                // don't skip trivia to keep violations in the same position as the legacy implementation
+                violations.append(firstElement.position)
+            }
+
+            if areOnTheSameLine(lastElement, closingToken) {
+                violations.append(closingToken.positionAfterSkippingLeadingTrivia)
+            }
+        }
+
+        private func isMultiline(_ node: some ExprSyntaxProtocol) -> Bool {
+            let startLocation = locationConverter.location(for: node.positionAfterSkippingLeadingTrivia)
+            let endLocation = locationConverter.location(for: node.endPositionBeforeTrailingTrivia)
+
+            return endLocation.line > startLocation.line
+        }
+
+        private func areOnTheSameLine(_ first: some SyntaxProtocol, _ second: some SyntaxProtocol) -> Bool {
+            let firstLocation = locationConverter.location(for: first.endPositionBeforeTrailingTrivia)
+            let secondLocation = locationConverter.location(for: second.positionAfterSkippingLeadingTrivia)
+
+            return firstLocation.line == secondLocation.line
         }
     }
 }

eecb44db6ed5bc60bb79faa254d65547f06034e7
Leonardo de Sousa Rodrigues <63623604+leonardosrodrigues0@users.noreply.github.com>
2023-11-30 17:35:53 -0300

    Add option to make `unneeded_override` rule affect initializers (#5270)

---
+++ Source/SwiftLintBuiltInRules/Rules/RuleConfigurations/UnneededOverrideRuleConfiguration.swift
@@ -0,0 +1,11 @@
+import SwiftLintCore
+
+@AutoApply
+struct UnneededOverrideRuleConfiguration: SeverityBasedRuleConfiguration {
+    typealias Parent = UnneededOverrideRule
+
+    @ConfigurationElement(key: "severity")
+    private(set) var severityConfiguration = SeverityConfiguration<Parent>(.warning)
+    @ConfigurationElement(key: "affect_initializers")
+    private(set) var affectInits = false
+}

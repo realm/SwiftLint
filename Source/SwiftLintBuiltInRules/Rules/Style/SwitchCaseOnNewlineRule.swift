c91e74e3b5aa197eb102f410bc2ec6e67f53ca4f
gsl-anthonymerle <anthony.merle@groupeseloger.com>
2024-03-05 11:49:49 +0100

    Add test cases for returnless cases with default configuration

--- Source/SwiftLintBuiltInRules/Rules/Style/SwitchCaseOnNewlineRule.swift
+++ Source/SwiftLintBuiltInRules/Rules/Style/SwitchCaseOnNewlineRule.swift
@@ -57,7 +57,16 @@ struct SwitchCaseOnNewlineRule: OptInRule {
             wrapInSwitch("↓case let .myCase(value) where value > 10: return false"),
             wrapInSwitch("↓case #selector(aFunction(_:)): return false"),
             wrapInSwitch("↓case let .myCase(value)\n where value > 10: return false"),
-            wrapInSwitch("↓case .first,\n .second: return false")
+            wrapInSwitch("↓case .first,\n .second: return false"),
+            wrapInSwitch("↓case 1: true"),
+            wrapInSwitch("↓case let value: true"),
+            wrapInSwitch("↓default: true"),
+            wrapInSwitch("↓case \"a string\": false"),
+            wrapInSwitch("↓case .myCase: false // error from network"),
+            wrapInSwitch("↓case let .myCase(value) where value > 10: false"),
+            wrapInSwitch("↓case #selector(aFunction(_:)): false"),
+            wrapInSwitch("↓case let .myCase(value)\n where value > 10: false"),
+            wrapInSwitch("↓case .first,\n .second: false")
         ]
     )
 }

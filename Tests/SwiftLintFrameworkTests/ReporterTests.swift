1b91e16495895b2107929ce62961e0da653e332e
Jacob Ackerman <jacob.ackerman145@gmail.com>
2024-03-29 22:00:04 -0500

    additional test improvements

--- Tests/SwiftLintFrameworkTests/ReporterTests.swift
+++ Tests/SwiftLintFrameworkTests/ReporterTests.swift
@@ -100,23 +100,25 @@ class ReporterTests: SwiftLintTestCase {
     func testSARIFReporter() {
         let expectedOutput = stringFromFile("CannedSARIFReporterOutput.json")
 
-        var expectedDictionary: [String: AnyObject] = [:]
+        var expectedDictionary: [String: Any] = [:]
         do {
-            expectedDictionary = try JSONSerialization.jsonObject(with: Data(expectedOutput.utf8), options: JSONSerialization.ReadingOptions.allowFragments) as! [String: AnyObject]
+            expectedDictionary = try JSONSerialization.jsonObject(with: Data(expectedOutput.utf8),
+                                                                  options: .allowFragments) as? [String: Any] ?? [:]
         } catch {
             print(error)
         }
 
         let result = SARIFReporter.generateReport(generateViolations())
 
-        var resultDictionary: [String: AnyObject] = [:]
+        var resultDictionary: [String: Any] = [:]
         do {
-            resultDictionary = try JSONSerialization.jsonObject(with: Data(result.utf8), options: JSONSerialization.ReadingOptions.allowFragments) as! [String: AnyObject]
+            resultDictionary = try JSONSerialization.jsonObject(with: Data(result.utf8),
+                                                                options: .allowFragments) as? [String: Any] ?? [:]
         } catch {
             print(error)
         }
 
-        XCTAssertEqual(expectedDictionary as NSDictionary, resultDictionary as NSDictionary)
+        XCTAssertTrue(areEqual(expectedDictionary, resultDictionary))
     }
 
     func testJunitReporter() {
@@ -182,5 +184,42 @@ class ReporterTests: SwiftLintTestCase {
             .trimmingTrailingCharacters(in: .whitespacesAndNewlines)
         let result = SummaryReporter.generateReport([])
         XCTAssertEqual(result, expectedOutput)
+    }
+}
+
+/// Utilities used to compare [String: Any] dictionaries
+private func areEqual (_ left: Any, _ right: Any) -> Bool {
+    if  type(of: left) == type(of: right) &&
+        String(describing: left) == String(describing: right) { return true }
+    if let left = left as? [Any], let right = right as? [Any] { return left == right }
+    if let left = left as? [AnyHashable: Any], let right = right as? [AnyHashable: Any] { return left == right }
+    return false
+}
+
+private extension Array where Element: Any {
+    static func != (left: [Element], right: [Element]) -> Bool { return !(left == right) }
+    static func == (left: [Element], right: [Element]) -> Bool {
+        if left.count != right.count { return false }
+        var right = right
+        loop: for leftValue in left {
+            for (rightIndex, rightValue) in right.enumerated() where areEqual(leftValue, rightValue) {
+                right.remove(at: rightIndex)
+                continue loop
+            }
+            return false
+        }
+        return true
+    }
+}
+
+private extension Dictionary where Value: Any {
+    static func != (left: [Key: Value], right: [Key: Value]) -> Bool { return !(left == right) }
+    static func == (left: [Key: Value], right: [Key: Value]) -> Bool {
+        if left.count != right.count { return false }
+        for element in left {
+            guard   let rightValue = right[element.key],
+                areEqual(rightValue, element.value) else { return false }
+        }
+        return true
     }
 }

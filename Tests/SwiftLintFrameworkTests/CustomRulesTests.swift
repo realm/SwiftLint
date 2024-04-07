ba6d41d93343afae97aa1f50d6285ea00120b56d
Martin Redington <martin.redington@hudl.com>
2024-04-07 22:00:32 +0100

    Better pattern

--- Tests/SwiftLintFrameworkTests/CustomRulesTests.swift
+++ Tests/SwiftLintFrameworkTests/CustomRulesTests.swift
@@ -245,7 +245,7 @@ class CustomRulesTests: SwiftLintTestCase {
             "only_rules": ["custom_rules", "superfluous_disable_command"],
             "custom_rules": [
                 "dont_print": [
-                  "regex": "print"
+                  "regex": "print\\("
                   ]
             ]
         ]

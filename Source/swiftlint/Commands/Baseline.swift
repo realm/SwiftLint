008328fd58b26cd9649b3daf3db9862b7d137405
Martin Redington <martin.redington@hudl.com>
2024-04-16 23:37:03 +0100

    Use subcommands

--- Source/swiftlint/Commands/Baseline.swift
+++ Source/swiftlint/Commands/Baseline.swift
@@ -2,29 +2,21 @@ import ArgumentParser
 import Foundation
 import SwiftLintFramework
 
-enum Action: String, ExpressibleByArgument {
-    case report, compare
-}
-
 extension SwiftLint {
     struct Baseline: ParsableCommand {
         static let configuration = CommandConfiguration(
-            abstract: "Reports the violations in a baseline (the '\(Action.report)' action) or the violations " +
-                      "that are present in another baseline, but not in the original (the '\(Action.compare)' action)."
+            abstract: "Reports the violations in a baseline (the 'report' subcommand) or the violations " +
+                      "that are present in another baseline, but not in the original (the 'compare' subcommand).",
+            subcommands: [Report.self, Compare.self]
         )
+    }
 
-        @Argument(help: "The action to perform on the baseline ('\(Action.report)' or '\(Action.compare)').")
-        var action: Action
+    private struct BaselineOptions: ParsableArguments {
         @Option(help: "The path to the baseline file.")
         var baseline: String
-        @Option(
-            help: """
-                  The path to a second baseline to compare against the baseline. Violations in \
-                  the second baseline that are not present in the original baseline will be reported \
-                  when the '\(Action.compare)' action is selected.
-                  """
-        )
-        var otherBaseline: String?
+    }
+
+    private struct ReportingOptions: ParsableArguments {
         @Option(
             help: """
                   The reporter used to report violations. The 'summary' reporter can be useful to \
@@ -34,59 +26,59 @@ extension SwiftLint {
         var reporter: String?
         @Option(help: "The file where violations should be saved. Prints to stdout by default.")
         var output: String?
+    }
+
+    private struct Report: ParsableCommand {
+        static let configuration = CommandConfiguration(abstract: "Reports the violations in a baseline.")
+
+        @OptionGroup var options: BaselineOptions
+        @OptionGroup var reportingOptions: ReportingOptions
 
         func run() throws {
-            switch action {
-            case .report:
-                try report()
-            case .compare:
-                try compare()
-            }
+            let savedBaseline = try SwiftLintCore.Baseline(fromPath: options.baseline)
+            try report(savedBaseline.violations, using: reportingOptions.reporter, to: reportingOptions.output)
             ExitHelper.successfullyExit()
         }
+    }
 
-        func validate() throws {
-            switch action {
-            case .report:
-                guard otherBaseline == nil else {
-                    throw ValidationError("Unexpected argument '--other-baseline <new-baseline>'")
-                }
-            case .compare:
-                guard otherBaseline != nil else {
-                    throw ValidationError("Missing expected argument '--other-baseline <other-baseline>'")
-                }
-            }
+    private struct Compare: ParsableCommand {
+        static let configuration = CommandConfiguration(
+            abstract: "Reports the violations that are present in another baseline " +
+                      "but not in the original baseline."
+        )
+
+        @OptionGroup var options: BaselineOptions
+        @Option(
+            help: """
+                  The path to a second baseline to compare against the baseline. Violations in \
+                  the second baseline that are not present in the original baseline will be reported.
+                  """
+        )
+        var otherBaseline: String
+        @OptionGroup var reportingOptions: ReportingOptions
+
+        func run() throws {
+            let baseline = try SwiftLintCore.Baseline(fromPath: options.baseline)
+            let otherBaseline = try SwiftLintCore.Baseline(fromPath: otherBaseline)
+            try report(baseline.compare(otherBaseline), using: reportingOptions.reporter, to: reportingOptions.output)
+            ExitHelper.successfullyExit()
         }
+    }
+}
 
-        private func report() throws {
-            let savedBaseline = try SwiftLintCore.Baseline(fromPath: baseline)
-            try report(savedBaseline.violations)
-        }
-
-        private func compare() throws {
-            guard let otherBaselinePath = otherBaseline else {
-                return
+private func report(_ violations: [StyleViolation], using reporterIdentifier: String?, to output: String?) throws {
+    let reporter = reporterFrom(identifier: reporterIdentifier)
+    let report = reporter.generateReport(violations)
+    if report.isNotEmpty {
+        if let output {
+            let data = Data((report + "\n").utf8)
+            do {
+                try data.write(to: URL(fileURLWithPath: output))
+            } catch {
+                Issue.fileNotWritable(path: output).print()
             }
-            let baseline = try SwiftLintCore.Baseline(fromPath: baseline)
-            let otherBaseline = try SwiftLintCore.Baseline(fromPath: otherBaselinePath)
-            try report(baseline.compare(otherBaseline))
-        }
-
-        private func report(_ violations: [StyleViolation]) throws {
-            let reporter = reporterFrom(identifier: reporter)
-            let report = reporter.generateReport(violations)
-            if report.isNotEmpty {
-                if let output {
-                    let data = Data((report + "\n").utf8)
-                    do {
-                        try data.write(to: URL(fileURLWithPath: output))
-                    } catch {
-                        Issue.fileNotWritable(path: output).print()
-                    }
-                } else {
-                    queuedPrint(report)
-                }
-            }
+        } else {
+            queuedPrint(report)
         }
     }
 }

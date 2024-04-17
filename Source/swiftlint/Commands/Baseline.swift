5bb2d9c858b5e4ee170c1aa2a91309bd78f9a373
Martin Redington <mildm8nnered@users.noreply.github.com>
2024-04-17 21:52:14 +0100

    Update Source/swiftlint/Commands/Baseline.swift
    Co-authored-by: Danny Mösch <danny.moesch@icloud.com>

--- Source/swiftlint/Commands/Baseline.swift
+++ Source/swiftlint/Commands/Baseline.swift
@@ -5,8 +5,7 @@ import SwiftLintFramework
 extension SwiftLint {
     struct Baseline: ParsableCommand {
         static let configuration = CommandConfiguration(
-            abstract: "Reports the violations in a baseline or the violations " +
-                      "that are present in another baseline, but not in the original.",
+            abstract: "Operations on existing baselines",
             subcommands: [Report.self, Compare.self],
             defaultSubcommand: Report.self
         )

79e54a75c346d8994b30d32ee87183c61ea0f0ab
Martin Redington <martin.redington@hudl.com>
2024-04-21 01:13:31 +0100

    Autocorrects

--- Source/swiftlint/Helpers/LintOrAnalyzeCommand.swift
+++ Source/swiftlint/Helpers/LintOrAnalyzeCommand.swift
@@ -127,7 +127,9 @@ struct LintOrAnalyzeCommand {
                 return try Baseline(fromPath: baselinePath)
             } catch {
                 Issue.baselineNotReadable(path: baselinePath).print()
-                if (error as? CocoaError)?.code != CocoaError.fileReadNoSuchFile || options.writeBaseline != options.baseline {
+                if
+                    (error as? CocoaError)?.code != CocoaError.fileReadNoSuchFile ||
+                    options.writeBaseline != options.baseline {
                     throw error
                 }
             }

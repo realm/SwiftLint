16c0213027d4047bd4c265faffad7ce3d104c67d
Garric G. Nahapetian <garricn@icloud.com>
2024-05-01 12:15:35 -0700

    Add option to `redundant_type_annotation` rule to consider default literal types redundant (#4756)

--- Source/SwiftLintBuiltInRules/Rules/RuleConfigurations/RedundantTypeAnnotationConfiguration.swift
+++ Source/SwiftLintBuiltInRules/Rules/RuleConfigurations/RedundantTypeAnnotationConfiguration.swift
@@ -8,4 +8,6 @@ struct RedundantTypeAnnotationConfiguration: SeverityBasedRuleConfiguration {
     var severityConfiguration = SeverityConfiguration<Parent>(.warning)
     @ConfigurationElement(key: "ignore_attributes")
     var ignoreAttributes = Set<String>(["IBInspectable"])
+    @ConfigurationElement(key: "consider_default_literal_types_redundant")
+    private(set) var considerDefaultLiteralTypesRedundant = false
 }

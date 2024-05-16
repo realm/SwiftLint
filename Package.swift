de66fea453ba730a3636328dc5185226c026418d
Danny Mösch <danny.moesch@icloud.com>
2024-05-16 19:31:26 +0200

    Completely exclude the binary target from the Linux build (#5585)

--- Package.swift
+++ Package.swift
@@ -143,11 +143,6 @@ let package = Package(
                 "SwiftLintTestHelpers"
             ]
         ),
-        .binaryTarget(
-            name: "SwiftLintBinary",
-            url: "https://github.com/realm/SwiftLint/releases/download/0.55.0/SwiftLintBinary-macos.artifactbundle.zip",
-            checksum: "0a689bf8f851e4ab06bfce1bf5a01840984473ef720a63916611664e442499d6"
-        ),
         .macro(
             name: "SwiftLintCoreMacros",
             dependencies: [
@@ -167,3 +162,13 @@ let package = Package(
         ),
     ]
 )
+
+#if os(macOS)
+package.targets.append(
+    .binaryTarget(
+        name: "SwiftLintBinary",
+        url: "https://github.com/realm/SwiftLint/releases/download/0.55.0/SwiftLintBinary-macos.artifactbundle.zip",
+        checksum: "0a689bf8f851e4ab06bfce1bf5a01840984473ef720a63916611664e442499d6"
+    )
+)
+#endif

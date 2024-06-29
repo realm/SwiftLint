cf1f0500c79b8b5650600b4993a39ef6d7d29eb0
Martin Redington <mildm8nnered@gmail.com>
2024-06-29 17:06:42 +0100

    Abstracted out the version comparison

--- Source/SwiftLintCore/Models/Version.swift
+++ Source/SwiftLintCore/Models/Version.swift
@@ -1,7 +1,11 @@
 /// A type describing the SwiftLint version.
-public struct Version {
+public struct Version: VersionComparable {
     /// The string value for this version.
     public let value: String
+
+    public var rawValue: String {
+        value
+    }
 
     /// The current SwiftLint version.
     public static let current = Version(value: "0.55.1")
@@ -14,25 +18,3 @@ public struct Version {
     }
 }
 
-extension Version: Comparable {
-    public static func == (lhs: Version, rhs: Version) -> Bool {
-        lhs.value == rhs.value
-    }
-
-    public static func < (lhs: Version, rhs: Version) -> Bool {
-        if let lhsComparators = lhs.comparators, let rhsComparators = rhs.comparators {
-            return lhsComparators.lexicographicallyPrecedes(rhsComparators)
-        }
-        return lhs.value < rhs.value
-    }
-
-    private var comparators: [Int]? {
-        let components = value.split(separator: ".").compactMap { Int($0) }
-        guard let major = components.first else {
-            return nil
-        }
-        let minor = components.dropFirst(1).first ?? 0
-        let patch = components.dropFirst(2).first ?? 0
-        return [major, minor, patch]
-    }
-}

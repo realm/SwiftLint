411a2d94f20151974cdb34812a5c487633631296
aryamansharda <asharda@turo.com>
2024-07-13 01:31:16 -0700

    Adding rewriter

--- Source/SwiftLintBuiltInRules/Rules/Style/AccessControlSetterSpacingRule.swift
+++ Source/SwiftLintBuiltInRules/Rules/Style/AccessControlSetterSpacingRule.swift
@@ -1,13 +1,13 @@
 import SwiftSyntax
 
-@SwiftSyntaxRule
-struct AccessControlSetterSpacingRule: SwiftSyntaxCorrectableRule {
+@SwiftSyntaxRule(explicitRewriter: true)
+struct AccessControlSetterSpacingRule: Rule {
     var configuration = SeverityConfiguration<Self>(.warning)
 
     static let description = RuleDescription(
         identifier: "access_control_setter_spacing",
         name: "Access Control Setter Spacing",
-        description: "Files should not contain leading whitespace",
+        description: "There should not be a space between the access control modifier and (set)",
         kind: .style,
         nonTriggeringExamples: [
             Example("private(set) var foo: Bool = false"),
@@ -22,14 +22,13 @@ struct AccessControlSetterSpacingRule: SwiftSyntaxCorrectableRule {
             Example("internal ↓(set) var foo: Bool = false"),
             Example("public ↓(set) var foo: Bool = false"),
             Example("  public  ↓(set) var foo: Bool = false"),
-        ]//,
-//        corrections: [
-//            Example("private ↓(set) var foo: Bool = false"): Example("private(set) var foo: Bool = false"),
-//            Example("fileprivate ↓(set) var foo: Bool = false"): Example("fileprivate(set) var foo: Bool = false"),
-//            Example("internal ↓(set) var foo: Bool = false"): Example("internal(set) var foo: Bool = false"),
-//            Example("public ↓(set) var foo: Bool = false"): Example("public(set) var foo: Bool = false"),
-//            Example("open ↓(set) var foo: Bool = false"): Example("open(set) var foo: Bool = false"),
-//        ]
+        ],
+        corrections: [
+            Example("private ↓(set) var foo: Bool = false"): Example("private(set) var foo: Bool = false"),
+            Example("fileprivate ↓(set) var foo: Bool = false"): Example("fileprivate(set) var foo: Bool = false"),
+            Example("internal ↓(set) var foo: Bool = false"): Example("internal(set) var foo: Bool = false"),
+            Example("public ↓(set) var foo: Bool = false"): Example("public(set) var foo: Bool = false"),
+        ]
     )
 }
 
@@ -37,13 +36,28 @@ private extension AccessControlSetterSpacingRule {
     final class Visitor: ViolationsSyntaxVisitor<ConfigurationType> {
         override func visitPost(_ node: DeclModifierSyntax) {
             // If there is an access level modifier followed be a (set)
-            guard let _ = node.asAccessLevelModifier, let detail = node.detail, detail.detail.tokenKind == .identifier("set") else {
+            guard let _ = node.asAccessLevelModifier, node.detail?.detail.tokenKind == .identifier("set") else {
                 return
             }
 
             if node.name.trailingTrivia.isNotEmpty {
                 violations.append(node.name.endPosition)
             }
+        }
+    }
+
+    final class Rewriter: ViolationsSyntaxRewriter<ConfigurationType> {
+        override func visit(_ node: DeclModifierSyntax) -> DeclModifierSyntax {
+            guard node.name.trailingTrivia.isNotEmpty else {
+                return super.visit(node)
+            }
+
+            correctionPositions.append(node.name.endPosition)
+
+            // Remove trailing whitespace from the name token
+            let cleanedName = node.name.with(\.trailingTrivia, Trivia())
+            let newNode = node.with(\.name, cleanedName)
+            return super.visit(newNode)
         }
     }
 }

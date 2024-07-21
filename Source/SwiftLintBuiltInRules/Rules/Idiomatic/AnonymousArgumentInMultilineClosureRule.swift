676562b7ea08ba88b63d3cb25a1c6c81fb745cee
Martin Redington <mildm8nnered@gmail.com>
2024-07-21 13:19:18 +0100

    Wording

--- Source/SwiftLintBuiltInRules/Rules/Idiomatic/AnonymousArgumentInMultilineClosureRule.swift
+++ Source/SwiftLintBuiltInRules/Rules/Idiomatic/AnonymousArgumentInMultilineClosureRule.swift
@@ -9,7 +9,7 @@ struct AnonymousArgumentInMultilineClosureRule: OptInRule {
         name: "Anonymous Argument in Multiline Closure",
         description: "Use named arguments in multiline closures",
         rationale: """
-        In multiline closures, prefer using named arguments
+        In multiline closures, for clarity, prefer using named arguments
 
         ```
             closure { arg in

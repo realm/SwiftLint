95d4b30ae0457069c016a21ebc8fe89939cdb422
Martin Redington <mildm8nnered@gmail.com>
2024-07-22 18:26:27 +0100

    Fixed typos

--- Source/SwiftLintBuiltInRules/Rules/Metrics/ClosureBodyLengthRule.swift
+++ Source/SwiftLintBuiltInRules/Rules/Metrics/ClosureBodyLengthRule.swift
@@ -1,6 +1,6 @@
 struct ClosureBodyLengthRule: OptInRule, SwiftSyntaxRule {
-    let defaultWarningThreshold = 30
-    var configuration = SeverityLevelsConfiguration<Self>(warning: defaultWarningThreshold, error: 100)
+    private static let defaultWarningThreshold = 30
+    var configuration = SeverityLevelsConfiguration<Self>(warning: Self.defaultWarningThreshold, error: 100)
 
     static let description = RuleDescription(
         identifier: "closure_body_length",
@@ -11,8 +11,8 @@ struct ClosureBodyLengthRule: OptInRule, SwiftSyntaxRule {
 
         Possibly you could refactor your closure code and extract some of it into a function.
 
-        Many installations, including SwiftLint's, increase the default warning value from \(defaultWarningThreshold) \
-        to 50, which is a bit more permissive.
+        Many installations, including SwiftLint's, increase the default warning value from \
+        \(Self.defaultWarningThreshold) to 50, which is a bit more permissive.
         """,
         kind: .metrics,
         nonTriggeringExamples: ClosureBodyLengthRuleExamples.nonTriggeringExamples,

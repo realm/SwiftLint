ba3819dd99fdfd150ae6df74097004ddfce9d8e5
Danny Mösch <danny.moesch@icloud.com>
2024-07-26 18:47:43 +0200

    Fix some issue in SARIF reporter

--- Source/SwiftLintCore/Reporters/JSONReporter.swift
+++ Source/SwiftLintCore/Reporters/JSONReporter.swift
@@ -10,7 +10,7 @@ struct JSONReporter: Reporter {
     static let description = "Reports violations as a JSON array."
 
     static func generateReport(_ violations: [StyleViolation]) -> String {
-        toJSON(violations.map(dictionary(for:)))
+        toJSON(violations.map(dictionary(for:)), options: [.prettyPrinted, .sortedKeys, .withoutEscapingSlashes])
     }
 
     // MARK: - Private

d17e18df36306206c67ed44f290625906149bd2f
wanxiangchwng <cui.shuang@foxmail.com>
2024-08-01 17:29:50 +0800

    chore: fix some comments
    Signed-off-by: wanxiangchwng <cui.shuang@foxmail.com>

--- Source/SwiftLintCore/Extensions/Dictionary+SwiftLint.swift
+++ Source/SwiftLintCore/Extensions/Dictionary+SwiftLint.swift
@@ -193,7 +193,7 @@ extension SourceKittenDictionary {
     /// Block executed for every encountered entity during traversal of a dictionary.
     public typealias TraverseBlock<T> = (_ parent: SourceKittenDictionary, _ entity: SourceKittenDictionary) -> T?
 
-    /// Traversing all substuctures of the dictionary hierarchically, calling `traverseBlock` on each node.
+    /// Traversing all substructures of the dictionary hierarchically, calling `traverseBlock` on each node.
     /// Traversing using depth first strategy, so deepest substructures will be passed to `traverseBlock` first.
     ///
     /// - parameter traverseBlock: block that will be called for each substructure in the dictionary.

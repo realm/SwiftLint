4a2e7bd2e722d63aa90e841de5daac7598f765ca
Danny Mösch <danny.moesch@icloud.com>
2024-08-11 17:05:53 +0200

    Use lock

--- Source/SwiftLintCore/Extensions/Configuration+FileGraphSubtypes.swift
+++ Source/SwiftLintCore/Extensions/Configuration+FileGraphSubtypes.swift
@@ -8,7 +8,7 @@ extension Configuration.FileGraph {
     }
 
     // MARK: - Vertex
-    final class Vertex: Hashable, Sendable {
+    final class Vertex: Hashable, @unchecked Sendable {
         var originatesFromRemote: Bool { originalRemoteString != nil }
         var rootDirectory: String {
             if !originatesFromRemote, case let .existing(path) = filePath {
@@ -22,8 +22,9 @@ extension Configuration.FileGraph {
         private let originalRootDirectory: String
         let originalRemoteString: String?
         let isInitialVertex: Bool
-        private(set) nonisolated(unsafe) var filePath: FilePath
-        private(set) nonisolated(unsafe) var configurationDict: [String: any Sendable]
+        private let lock = NSLock()
+        private(set) var filePath: FilePath
+        private(set) var configurationDict: [String: any Sendable]
 
         convenience init(string: String, rootDirectory: String, isInitialVertex: Bool) {
             let (originalRemoteString, filePath): (String?, FilePath) =
@@ -66,12 +67,14 @@ extension Configuration.FileGraph {
             remoteConfigTimeout: TimeInterval,
             remoteConfigTimeoutIfCached: TimeInterval
         ) throws {
-            let path = try filePath.resolve(
-                remoteConfigTimeout: remoteConfigTimeout,
-                remoteConfigTimeoutIfCached: remoteConfigTimeoutIfCached
-            )
-            self.filePath = .existing(path: path)
-            self.configurationDict = try YamlParser.parse(read(at: path))
+            try lock.withLock {
+                let path = try filePath.resolve(
+                    remoteConfigTimeout: remoteConfigTimeout,
+                    remoteConfigTimeoutIfCached: remoteConfigTimeoutIfCached
+                )
+                self.filePath = .existing(path: path)
+                self.configurationDict = try YamlParser.parse(read(at: path))
+            }
         }
 
         private func read(at path: String) throws -> String {

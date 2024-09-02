d3d888695d97605fd5d549cc76f1d9d2f0cae4df
Kim de Vos <kim.de.vos@vippsmobilepay.com>
2024-09-02 16:39:53 +0200

    Add max number if params allowed

--- Source/SwiftLintBuiltInRules/Rules/RuleConfigurations/MultilineParametersConfiguration.swift
+++ Source/SwiftLintBuiltInRules/Rules/RuleConfigurations/MultilineParametersConfiguration.swift
@@ -8,4 +8,6 @@ struct MultilineParametersConfiguration: SeverityBasedRuleConfiguration {
     private(set) var severityConfiguration = SeverityConfiguration<Parent>(.warning)
     @ConfigurationElement(key: "allows_single_line")
     private(set) var allowsSingleLine = true
+    @ConfigurationElement(key: "max_number_of_single_line_parameters")
+    private(set) var maxNumberOfSingleLineParameters: Int?
 }

85c4dbe963ab42875fd4d89307b628aeae073ec1
Danny Mösch <danny.moesch@icloud.com>
2024-09-09 23:18:00 +0200

    Ensure expected initializer signature (#5786)

--- Source/SwiftLintBuiltInRules/Rules/Lint/OptionalDataStringConversionRule.swift
+++ Source/SwiftLintBuiltInRules/Rules/Lint/OptionalDataStringConversionRule.swift
@@ -9,7 +9,10 @@ struct OptionalDataStringConversionRule: Rule {
         description: "Prefer failable `String(data:encoding:)` initializer when converting `Data` to `String`",
         kind: .lint,
         nonTriggeringExamples: [
-            Example("String(data: data, encoding: .utf8)")
+            Example("String(data: data, encoding: .utf8)"),
+            Example("String(UTF8.self)"),
+            Example("String(a, b, c, UTF8.self)"),
+            Example("String(decoding: data, encoding: UTF8.self)"),
         ],
         triggeringExamples: [
             Example("String(decoding: data, as: UTF8.self)")
@@ -22,6 +25,7 @@ private extension OptionalDataStringConversionRule {
         override func visitPost(_ node: DeclReferenceExprSyntax) {
             if node.baseName.text == "String",
                let parent = node.parent?.as(FunctionCallExprSyntax.self),
+               parent.arguments.map(\.label?.text) == ["decoding", "as"],
                let expr = parent.arguments.last?.expression.as(MemberAccessExprSyntax.self),
                expr.base?.description == "UTF8",
                expr.declName.baseName.description == "self" {

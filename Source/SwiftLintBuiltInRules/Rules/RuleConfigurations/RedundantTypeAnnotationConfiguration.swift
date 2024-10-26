325c4ff24a09c60012b73ef94e8dac3cc61b077e
Jared Grubb <jaredgrubb+github@gmail.com>
2024-10-26 13:00:13 -0700

    RedundantTypeAnnotation: add 'ignore_type_interfaces' option
    The `redundant_type_annotation` and `explicit_type_interface` rules conflict.
    For users that want to have an explicit type interface, but still be able to write
    simple code inside the bodies of functions, I want to add an option to
    `redundant_type_annotation` that has it ignore "type interfaces".
    
    For example, this allows:
    ```
    struct Foo {
        var bar: Bar = Bar() // OK: ignore this! I want explicit types on interfaces.
        func baz() {
           let bar: Bar = Bar() // WARN: redundant_type_annotation kicks in here though.
        }
    }
    ```

--- Source/SwiftLintBuiltInRules/Rules/RuleConfigurations/RedundantTypeAnnotationConfiguration.swift
+++ Source/SwiftLintBuiltInRules/Rules/RuleConfigurations/RedundantTypeAnnotationConfiguration.swift
@@ -8,6 +8,8 @@ struct RedundantTypeAnnotationConfiguration: SeverityBasedRuleConfiguration {
     var severityConfiguration = SeverityConfiguration<Parent>(.warning)
     @ConfigurationElement(key: "ignore_attributes")
     var ignoreAttributes = Set<String>(["IBInspectable"])
+    @ConfigurationElement(key: "ignore_type_interfaces")
+    private(set) var ignoreTypeInterfaces = false
     @ConfigurationElement(key: "consider_default_literal_types_redundant")
     private(set) var considerDefaultLiteralTypesRedundant = false
 }

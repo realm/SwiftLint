26923b022daa2bde60dd13f7d68af2eaee96d0b3
Martin Redington <mildm8nnered@gmail.com>
2024-11-05 22:31:16 +0000

    CR comments

--- Source/SwiftLintBuiltInRules/Rules/Style/AttributesRule.swift
+++ Source/SwiftLintBuiltInRules/Rules/Style/AttributesRule.swift
@@ -12,21 +12,20 @@ struct AttributesRule: Rule {
             imports
             """,
         rationale: """
-        See https://ericasadun.com/2016/10/02/quick-style-survey/ for discussion.
+        Erica Sadun says:
 
-        Summarizing here: "[Erica Sadun's] take on things after the poll and after talking directly with a number of \
+        > my take on things after the poll and after talking directly with a number of \
         developers is this: Placing attributes like `@objc`, `@testable`, `@available`, `@discardableResult` on \
-        their own lines before a member declaration has become a conventional Swift style."
+        their own lines before a member declaration has become a conventional Swift style.
 
-        "This approach limits declaration length. It allows a member to float below its attribute and supports \
+        > This approach limits declaration length. It allows a member to float below its attribute and supports \
         flush-left access modifiers, so `internal`, `public`, etc appear in the leftmost column. Many developers \
-        mix-and-match styles for short Swift attributes like `@objc`"
+        mix-and-match styles for short Swift attributes like `@objc`
+
+        See https://ericasadun.com/2016/10/02/quick-style-survey/ for discussion.
 
         SwiftLint's rule requires attributes to be on their own lines for functions and types, but on the same line \
         for variables and imports.
-
-        The `attributes_with_arguments_always_on_line_above`, `always_on_same_line`, and `always_on_line_above` \
-        configuration parameters can be used to fine-tune the rules behaviour for particular attributes.
         """,
         kind: .style,
         nonTriggeringExamples: AttributesRuleExamples.nonTriggeringExamples,

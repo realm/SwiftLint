cd875d0a1b68a5caac475fc34a2f2ac8e27fc9ce
jkolarik-paylocity <138468976+jkolarik-paylocity@users.noreply.github.com>
2024-11-25 16:23:02 +0100

    Fix lint in rule

--- Source/SwiftLintBuiltInRules/Rules/Idiomatic/NoAsyncWithoutAwaitRule.swift
+++ Source/SwiftLintBuiltInRules/Rules/Idiomatic/NoAsyncWithoutAwaitRule.swift
@@ -100,6 +100,7 @@ private extension NoAsyncWithoutAwaitRule {
             }
         }
 
+        // swiftlint:disable:next unused_parameter
         override func visitPost(_ node: AwaitExprSyntax) {
             awaitCount.modifyLast {
                 $0.awaitCount += 1

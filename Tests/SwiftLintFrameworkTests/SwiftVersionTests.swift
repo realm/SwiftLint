191bb64d75295df779546ebd4fdc8878cb37d946
Danny Mösch <danny.moesch@icloud.com>
2024-12-14 22:33:59 +0100

    Support Swift version 6.0.3 (#5896)

--- Tests/SwiftLintFrameworkTests/SwiftVersionTests.swift
+++ Tests/SwiftLintFrameworkTests/SwiftVersionTests.swift
@@ -3,7 +3,9 @@ import XCTest
 
 final class SwiftVersionTests: SwiftLintTestCase {
     func testDetectSwiftVersion() {
-#if compiler(>=6.0.2)
+#if compiler(>=6.0.3)
+        let version = "6.0.3"
+#elseif compiler(>=6.0.2)
         let version = "6.0.2"
 #elseif compiler(>=6.0.1)
         let version = "6.0.1"

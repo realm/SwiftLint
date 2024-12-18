ebdd23a4adf918dad5954b98918136915b74aa13
Danny Mösch <danny.moesch@icloud.com>
2024-12-18 19:53:34 +0100

    Support Musl C library (#5899)

--- Source/SwiftLintFramework/ExitHelper.swift
+++ Source/SwiftLintFramework/ExitHelper.swift
@@ -1,12 +1,16 @@
 #if os(Linux)
-import Glibc
+#if canImport(Glibc)
+import func Glibc.exit
+#elseif canImport(Musl)
+import func Musl.exit
+#endif
 #endif
 
 package enum ExitHelper {
     package static func successfullyExit() {
 #if os(Linux)
         // Workaround for https://github.com/apple/swift/issues/59961
-        Glibc.exit(0)
+        exit(0)
 #endif
     }
 }

233897ae5e11bc9f479c96b6f098f24ded70dc94
Martin Redington <mildm8nnered@gmail.com>
2024-12-22 21:38:03 +0000

    Added unit tests

--- Tests/FrameworkTests/CoverageTests.swift
+++ Tests/FrameworkTests/CoverageTests.swift
@@ -3,23 +3,85 @@
 import XCTest
 
 final class CoverageTests: SwiftLintTestCase {
-    func testCoverage() {
-        let rules: [any Rule] = [
-            ArrayInitRule(),
-            BlockBasedKVORule(),
-            ClosingBraceRule(),
-            DirectReturnRule(),
-        ]
-        var coverage = Coverage(numberOfEnabledRules: 4, totalNumberOfRules: 10)
+    private static let rules: [any Rule] = [
+        ArrayInitRule(),
+        BlockBasedKVORule(),
+        ClosingBraceRule(),
+        DirectReturnRule(),
+    ]
+
+    private static let totalNumberOfRules = 10
+
+    func testEmptySourceCoverage() {
+        testCoverage(
+            source: "",
+            enabledRulesCoverage: "1.0",
+            allRulesCoverage: "0.4"
+        )
+    }
+
+    func testNoDisabledCommandCoverage() {
+        let source = """
+             func foo() -> Int {
+                 return 0
+             }
+             """
+
+        testCoverage(
+            source: source,
+            enabledRulesCoverage: "1.0",
+            allRulesCoverage: "0.4"
+        )
+    }
+
+    func testCoverageWithRegions() {
+        let sourceWithRegionsForEnabledRulesOnly = """
+             func foo() -> Int {
+                 // swiftlint:disable:next direct_return
+                 return 0
+             }
+             
+             // These blank lines keep the linecount consistent
+             """
+
+        let expectedEnabledRulesCoverage = "0.95"
+        let expectedAllRulesCoverage = "0.38"
+
+        testCoverage(
+            source: sourceWithRegionsForEnabledRulesOnly,
+            enabledRulesCoverage: expectedEnabledRulesCoverage,
+            allRulesCoverage: expectedAllRulesCoverage
+        )
+
+        let sourceWithRegionsForIrrelevantRules = sourceWithRegionsForEnabledRulesOnly
+            .components(separatedBy: "\n")
+            .dropLast()
+            .joined(separator: "\n")
+            + ("// swiftlint:disable:previous expiring_todo")
+
+        testCoverage(
+            source: sourceWithRegionsForIrrelevantRules,
+            enabledRulesCoverage: expectedEnabledRulesCoverage,
+            allRulesCoverage: expectedAllRulesCoverage
+        )
+    }
+
+    private func testCoverage(
+        for rules: [any Rule] = CoverageTests.rules,
+        totalNumberOfRules: Int = CoverageTests.totalNumberOfRules,
+        source: String,
+        enabledRulesCoverage: String,
+        allRulesCoverage: String
+    ) {
+        var coverage = Coverage(numberOfEnabledRules: rules.count, totalNumberOfRules: 10)
         XCTAssertEqual(coverage.enabledRulesCoverage, 0)
         XCTAssertEqual(coverage.allRulesCoverage, 0)
-        let file = SwiftLintFile(contents: "\n")
+        let file = SwiftLintFile(contents: source)
         coverage.addCoverage(for: file, rules: rules)
-        XCTAssertEqual(coverage.enabledRulesCoverage, 1.0)
-        XCTAssertEqual(coverage.allRulesCoverage, 0.4)
         XCTAssertEqual(coverage.report, """
-                                        Enabled rules coverage: 1.0
-                                            All rules coverage: 0.4
+                                        Enabled rules coverage: \(enabledRulesCoverage)
+                                            All rules coverage: \(allRulesCoverage)
                                         """)
+
     }
 }

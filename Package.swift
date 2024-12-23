5be335f89257bcebf9c2eb56dee623dfe85cd1a5
Danny Mösch <danny.moesch@icloud.com>
2024-12-23 16:32:35 +0100

    Update CryptoSwift with support for Musl C library

--- Package.swift
+++ Package.swift
@@ -35,7 +35,7 @@ let package = Package(
         .package(url: "https://github.com/jpsim/Yams.git", from: "5.0.6"),
         .package(url: "https://github.com/scottrhoyt/SwiftyTextTable.git", from: "0.9.0"),
         .package(url: "https://github.com/JohnSundell/CollectionConcurrencyKit.git", from: "0.2.0"),
-        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .upToNextMinor(from: "1.8.0")),
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .upToNextMinor(from: "1.8.4")),
     ],
     targets: [
         .plugin(

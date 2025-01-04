2cfae84a45a3568e124ffb119cdd4d37f86836d5
Martin Redington <mildm8nnered@gmail.com>
2025-01-04 09:54:14 +0000

    Removed mention of configuration options

--- Source/SwiftLintBuiltInRules/Rules/Metrics/ClosureBodyLengthRule.swift
+++ Source/SwiftLintBuiltInRules/Rules/Metrics/ClosureBodyLengthRule.swift
@@ -10,9 +10,6 @@ struct ClosureBodyLengthRule: OptInRule, SwiftSyntaxRule {
         "Closure bodies should not span too many lines" says it all.
 
         Possibly you could refactor your closure code and extract some of it into a function.
-
-        Many installations, including SwiftLint's, increase the default warning value from \
-        \(Self.defaultWarningThreshold) to 50, which is a bit more permissive.
         """,
         kind: .metrics,
         nonTriggeringExamples: ClosureBodyLengthRuleExamples.nonTriggeringExamples,

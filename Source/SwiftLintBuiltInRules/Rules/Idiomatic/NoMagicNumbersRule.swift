22f489ab8a5dc36cc174a2129ff8d76ead843900
Effie <effie.thedev@gmail.com>
2025-03-30 16:23:53 +0900

    Add Condition to ensure only `UIColor.init(...)` calls are matched (#5183)

--- Source/SwiftLintBuiltInRules/Rules/Idiomatic/NoMagicNumbersRule.swift
+++ Source/SwiftLintBuiltInRules/Rules/Idiomatic/NoMagicNumbersRule.swift
@@ -369,6 +369,7 @@ private extension ExprSyntaxProtocol {
                 return true
             }
             if let memberAccess = call.calledExpression.as(MemberAccessExprSyntax.self),
+               memberAccess.declName.baseName.text == "init",
                let baseExpr = memberAccess.base?.as(DeclReferenceExprSyntax.self),
                baseExpr.baseName.text == "UIColor" {
                 return true

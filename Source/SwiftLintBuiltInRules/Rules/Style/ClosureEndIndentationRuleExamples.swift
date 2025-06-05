ae7a5bf28e709a8944c08b598ffd3000984fee6a
Danny Mösch <danny.moesch@icloud.com>
2025-06-05 22:41:14 +0200

    Prefer string blocks

--- Source/SwiftLintBuiltInRules/Rules/Style/ClosureEndIndentationRuleExamples.swift
+++ Source/SwiftLintBuiltInRules/Rules/Style/ClosureEndIndentationRuleExamples.swift
@@ -1,173 +1,239 @@
 internal struct ClosureEndIndentationRuleExamples {
     static let nonTriggeringExamples = [
-        Example("SignalProducer(values: [1, 2, 3])\n" +
-        "   .startWithNext { number in\n" +
-        "       print(number)\n" +
-        "   }\n"),
+        Example("""
+        SignalProducer(values: [1, 2, 3])
+           .startWithNext { number in
+               print(number)
+           }
+        """),
         Example("[1, 2].map { $0 + 1 }\n"),
-        Example("return match(pattern: pattern, with: [.comment]).flatMap { range in\n" +
-        "   return Command(string: contents, range: range)\n" +
-        "}.flatMap { command in\n" +
-        "   return command.expand()\n" +
-        "}\n"),
-        Example("foo(foo: bar,\n" +
-        "    options: baz) { _ in }\n"),
-        Example("someReallyLongProperty.chainingWithAnotherProperty\n" +
-        "   .foo { _ in }"),
-        Example("foo(abc, 123)\n" +
-        "{ _ in }\n"),
-        Example("function(\n" +
-        "    closure: { x in\n" +
-        "        print(x)\n" +
-        "    },\n" +
-        "    anotherClosure: { y in\n" +
-        "        print(y)\n" +
-        "    })"),
-        Example("function(parameter: param,\n" +
-        "         closure: { x in\n" +
-        "    print(x)\n" +
-        "})"),
-        Example("function(parameter: param, closure: { x in\n" +
-        "        print(x)\n" +
-        "    },\n" +
-        "    anotherClosure: { y in\n" +
-        "        print(y)\n" +
-        "    })"),
+        Example("""
+        return match(pattern: pattern, with: [.comment]).flatMap { range in
+           return Command(string: contents, range: range)
+        }.flatMap { command in
+           return command.expand()
+        }
+        """),
+        Example("""
+        foo(foo: bar,
+            options: baz) { _ in }
+        """),
+        Example("""
+        someReallyLongProperty.chainingWithAnotherProperty
+           .foo { _ in }
+        """),
+        Example("""
+        foo(abc, 123)
+        { _ in }
+        """),
+        Example("""
+        function(
+            closure: { x in
+                print(x)
+            },
+            anotherClosure: { y in
+                print(y)
+            })
+        """),
+        Example("""
+        function(parameter: param,
+                 closure: { x in
+            print(x)
+        })
+        """),
+        Example("""
+        function(parameter: param, closure: { x in
+                print(x)
+            },
+            anotherClosure: { y in
+                print(y)
+            })
+        """),
         Example("(-variable).foo()"),
     ]
 
     static let triggeringExamples = [
-        Example("SignalProducer(values: [1, 2, 3])\n" +
-        "   .startWithNext { number in\n" +
-        "       print(number)\n" +
-        "↓}\n"),
-        Example("return match(pattern: pattern, with: [.comment]).flatMap { range in\n" +
-        "   return Command(string: contents, range: range)\n" +
-        "   ↓}.flatMap { command in\n" +
-        "   return command.expand()\n" +
-        "↓}\n"),
-        Example("function(\n" +
-        "    closure: { x in\n" +
-        "        print(x)\n" +
-        "↓},\n" +
-        "    anotherClosure: { y in\n" +
-        "        print(y)\n" +
-        "↓})"),
+        Example("""
+        SignalProducer(values: [1, 2, 3])
+           .startWithNext { number in
+               print(number)
+        ↓}
+        """),
+        Example("""
+        return match(pattern: pattern, with: [.comment]).flatMap { range in
+           return Command(string: contents, range: range)
+           ↓}.flatMap { command in
+           return command.expand()
+        ↓}
+        """),
+        Example("""
+        function(
+            closure: { x in
+                print(x)
+        ↓},
+            anotherClosure: { y in
+                print(y)
+        ↓})
+        """),
     ]
 
     static let corrections = [
-        Example("SignalProducer(values: [1, 2, 3])\n" +
-        "   .startWithNext { number in\n" +
-        "       print(number)\n" +
-        "↓}\n"): Example("SignalProducer(values: [1, 2, 3])\n" +
-        "   .startWithNext { number in\n" +
-        "       print(number)\n" +
-        "   }\n"),
-        Example("SignalProducer(values: [1, 2, 3])\n" +
-        "   .startWithNext { number in\n" +
-        "       print(number)\n" +
-        "↓}.another { x in\n" +
-        "       print(x)\n" +
-        "↓}.yetAnother { y in\n" +
-        "       print(y)\n" +
-        "↓})"): Example("SignalProducer(values: [1, 2, 3])\n" +
-        "   .startWithNext { number in\n" +
-        "       print(number)\n" +
-        "   }.another { x in\n" +
-        "       print(x)\n" +
-        "   }.yetAnother { y in\n" +
-        "       print(y)\n" +
-        "   })"),
-        Example("return match(pattern: pattern, with: [.comment]).flatMap { range in\n" +
-        "   return Command(string: contents, range: range)\n" +
-        "↓   }.flatMap { command in\n" +
-        "   return command.expand()\n" +
-        "↓}\n"): Example("return match(pattern: pattern, with: [.comment]).flatMap { range in\n" +
-        "   return Command(string: contents, range: range)\n" +
-        "}.flatMap { command in\n" +
-        "   return command.expand()\n" +
-        "}\n"),
-        Example("function(\n" +
-        "    closure: { x in\n" +
-        "        print(x)\n" +
-        "↓})"): Example("function(\n" +
-        "    closure: { x in\n" +
-        "        print(x)\n" +
-        "    })"),
-        Example("function(\n" +
-        "    closure: { x in\n" +
-        "↓        print(x) })"): Example("function(\n" +
-        "    closure: { x in\n" +
-        "        print(x) \n" +
-        "    })"),
-        Example("function(\n" +
-        "    closure: { x in\n" +
-        "↓ab})"): Example("function(\n" +
-        "    closure: { x in\n" +
-        "ab\n" +
-        "    })"),
-        Example("function(\n" +
-        "    closure: { x in\n" +
-        "        print(x)\n" +
-        "↓},\n" +
-        "    anotherClosure: { y in\n" +
-        "        print(y)\n" +
-        "    })"): Example("function(\n" +
-        "    closure: { x in\n" +
-        "        print(x)\n" +
-        "    },\n" +
-        "    anotherClosure: { y in\n" +
-        "        print(y)\n" +
-        "    })"),
-        Example("function(\n" +
-        "    closure: { x in\n" +
-        "        print(x)\n" +
-        "↓       },\n" +
-        "    anotherClosure: { y in\n" +
-        "        print(y)\n" +
-        "    })"): Example("function(\n" +
-        "    closure: { x in\n" +
-        "        print(x)\n" +
-        "    },\n" +
-        "    anotherClosure: { y in\n" +
-        "        print(y)\n" +
-        "    })"),
-        Example("function(\n" +
-        "    closure: { x in\n" +
-        "        print(x)\n" +
-        "↓ab},\n" +
-        "    anotherClosure: { y in\n" +
-        "        print(y)\n" +
-        "    })"): Example("function(\n" +
-        "    closure: { x in\n" +
-        "        print(x)\n" +
-        "ab\n" +
-        "    },\n" +
-        "    anotherClosure: { y in\n" +
-        "        print(y)\n" +
-        "    })"),
-        Example("function(\n" +
-        "    closure: { x in\n" +
-        "↓        print(x) },\n" +
-        "    anotherClosure: { y in\n" +
-        "        print(y)\n" +
-        "    })"): Example("function(\n" +
-        "    closure: { x in\n" +
-        "        print(x) \n" +
-        "    },\n" +
-        "    anotherClosure: { y in\n" +
-        "        print(y)\n" +
-        "    })"),
-        Example("function(\n" +
-        "    closure: { x in\n" +
-        "        print(x)\n" +
-        "↓}, anotherClosure: { y in\n" +
-        "    print(y)\n" +
-        "↓})"): Example("function(\n" +
-        "    closure: { x in\n" +
-        "        print(x)\n" +
-        "    }, anotherClosure: { y in\n" +
-        "    print(y)\n" +
-        "    })"),
+        Example("""
+        SignalProducer(values: [1, 2, 3])
+            .startWithNext { number in
+                print(number)
+        ↓}
+        """): Example("""
+            SignalProducer(values: [1, 2, 3])
+                .startWithNext { number in
+                    print(number)
+                }
+            """),
+        Example("""
+        SignalProducer(values: [1, 2, 3])
+            .startWithNext { number in
+                print(number)
+        ↓}.another { x in
+                print(x)
+        ↓}.yetAnother { y in
+                print(y)
+        ↓})
+        """): Example("""
+            SignalProducer(values: [1, 2, 3])
+                .startWithNext { number in
+                    print(number)
+                }.another { x in
+                    print(x)
+                }.yetAnother { y in
+                    print(y)
+                })
+            """),
+        Example("""
+        return match(pattern: pattern, with: [.comment]).flatMap { range in
+        return Command(string: contents, range: range)
+        ↓   }.flatMap { command in
+        return command.expand()
+        ↓}
+        """): Example("""
+            return match(pattern: pattern, with: [.comment]).flatMap { range in
+            return Command(string: contents, range: range)
+            }.flatMap { command in
+            return command.expand()
+            }
+            """),
+        Example("""
+        function(
+            closure: { x in
+                print(x)
+        ↓})
+        """): Example("""
+            function(
+                closure: { x in
+                    print(x)
+                })
+            """),
+        Example("""
+        function(
+            closure: { x in
+        ↓        print(x) })
+        """): Example("""
+            function(
+                closure: { x in
+                    print(x) \("")
+                })
+            """),
+        Example("""
+        function(
+            closure: { x in
+        ↓ab})
+        """): Example("""
+            function(
+                closure: { x in
+            ab
+                })
+            """),
+        Example("""
+        function(
+            closure: { x in
+                print(x)
+        ↓},
+            anotherClosure: { y in
+                print(y)
+            })
+        """): Example("""
+            function(
+                closure: { x in
+                    print(x)
+                },
+                anotherClosure: { y in
+                    print(y)
+                })
+            """),
+        Example("""
+        function(
+            closure: { x in
+                print(x)
+        ↓       },
+            anotherClosure: { y in
+                print(y)
+            })
+        """): Example("""
+            function(
+                closure: { x in
+                    print(x)
+                },
+                anotherClosure: { y in
+                    print(y)
+                })
+            """),
+        Example("""
+        function(
+            closure: { x in
+                print(x)
+        ↓ab},
+            anotherClosure: { y in
+                print(y)
+            })
+        """): Example("""
+            function(
+                closure: { x in
+                    print(x)
+            ab
+                },
+                anotherClosure: { y in
+                    print(y)
+                })
+            """),
+        Example("""
+        function(
+            closure: { x in
+        ↓        print(x) },
+            anotherClosure: { y in
+                print(y)
+            })
+        """): Example("""
+            function(
+                closure: { x in
+                    print(x) \("")
+                },
+                anotherClosure: { y in
+                    print(y)
+                })
+            """),
+        Example("""
+        function(
+            closure: { x in
+                print(x)
+        ↓}, anotherClosure: { y in
+            print(y)
+        ↓})
+        """): Example("""
+            function(
+                closure: { x in
+                    print(x)
+                }, anotherClosure: { y in
+                print(y)
+                })
+            """),
     ]
 }

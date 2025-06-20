realm/SwiftLint

commit 7e0ea598ceeba4af261f8dc8781d7bf09d32e533

Let `large_tuple` rule adhere to common style (#6120)

Source/SwiftLintBuiltInRules/Rules/Metrics/LargeTupleRule.swift
@@ -1,6 +1,7 @@
 import SwiftSyntax
 
-struct LargeTupleRule: SourceKitFreeRule {
+@SwiftSyntaxRule
+struct LargeTupleRule: Rule {
     var configuration = SeverityLevelsConfiguration<Self>(warning: 2, error: 3)
 
     static let description = RuleDescription(
@@ -8,32 +9,74 @@ struct LargeTupleRule: SourceKitFreeRule {
         name: "Large Tuple",
         description: "Tuples shouldn't have too many members. Create a custom type instead.",
         kind: .metrics,
-        nonTriggeringExamples: LargeTupleRuleExamples.nonTriggeringExamples,
-        triggeringExamples: LargeTupleRuleExamples.triggeringExamples
+        nonTriggeringExamples: [
+            Example("let foo: (Int, Int)"),
+            Example("let foo: (start: Int, end: Int)"),
+            Example("let foo: (Int, (Int, String))"),
+            Example("func foo() -> (Int, Int)"),
+            Example("func foo() -> (Int, Int) {}"),
+            Example("func foo(bar: String) -> (Int, Int)"),
+            Example("func foo(bar: String) -> (Int, Int) {}"),
+            Example("func foo() throws -> (Int, Int)"),
+            Example("func foo() throws -> (Int, Int) {}"),
+            Example("let foo: (Int, Int, Int) -> Void"),
+            Example("let foo: (Int, Int, Int) throws -> Void"),
+            Example("func foo(bar: (Int, String, Float) -> Void)"),
+            Example("func foo(bar: (Int, String, Float) throws -> Void)"),
+            Example("var completionHandler: ((_ data: Data?, _ resp: URLResponse?, _ e: NSError?) -> Void)!"),
+            Example("func getDictionaryAndInt() -> (Dictionary<Int, String>, Int)?"),
+            Example("func getGenericTypeAndInt() -> (Type<Int, String, Float>, Int)?"),
+            Example("func foo() async -> (Int, Int)"),
+            Example("func foo() async -> (Int, Int) {}"),
+            Example("func foo(bar: String) async -> (Int, Int)"),
+            Example("func foo(bar: String) async -> (Int, Int) {}"),
+            Example("func foo() async throws -> (Int, Int)"),
+            Example("func foo() async throws -> (Int, Int) {}"),
+            Example("let foo: (Int, Int, Int) async -> Void"),
+            Example("let foo: (Int, Int, Int) async throws -> Void"),
+            Example("func foo(bar: (Int, String, Float) async -> Void)"),
+            Example("func foo(bar: (Int, String, Float) async throws -> Void)"),
+            Example("func getDictionaryAndInt() async -> (Dictionary<Int, String>, Int)?"),
+            Example("func getGenericTypeAndInt() async -> (Type<Int, String, Float>, Int)?"),
+        ],
+        triggeringExamples: [
+            Example("let foo: ↓(Int, Int, Int)"),
+            Example("let foo: ↓(start: Int, end: Int, value: String)"),
+            Example("let foo: (Int, ↓(Int, Int, Int))"),
+            Example("func foo(bar: ↓(Int, Int, Int))"),
+            Example("func foo() -> ↓(Int, Int, Int)"),
+            Example("func foo() -> ↓(Int, Int, Int) {}"),
+            Example("func foo(bar: String) -> ↓(Int, Int, Int)"),
+            Example("func foo(bar: String) -> ↓(Int, Int, Int) {}"),
+            Example("func foo() throws -> ↓(Int, Int, Int)"),
+            Example("func foo() throws -> ↓(Int, Int, Int) {}"),
+            Example("func foo() throws -> ↓(Int, ↓(String, String, String), Int) {}"),
+            Example("func getDictionaryAndInt() -> (Dictionary<Int, ↓(String, String, String)>, Int)?"),
+            Example("func foo(bar: ↓(Int, Int, Int)) async"),
+            Example("func foo() async -> ↓(Int, Int, Int)"),
+            Example("func foo() async -> ↓(Int, Int, Int) {}"),
+            Example("func foo(bar: String) async -> ↓(Int, Int, Int)"),
+            Example("func foo(bar: String) async -> ↓(Int, Int, Int) {}"),
+            Example("func foo() async throws -> ↓(Int, Int, Int)"),
+            Example("func foo() async throws -> ↓(Int, Int, Int) {}"),
+            Example("func foo() async throws -> ↓(Int, ↓(String, String, String), Int) {}"),
+            Example("func getDictionaryAndInt() async -> (Dictionary<Int, ↓(String, String, String)>, Int)?"),
+        ]
     )
-
-    func validate(file: SwiftLintFile) -> [StyleViolation] {
-        LargeTupleRuleVisitor(viewMode: .sourceAccurate)
-            .walk(file: file, handler: \.violationPositions)
-            .sorted(by: { $0.position < $1.position })
-            .compactMap { position, size in
-                for parameter in configuration.params where size > parameter.value {
-                    let reason = "Tuples should have at most \(configuration.warning) members"
-                    return StyleViolation(ruleDescription: Self.description,
-                                          severity: parameter.severity,
-                                          location: Location(file: file, position: position),
-                                          reason: reason)
-                }
-
-                return nil
-            }
-    }
 }
 
-private final class LargeTupleRuleVisitor: SyntaxVisitor {
-    private(set) var violationPositions: [(position: AbsolutePosition, memberCount: Int)] = []
-
-    override func visitPost(_ node: TupleTypeSyntax) {
-        violationPositions.append((node.positionAfterSkippingLeadingTrivia, node.elements.count))
+private extension LargeTupleRule {
+    final class Visitor: ViolationsSyntaxVisitor<ConfigurationType> {
+        override func visitPost(_ node: TupleTypeSyntax) {
+            let memberCount = node.elements.count
+            for parameter in configuration.params where memberCount > parameter.value {
+                violations.append(.init(
+                    position: node.positionAfterSkippingLeadingTrivia,
+                    reason: "Tuples should have at most \(parameter.value) members",
+                    severity: parameter.severity
+                ))
+                return
+            }
+        }
     }
 }

Source/SwiftLintBuiltInRules/Rules/Metrics/LargeTupleRuleExamples.swift
@@ -1,56 +1,0 @@
-struct LargeTupleRuleExamples {
-    static let nonTriggeringExamples: [Example] = [
-        Example("let foo: (Int, Int)"),
-        Example("let foo: (start: Int, end: Int)"),
-        Example("let foo: (Int, (Int, String))"),
-        Example("func foo() -> (Int, Int)"),
-        Example("func foo() -> (Int, Int) {}"),
-        Example("func foo(bar: String) -> (Int, Int)"),
-        Example("func foo(bar: String) -> (Int, Int) {}"),
-        Example("func foo() throws -> (Int, Int)"),
-        Example("func foo() throws -> (Int, Int) {}"),
-        Example("let foo: (Int, Int, Int) -> Void"),
-        Example("let foo: (Int, Int, Int) throws -> Void"),
-        Example("func foo(bar: (Int, String, Float) -> Void)"),
-        Example("func foo(bar: (Int, String, Float) throws -> Void)"),
-        Example("var completionHandler: ((_ data: Data?, _ resp: URLResponse?, _ e: NSError?) -> Void)!"),
-        Example("func getDictionaryAndInt() -> (Dictionary<Int, String>, Int)?"),
-        Example("func getGenericTypeAndInt() -> (Type<Int, String, Float>, Int)?"),
-        Example("func foo() async -> (Int, Int)"),
-        Example("func foo() async -> (Int, Int) {}"),
-        Example("func foo(bar: String) async -> (Int, Int)"),
-        Example("func foo(bar: String) async -> (Int, Int) {}"),
-        Example("func foo() async throws -> (Int, Int)"),
-        Example("func foo() async throws -> (Int, Int) {}"),
-        Example("let foo: (Int, Int, Int) async -> Void"),
-        Example("let foo: (Int, Int, Int) async throws -> Void"),
-        Example("func foo(bar: (Int, String, Float) async -> Void)"),
-        Example("func foo(bar: (Int, String, Float) async throws -> Void)"),
-        Example("func getDictionaryAndInt() async -> (Dictionary<Int, String>, Int)?"),
-        Example("func getGenericTypeAndInt() async -> (Type<Int, String, Float>, Int)?"),
-    ]
-
-    static let triggeringExamples: [Example] = [
-        Example("let foo: ↓(Int, Int, Int)"),
-        Example("let foo: ↓(start: Int, end: Int, value: String)"),
-        Example("let foo: (Int, ↓(Int, Int, Int))"),
-        Example("func foo(bar: ↓(Int, Int, Int))"),
-        Example("func foo() -> ↓(Int, Int, Int)"),
-        Example("func foo() -> ↓(Int, Int, Int) {}"),
-        Example("func foo(bar: String) -> ↓(Int, Int, Int)"),
-        Example("func foo(bar: String) -> ↓(Int, Int, Int) {}"),
-        Example("func foo() throws -> ↓(Int, Int, Int)"),
-        Example("func foo() throws -> ↓(Int, Int, Int) {}"),
-        Example("func foo() throws -> ↓(Int, ↓(String, String, String), Int) {}"),
-        Example("func getDictionaryAndInt() -> (Dictionary<Int, ↓(String, String, String)>, Int)?"),
-        Example("func foo(bar: ↓(Int, Int, Int)) async"),
-        Example("func foo() async -> ↓(Int, Int, Int)"),
-        Example("func foo() async -> ↓(Int, Int, Int) {}"),
-        Example("func foo(bar: String) async -> ↓(Int, Int, Int)"),
-        Example("func foo(bar: String) async -> ↓(Int, Int, Int) {}"),
-        Example("func foo() async throws -> ↓(Int, Int, Int)"),
-        Example("func foo() async throws -> ↓(Int, Int, Int) {}"),
-        Example("func foo() async throws -> ↓(Int, ↓(String, String, String), Int) {}"),
-        Example("func getDictionaryAndInt() async -> (Dictionary<Int, ↓(String, String, String)>, Int)?"),
-    ]
-}

commit c1c2418a1f71b4ebaa8e3a306c46eed71cc80043

Report warning threshold
Behavioral change caused by 7e0ea598c.

Source/SwiftLintBuiltInRules/Rules/Metrics/LargeTupleRule.swift
@@ -72,7 +72,7 @@ private extension LargeTupleRule {
             for parameter in configuration.params where memberCount > parameter.value {
                 violations.append(.init(
                     position: node.positionAfterSkippingLeadingTrivia,
-                    reason: "Tuples should have at most \(parameter.value) members",
+                    reason: "Tuples should have at most \(configuration.warning) members",
                     severity: parameter.severity
                 ))
                 return
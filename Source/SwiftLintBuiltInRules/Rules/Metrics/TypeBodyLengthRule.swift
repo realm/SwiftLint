8f8166c174aad02d8bb893bf12945f45797531f8
Danny Mösch <danny.moesch@icloud.com>
2025-06-21 15:09:57 +0200

    Support protocols and extensions in type body length checking

--- Source/SwiftLintBuiltInRules/Rules/Metrics/TypeBodyLengthRule.swift
+++ Source/SwiftLintBuiltInRules/Rules/Metrics/TypeBodyLengthRule.swift
@@ -1,37 +1,90 @@
 import SwiftSyntax
 
-private func wrapExample(
-    prefix: String = "",
-    _ type: String,
-    _ template: String,
-    _ count: Int,
-    _ add: String = "",
-    file: StaticString = #filePath,
-    line: UInt = #line) -> Example {
-    Example("\(prefix)\(type) Abc {\n" +
-                   repeatElement(template, count: count).joined() + "\(add)}\n", file: file, line: line)
-}
-
 @SwiftSyntaxRule
 struct TypeBodyLengthRule: Rule {
     var configuration = SeverityLevelsConfiguration<Self>(warning: 250, error: 350)
+
+    private static let testConfig = ["warning": 2]
 
     static let description = RuleDescription(
         identifier: "type_body_length",
         name: "Type Body Length",
         description: "Type bodies should not span too many lines",
         kind: .metrics,
-        nonTriggeringExamples: ["class", "struct", "enum", "actor"].flatMap({ type in
-            [
-                wrapExample(type, "let abc = 0\n", 249),
-                wrapExample(type, "\n", 251),
-                wrapExample(type, "// this is a comment\n", 251),
-                wrapExample(type, "let abc = 0\n", 249, "\n/* this is\na multiline comment\n*/"),
-            ]
-        }),
-        triggeringExamples: ["class", "struct", "enum", "actor"].map({ type in
-             wrapExample(prefix: "↓", type, "let abc = 0\n", 251)
-        })
+        nonTriggeringExamples: [
+            Example("actor A {}", configuration: testConfig),
+            Example("class C {}", configuration: testConfig),
+            Example("enum E {}", configuration: testConfig),
+            Example("extension E {}", configuration: testConfig),
+            Example("protocol P {}", configuration: testConfig),
+            Example("struct S {}", configuration: testConfig),
+            Example("""
+                actor A {
+                    let x = 0
+                }
+                """, configuration: testConfig),
+            Example("""
+                class C {
+                    let x = 0
+                    // comments
+                    // will
+                    // be
+                    // ignored
+                }
+                """, configuration: testConfig),
+            Example("""
+                enum E {
+                    let x = 0
+                    // empty lines will be ignored
+
+
+                }
+                """, configuration: testConfig),
+        ],
+        triggeringExamples: [
+            Example("""
+                ↓actor A {
+                    let x = 0
+                    let y = 1
+                    let z = 2
+                }
+                """, configuration: testConfig),
+            Example("""
+                ↓class C {
+                    let x = 0
+                    let y = 1
+                    let z = 2
+                }
+                """, configuration: testConfig),
+            Example("""
+                ↓enum E {
+                    let x = 0
+                    let y = 1
+                    let z = 2
+                }
+                """, configuration: testConfig),
+            Example("""
+                ↓extension E {
+                    let x = 0
+                    let y = 1
+                    let z = 2
+                }
+                """, configuration: testConfig),
+            Example("""
+                ↓protocol P {
+                    let x = 0
+                    let y = 1
+                    let z = 2
+                }
+                """, configuration: testConfig),
+            Example("""
+                ↓struct S {
+                    let x = 0
+                    let y = 1
+                    let z = 2
+                }
+                """, configuration: testConfig),
+        ]
     )
 }
 
@@ -41,11 +94,19 @@ private extension TypeBodyLengthRule {
             collectViolation(node)
         }
 
+        override func visitPost(_ node: ClassDeclSyntax) {
+            collectViolation(node)
+        }
+
         override func visitPost(_ node: EnumDeclSyntax) {
             collectViolation(node)
         }
 
-        override func visitPost(_ node: ClassDeclSyntax) {
+        override func visitPost(_ node: ExtensionDeclSyntax) {
+            collectViolation(node)
+        }
+
+        override func visitPost(_ node: ProtocolDeclSyntax) {
             collectViolation(node)
         }
 

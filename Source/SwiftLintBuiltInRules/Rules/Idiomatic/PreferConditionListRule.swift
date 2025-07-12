530e5d8d27d8dbd09ec19c51d73fe908f2a91ceb
Danny Mösch <danny.moesch@icloud.com>
2025-07-12 13:23:21 +0200

    Add rationale

--- Source/SwiftLintBuiltInRules/Rules/Idiomatic/PreferConditionListRule.swift
+++ Source/SwiftLintBuiltInRules/Rules/Idiomatic/PreferConditionListRule.swift
@@ -9,6 +9,28 @@ struct PreferConditionListRule: Rule {
         identifier: "prefer_condition_list",
         name: "Prefer Condition List",
         description: "Prefer a condition list over chaining conditions with '&&'",
+        rationale: """
+            Instead of chaining conditions with `&&`, use a condition list to separate conditions with commas, that is,
+            use
+
+            ```
+            if a, b {}
+            ```
+
+            instead of
+
+            ```
+            if a && b {}
+            ```
+
+            Using a condition list improves readability and makes it easier to add or remove conditions in the future.
+            It also allows for better formatting and alignment of conditions. All in all, it's the idiomatic way to
+            write conditions in Swift.
+
+            Since function calls with trailing closures trigger a warning in the Swift compiler when used in
+            conditions, this rule makes sure to wrap such expressions in parentheses when transforming them to
+            condition list elements. The scope of the parentheses is limited to the function call itself.
+            """,
         kind: .idiomatic,
         nonTriggeringExamples: [
             Example("if a, b {}"),

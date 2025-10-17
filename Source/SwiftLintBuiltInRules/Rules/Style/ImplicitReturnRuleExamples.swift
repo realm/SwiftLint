9c869358587c8660b9148efb329cfd9cbfaf76c6
nandhinisubbu <nandhinis2608@gmail.com>
2025-10-17 21:05:54 +0530

    Fix linting in ImplicitReturnRuleExamples

--- Source/SwiftLintBuiltInRules/Rules/Style/ImplicitReturnRuleExamples.swift
+++ Source/SwiftLintBuiltInRules/Rules/Style/ImplicitReturnRuleExamples.swift
@@ -1,5 +1,5 @@
 // swiftlint:disable file_length
-
+// swiftlint:disable:next type_body_length
 struct ImplicitReturnRuleExamples {
     struct ClosureExamples {
         static let nonTriggeringExamples = [

721780388056a3bbf6bddf8641443f458f52c448
Martin Redington <mildm8nnered@gmail.com>
2025-10-23 23:17:55 +0100

    Wrong rationale

--- Source/SwiftLintBuiltInRules/Rules/Performance/ContainsOverRangeNilComparisonRule.swift
+++ Source/SwiftLintBuiltInRules/Rules/Performance/ContainsOverRangeNilComparisonRule.swift
@@ -8,10 +8,6 @@ struct ContainsOverRangeNilComparisonRule: Rule {
         identifier: "contains_over_range_nil_comparison",
         name: "Contains over Range Comparison to Nil",
         description: "Prefer `contains` over `range(of:) != nil` and `range(of:) == nil`",
-        rationale: """
-        `filter` always needs to scan the entire collection, whereas `contains` can exit early as 
-        soon as a match is found.
-        """,
         kind: .performance,
         nonTriggeringExamples: [
             Example("let range = myString.range(of: \"Test\")"),

da9d742874d88f6d5d0f7b315d1fcf12655f2311
Danny Mösch <16365760+SimplyDanny@users.noreply.github.com>
2025-10-28 17:10:24 +0000

    Release 0.62.2

--- Package.swift
+++ Package.swift
@@ -235,8 +235,8 @@ let package = Package(
 package.targets.append(
     .binaryTarget(
         name: "SwiftLintBinary",
-        url: "https://github.com/realm/SwiftLint/releases/download/0.62.1/SwiftLintBinary.artifactbundle.zip",
-        checksum: "7be75aeb533dd91e66e1d47123828643d7fa606807de1b0335c3cc14d2d1abc2"
+        url: "https://github.com/realm/SwiftLint/releases/download/0.62.2/SwiftLintBinary.artifactbundle.zip",
+        checksum: "3047357eee0838a0bafc7a6e65cd1aad61734b30d7233e28f3434149fe02f522"
     )
 )
 #endif

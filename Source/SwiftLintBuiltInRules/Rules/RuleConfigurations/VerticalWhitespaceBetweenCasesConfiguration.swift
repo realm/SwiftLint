a8c8b1aea8436a02bf170bccbf9b7f065cdfd623
Danny Mösch <danny.moesch@icloud.com>
2025-11-02 08:05:38 +0100

    Add `separation_style` option to `vertical_whitespace_between_cases`

---
+++ Source/SwiftLintBuiltInRules/Rules/RuleConfigurations/VerticalWhitespaceBetweenCasesConfiguration.swift
@@ -0,0 +1,17 @@
+import SwiftLintCore
+
+@AutoConfigParser // swiftlint:disable:next type_name
+struct VerticalWhitespaceBetweenCasesConfiguration: SeverityBasedRuleConfiguration {
+    typealias Parent = VerticalWhitespaceBetweenCasesRule
+
+    @AcceptableByConfigurationElement
+    enum SeparationStyle: String {
+        case always
+        case never
+    }
+
+    @ConfigurationElement(key: "severity")
+    private(set) var severityConfiguration = SeverityConfiguration<Parent>(.warning)
+    @ConfigurationElement(key: "separation")
+    private(set) var separation: SeparationStyle = .always
+}

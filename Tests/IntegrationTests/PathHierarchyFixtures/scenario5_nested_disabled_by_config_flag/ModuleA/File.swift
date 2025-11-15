23c9cc3d3156fd30c164c8e548e896e175b2cb94
Danny Mösch <danny.moesch@icloud.com>
2025-11-16 00:13:11 +0100

    Regression test

--- Tests/IntegrationTests/PathHierarchyFixtures/scenario5_nested_disabled_by_config_flag/ModuleA/File.swift
+++
@@ -1,4 +0,0 @@
-// Nested config should be ignored when --config flag is used
-struct ModuleAFile {
-    let name: String
-}
---
+++ Tests/IntegrationTests/PathHierarchyFixtures/scenario7_wildcard_regression_5953/MyProject/SubModule/Package.swift
@@ -0,0 +1,7 @@
+// Should NOT be linted - nested Package.swift
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "SubPackage"
+)

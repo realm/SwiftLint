6b1999a6a21d5de6266dceb39cd325554b0a56a5
Danny Mösch <danny.moesch@icloud.com>
2025-11-30 23:57:03 +0100

    Polish examples and violation message

--- Source/SwiftLintBuiltInRules/Rules/Lint/MultilineCallArgumentsRule.swift
+++ Source/SwiftLintBuiltInRules/Rules/Lint/MultilineCallArgumentsRule.swift
@@ -8,60 +8,60 @@ struct MultilineCallArgumentsRule: Rule {
     static let description = RuleDescription(
         identifier: "multiline_call_arguments",
         name: "Multiline Call Arguments",
-        description: "Call should have one parameter per line",
+        description: "Call should have each parameter on a separate line",
         kind: .style,
         nonTriggeringExamples: [
             Example("""
-                    foo(
-                    param1: "param1",
-                        param2: false,
-                        param3: []
-                    )
-                    """, configuration: ["max_number_of_single_line_parameters": 2]),
+                foo(
+                param1: "param1",
+                    param2: false,
+                    param3: []
+                )
+                """,
+                configuration: ["max_number_of_single_line_parameters": 2]),
             Example("""
-                    foo(param1: 1,
-                        param2: false,
-                        param3: [])
-                    """, configuration: ["max_number_of_single_line_parameters": 1]),
-            Example("foo(param1: 1, param2: false)",
-                    configuration: ["max_number_of_single_line_parameters": 2]),
-            Example("Enum.foo(param1: 1, param2: false)",
-                    configuration: ["max_number_of_single_line_parameters": 2]),
+                foo(param1: 1,
+                    param2: false,
+                    param3: [])
+                """,
+                configuration: ["max_number_of_single_line_parameters": 1]),
+            Example(
+                "foo(param1: 1, param2: false)",
+                configuration: ["max_number_of_single_line_parameters": 2]),
+            Example(
+                "Enum.foo(param1: 1, param2: false)",
+                configuration: ["max_number_of_single_line_parameters": 2]),
             Example("foo(param1: 1)", configuration: ["allows_single_line": false]),
             Example("Enum.foo(param1: 1)", configuration: ["allows_single_line": false]),
-            Example("Enum.foo(param1: 1, param2: 2, param3: 3)", configuration: ["allows_single_line": true]),
+            Example(
+                "Enum.foo(param1: 1, param2: 2, param3: 3)",
+                configuration: ["allows_single_line": true]),
             Example("""
-                    foo(
-                        param1: 1,
-                        param2: 2,
-                        param3: 3
-                    )
-                    """,
-                    configuration: [
-                        "allows_single_line": false
-                    ]),
+                foo(
+                    param1: 1,
+                    param2: 2,
+                    param3: 3
+                )
+                """,
+                configuration: ["allows_single_line": false]),
         ],
         triggeringExamples: [
-            Example("↓foo(param1: 1, param2: false, param3: [])",
-                    configuration: [
-                        "max_number_of_single_line_parameters": 2
-                    ]),
-            Example("↓Enum.foo(param1: 1, param2: false, param3: [])",
-                    configuration: [
-                        "max_number_of_single_line_parameters": 2
-                    ]),
+            Example(
+                "↓foo(param1: 1, param2: false, param3: [])",
+                configuration: ["max_number_of_single_line_parameters": 2]),
+            Example(
+                "↓Enum.foo(param1: 1, param2: false, param3: [])",
+                configuration: ["max_number_of_single_line_parameters": 2]),
             Example("""
-                    ↓foo(param1: 1, param2: false,
-                         param3: [])
-                    """, configuration: [
-                        "max_number_of_single_line_parameters": 3
-                    ]),
+                ↓foo(param1: 1, param2: false,
+                        param3: [])
+                """,
+                configuration: ["max_number_of_single_line_parameters": 3]),
             Example("""
-                    ↓Enum.foo(param1: 1, param2: false,
-                         param3: [])
-                    """, configuration: [
-                        "max_number_of_single_line_parameters": 3
-                    ]),
+                ↓Enum.foo(param1: 1, param2: false,
+                        param3: [])
+                """,
+                configuration: ["max_number_of_single_line_parameters": 3]),
             Example("↓foo(param1: 1, param2: false)", configuration: ["allows_single_line": false]),
             Example("↓Enum.foo(param1: 1, param2: false)", configuration: ["allows_single_line": false]),
         ]
@@ -71,17 +71,12 @@ struct MultilineCallArgumentsRule: Rule {
 private extension MultilineCallArgumentsRule {
     final class Visitor: ViolationsSyntaxVisitor<ConfigurationType> {
         override func visitPost(_ node: FunctionCallExprSyntax) {
-            let parameterPositions = node.arguments.map(\.positionAfterSkippingLeadingTrivia)
-            if containsViolation(parameterPositions: parameterPositions) {
+            if containsViolation(parameterPositions: node.arguments.map(\.positionAfterSkippingLeadingTrivia)) {
                 violations.append(node.calledExpression.positionAfterSkippingLeadingTrivia)
             }
         }
 
         private func containsViolation(parameterPositions: [AbsolutePosition]) -> Bool {
-            guard parameterPositions.isNotEmpty else {
-                return false
-            }
-
             var numberOfParameters = 0
             var linesWithParameters: Set<Int> = []
             var hasMultipleParametersOnSameLine = false
